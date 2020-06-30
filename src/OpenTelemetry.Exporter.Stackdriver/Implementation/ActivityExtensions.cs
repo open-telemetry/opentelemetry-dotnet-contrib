9852017e724286d525ef63113c657941485bafd6
Ian Milligan <ianmllgn@gmail.com>
2020-06-30 15:40:01 -0700

    Add StackdriverTraceActivityExporter

---
+++ src/OpenTelemetry.Exporter.Stackdriver/Implementation/ActivityExtensions.cs
@@ -0,0 +1,127 @@
+// <copyright file="ActivityExtensions.cs" company="OpenTelemetry Authors">
+// Copyright 2020, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Linq;
+using System.Diagnostics;
+using Google.Cloud.Trace.V2;
+using Google.Protobuf.WellKnownTypes;
+
+namespace OpenTelemetry.Exporter.Stackdriver.Implementation
+{
+    internal static class ActivityExtensions {
+        /// <summary>
+        /// Translating an Activity to Stackdriver's Span format.
+        /// According to <see href="https://cloud.google.com/trace/docs/reference/v2/rpc/google.devtools.cloudtrace.v2"/> specifications.
+        /// </summary>
+        /// <param name="activity">Activity.</param>
+        /// <param name="projectId">Google Cloud Platform Project Id.</param>
+        /// <returns><see cref="TelemetrySpan"/>.</returns>
+        public static Google.Cloud.Trace.V2.Span ToSpan(this Activity activity, string projectId)
+        {
+            var spanId = activity.SpanId.ToHexString();
+
+            // Base span settings
+            var span = new Google.Cloud.Trace.V2.Span
+            {
+                SpanName = new SpanName(projectId, activity.TraceId.ToHexString(), spanId),
+                SpanId = spanId,
+                DisplayName = new TruncatableString { Value = activity.OperationName },
+                StartTime = Timestamp.FromDateTime(activity.StartTimeUtc),
+                EndTime = Timestamp.FromDateTime(activity.StartTimeUtc.Add(activity.Duration)),
+                ChildSpanCount = null,
+            };
+            if (!activity.ParentSpanId.Equals(ActivitySpanId.CreateFromBytes(new byte[0])))
+            {
+                var parentSpanId = activity.ParentSpanId.ToHexString();
+                if (!string.IsNullOrEmpty(parentSpanId))
+                {
+                    span.ParentSpanId = parentSpanId;
+                }
+            }
+
+            // Span Links
+            if (activity.Links != null)
+            {
+                span.Links = new Google.Cloud.Trace.V2.Span.Types.Links
+                {
+                    Link = { activity.Links.Select(l => l.ToLink()) },
+                };
+            }
+
+            // Span Attributes
+            if (activity.Tags != null)
+            {
+                span.Attributes = new Google.Cloud.Trace.V2.Span.Types.Attributes
+                {
+                    AttributeMap =
+                    {
+                        activity.Tags.ToDictionary(t => t.Key, t => t.Value.ToAttributeValue()),
+                    }
+                };
+            }
+
+            // Span Events
+            if (activity.Events != null)
+            {
+                span.TimeEvents = new Google.Cloud.Trace.V2.Span.Types.TimeEvents
+                {
+                    TimeEvent = { activity.Events.Select(e => e.toEvent()) },
+                };
+            }
+            // TODO: Attributes
+            return span;
+        }
+
+        public static Google.Cloud.Trace.V2.Span.Types.Link ToLink(this ActivityLink link)
+        {
+            var ret = new Google.Cloud.Trace.V2.Span.Types.Link();
+            ret.SpanId = link.Context.SpanId.ToHexString();
+            ret.TraceId = link.Context.TraceId.ToHexString();
+
+            if (link.Attributes != null)
+            {
+                ret.Attributes = new Google.Cloud.Trace.V2.Span.Types.Attributes
+                {
+                    AttributeMap =
+                    {
+                        link.Attributes.ToDictionary(
+                         att => att.Key,
+                         att => att.Value.ToAttributeValue()),
+                    },
+                };
+            }
+
+            return ret;
+        }
+
+        public static Google.Cloud.Trace.V2.Span.Types.TimeEvent toEvent(this ActivityEvent ev)
+        {
+            return new Google.Cloud.Trace.V2.Span.Types.TimeEvent{
+                Time = Timestamp.FromDateTimeOffset(ev.Timestamp),
+                Annotation = new Google.Cloud.Trace.V2.Span.Types.TimeEvent.Types.Annotation{
+                    Description = new TruncatableString{ Value = ev.Name },
+                    Attributes = new Google.Cloud.Trace.V2.Span.Types.Attributes
+                    {
+                        AttributeMap =
+                        {
+                            ev.Attributes.ToDictionary(att => att.Key, att => att.Value.ToAttributeValue()),
+                        },
+                    },
+                },
+            };
+        }
+    }
+}
---
+++ src/OpenTelemetry.Exporter.Stackdriver/StackdriverTraceActivityExporter.cs
@@ -0,0 +1,57 @@
+// <copyright file="StackdriverTraceActivityExporter.cs" company="OpenTelemetry Authors">
+// Copyright 2020, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Api.Gax.Grpc;
+using Google.Cloud.Trace.V2;
+using OpenTelemetry.Exporter.Stackdriver.Implementation;
+using OpenTelemetry.Trace.Export;
+
+namespace OpenTelemetry.Exporter.Stackdriver
+{
+    public class StackdriverTraceActivityExporter : ActivityExporter {
+        private readonly Google.Api.Gax.ResourceNames.ProjectName googleCloudProjectId;
+        private readonly TraceServiceClient traceClient;
+
+        public StackdriverTraceActivityExporter(string projectId) {
+            this.googleCloudProjectId = new Google.Api.Gax.ResourceNames.ProjectName(projectId);
+            this.traceClient = TraceServiceClient.Create();
+        }
+
+        // <inheritdoc/>
+        public override async Task<ExportResult> ExportAsync(IEnumerable<Activity> activityList, CancellationToken cancellationToken) {
+            var spans = activityList.Select(a => a.ToSpan(this.googleCloudProjectId.ProjectId));
+
+            // avoid cancelling here: this is no return point: if we reached this point
+            // and cancellation is requested, it's better if we try to finish sending spans rather than drop i
+            await this.traceClient.BatchWriteSpansAsync(this.googleCloudProjectId, spans).ConfigureAwait(false);
+
+            // TODO failures
+            return ExportResult.Success;
+        }
+    
+        /// <inheritdoc/>
+        public override Task ShutdownAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
