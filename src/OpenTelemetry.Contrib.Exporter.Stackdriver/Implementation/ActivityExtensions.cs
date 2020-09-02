afa3b3639c18a26d2fd0fc1d7e38f024bc3c76dd
Eddy Nakamura <ednakamu@microsoft.com>
2020-09-02 13:24:49 -0300

    Renaming to Contrib namespace (#23)
    * Renaming to Contrib namespace
    
    * updating test and identation

---
+++ src/OpenTelemetry.Contrib.Exporter.Stackdriver/Implementation/ActivityExtensions.cs
@@ -0,0 +1,157 @@
+﻿// <copyright file="ActivityExtensions.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Google.Cloud.Trace.V2;
+using Google.Protobuf.WellKnownTypes;
+using OpenTelemetry.Trace;
+
+namespace OpenTelemetry.Contrib.Exporter.Stackdriver.Implementation
+{
+    internal static class ActivityExtensions
+    {
+        private static Dictionary<string, string> labelsToReplace = new Dictionary<string, string>
+        {
+            { "component", "/component" },
+            { "http.method", "/http/method" },
+            { "http.host", "/http/host" },
+            { "http.status_code", "/http/status_code" },
+            { "http.user_agent", "/http/user_agent" },
+            { "http.path", "/http/path" },
+            { "http.url", "/http/url" },
+            { "http.route", "/http/route" },
+        };
+
+        /// <summary>
+        /// Translating <see cref="Activity"/> to Stackdriver's Span
+        /// According to <see href="https://cloud.google.com/trace/docs/reference/v2/rpc/google.devtools.cloudtrace.v2"/> specifications.
+        /// </summary>
+        /// <param name="activity">Activity in OpenTelemetry format.</param>
+        /// <param name="projectId">Google Cloud Platform Project Id.</param>
+        /// <returns><see cref="TelemetrySpan"/>.</returns>
+        public static Span ToSpan(this Activity activity, string projectId)
+        {
+            var spanId = activity.Context.SpanId.ToHexString();
+
+            // Base span settings
+            var span = new Span
+            {
+                SpanName = new SpanName(projectId, activity.Context.TraceId.ToHexString(), spanId),
+                SpanId = spanId,
+                DisplayName = new TruncatableString { Value = activity.DisplayName },
+                StartTime = activity.StartTimeUtc.ToTimestamp(),
+                EndTime = activity.StartTimeUtc.Add(activity.Duration).ToTimestamp(),
+                ChildSpanCount = null,
+            };
+            if (activity.ParentSpanId != null)
+            {
+                var parentSpanId = activity.ParentSpanId.ToHexString();
+                if (!string.IsNullOrEmpty(parentSpanId))
+                {
+                    span.ParentSpanId = parentSpanId;
+                }
+            }
+
+            // Span Links
+            if (activity.Links != null)
+            {
+                span.Links = new Span.Types.Links
+                {
+                    Link = { activity.Links.Select(l => l.ToLink()) },
+                };
+            }
+
+            // Span Attributes
+            if (activity.Tags != null)
+            {
+                span.Attributes = new Span.Types.Attributes
+                {
+                    AttributeMap =
+                    {
+                        activity.Tags?.ToDictionary(
+                                        s => s.Key,
+                                        s => s.Value?.ToAttributeValue()),
+                    },
+                };
+            }
+
+            // StackDriver uses different labels that are used to categorize spans
+            // replace attribute keys with StackDriver version
+            foreach (var entry in labelsToReplace)
+            {
+                if (span.Attributes.AttributeMap.TryGetValue(entry.Key, out var attrValue))
+                {
+                    span.Attributes.AttributeMap.Remove(entry.Key);
+                    span.Attributes.AttributeMap.Add(entry.Value, attrValue);
+                }
+            }
+
+            return span;
+        }
+
+        public static Span.Types.Link ToLink(this ActivityLink link)
+        {
+            var ret = new Span.Types.Link
+            {
+                SpanId = link.Context.SpanId.ToHexString(),
+                TraceId = link.Context.TraceId.ToHexString(),
+            };
+
+            if (link.Tags != null)
+            {
+                ret.Attributes = new Span.Types.Attributes
+                {
+                    AttributeMap =
+                    {
+                        link.Tags.ToDictionary(
+                            att => att.Key,
+                            att => att.Value.ToAttributeValue()),
+                    },
+                };
+            }
+
+            return ret;
+        }
+
+        public static AttributeValue ToAttributeValue(this object av)
+        {
+            switch (av)
+            {
+                case string s:
+                    return new AttributeValue()
+                    {
+                        StringValue = new TruncatableString() { Value = s },
+                    };
+                case bool b:
+                    return new AttributeValue() { BoolValue = b };
+                case long l:
+                    return new AttributeValue() { IntValue = l };
+                case double d:
+                    return new AttributeValue()
+                    {
+                        StringValue = new TruncatableString() { Value = d.ToString() },
+                    };
+                default:
+                    return new AttributeValue()
+                    {
+                        StringValue = new TruncatableString() { Value = av.ToString() },
+                    };
+            }
+        }
+    }
+}
---
+++ test/OpenTelemetry.Contrib.Instrumentation.MassTransit.Tests/MassTransitInstrumentationTests.cs
@@ -0,0 +1,143 @@
+﻿// <copyright file="MassTransitInstrumentationTests.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit.Testing;
+using Moq;
+using OpenTelemetry.Trace;
+using Xunit;
+
+namespace OpenTelemetry.Contrib.Instrumentation.MassTransit.Tests
+{
+    public class MassTransitInstrumentationTests
+    {
+        [Fact]
+        public async Task MassTransitInstrumentationConsumerAndHandlerTest()
+        {
+            var activityProcessor = new Mock<ActivityProcessor>();
+            using (Sdk.CreateTracerProviderBuilder()
+                .AddProcessor(activityProcessor.Object)
+                .AddMassTransitInstrumentation()
+                .Build())
+            {
+                var harness = new InMemoryTestHarness();
+                var consumerHarness = harness.Consumer<TestConsumer>();
+                var handlerHarness = harness.Handler<TestMessage>();
+                await harness.Start();
+                try
+                {
+                    await harness.InputQueueSendEndpoint.Send<TestMessage>(new
+                    {
+                        Text = "Hello, world!",
+                    });
+
+                    Assert.True(await harness.Consumed.SelectAsync<TestMessage>().Any());
+                    Assert.True(await consumerHarness.Consumed.SelectAsync<TestMessage>().Any());
+                    Assert.True(await handlerHarness.Consumed.SelectAsync().Any());
+                }
+                finally
+                {
+                    await harness.Stop();
+                }
+            }
+
+            Assert.Equal(10, activityProcessor.Invocations.Count);
+
+            var sends = this.GetActivitiesFromInvocationsByOperationName(activityProcessor.Invocations, OperationName.Transport.Send);
+            var receives = this.GetActivitiesFromInvocationsByOperationName(activityProcessor.Invocations, OperationName.Transport.Receive);
+            var consumes = this.GetActivitiesFromInvocationsByOperationName(activityProcessor.Invocations, OperationName.Consumer.Consume);
+            var handles = this.GetActivitiesFromInvocationsByOperationName(activityProcessor.Invocations, OperationName.Consumer.Handle);
+
+            foreach (var activity in sends)
+            {
+                Assert.Equal("SEND /input_queue", activity.DisplayName);
+            }
+
+            foreach (var activity in receives)
+            {
+                Assert.Equal("RECV /input_queue", activity.DisplayName);
+            }
+
+            foreach (var activity in consumes)
+            {
+                Assert.Equal("CONSUME OpenTelemetry.Contrib.Instrumentation.MassTransit.Tests.TestConsumer", activity.DisplayName);
+            }
+
+            foreach (var activity in handles)
+            {
+                Assert.Equal("HANDLE TestMessage/OpenTelemetry.Contrib.Instrumentation.MassTransit.Tests", activity.DisplayName);
+            }
+        }
+
+        [Fact]
+        public async Task MassTransitInstrumentationTestOptions()
+        {
+            using Activity activity = new Activity("Parent");
+            activity.SetParentId(
+                ActivityTraceId.CreateRandom(),
+                ActivitySpanId.CreateRandom(),
+                ActivityTraceFlags.Recorded);
+            activity.Start();
+
+            var activityProcessor = new Mock<ActivityProcessor>();
+            using (Sdk.CreateTracerProviderBuilder()
+                .AddProcessor(activityProcessor.Object)
+                .AddMassTransitInstrumentation(o =>
+                    o.TracedOperations = new HashSet<string>(new[] { OperationName.Consumer.Consume }))
+                .Build())
+            {
+                var harness = new InMemoryTestHarness();
+                var consumerHarness = harness.Consumer<TestConsumer>();
+                var handlerHarness = harness.Handler<TestMessage>();
+                await harness.Start();
+                try
+                {
+                    await harness.InputQueueSendEndpoint.Send<TestMessage>(new
+                    {
+                        Text = "Hello, world!",
+                    });
+
+                    Assert.True(await harness.Consumed.SelectAsync<TestMessage>().Any());
+                    Assert.True(await consumerHarness.Consumed.SelectAsync<TestMessage>().Any());
+                    Assert.True(await handlerHarness.Consumed.SelectAsync().Any());
+                }
+                finally
+                {
+                    await harness.Stop();
+                }
+            }
+
+            Assert.Equal(4, activityProcessor.Invocations.Count);
+
+            var consumes = this.GetActivitiesFromInvocationsByOperationName(activityProcessor.Invocations, OperationName.Consumer.Consume);
+
+            Assert.Equal(2, consumes.Count());
+        }
+
+        private IEnumerable<Activity> GetActivitiesFromInvocationsByOperationName(IEnumerable<IInvocation> invocations, string operationName)
+        {
+            return
+                invocations
+                    .Where(i =>
+                        i.Arguments.OfType<Activity>()
+                            .Any(a => a.OperationName == operationName))
+                    .Select(i => i.Arguments.OfType<Activity>().Single());
+        }
+    }
+}
