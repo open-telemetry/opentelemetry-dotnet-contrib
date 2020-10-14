329d3a4fdbbc355edb496c2ac2e6c4fa96c08c6b
Lu Peng <61207760+lupengamzn@users.noreply.github.com>
2020-10-14 09:51:26 -0700

    Added AWS X-Ray trace id generator and propagator. (#42)

---
+++ src/OpenTelemetry.Contrib.Extensions.AWSXRay/TracerProviderBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="TracerProviderBuilderExtensions.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using OpenTelemetry.Contrib.Extensions.AWSXRay;
+
+namespace OpenTelemetry.Trace
+{
+    /// <summary>
+    /// Extension method to generate AWS X-Ray compatible trace id and replace the trace id of root activity.
+    /// </summary>
+    public static class TracerProviderBuilderExtensions
+    {
+        /// <summary>
+        /// Replace the trace id of root activity.
+        /// </summary>
+        /// <param name="builder"><see cref="TracerProviderBuilder"/> being configured.</param>
+        /// <returns>The instance of <see cref="TracerProviderBuilder"/>.</returns>
+        public static TracerProviderBuilder AddXRayActivityTraceIdGenerator(this TracerProviderBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            AWSXRayIdGenerator.ReplaceTraceId();
+            return builder;
+        }
+    }
+}
