f34b0f6d34d97dd59f5a3febd76a8ddb83a24b17
Mikel Blanchard <mblanchard@macrosssoftware.com>
2021-02-20 22:07:31 -0800

    Code review.

--- examples/wcf/client-core/Program.cs
+++ examples/wcf/client-core/Program.cs
@@ -55,6 +55,9 @@ namespace Examples.Wcf.Client
 
         private static async Task CallService(Binding binding, EndpointAddress remoteAddress)
         {
+            // Note: Best practice is to re-use your client/channel instances.
+            // This code is not meant to illustrate best practics, only the
+            // instrumentation.
             StatusServiceClient client = new StatusServiceClient(binding, remoteAddress);
             client.Endpoint.EndpointBehaviors.Add(new TelemetryEndpointBehavior());
             try
