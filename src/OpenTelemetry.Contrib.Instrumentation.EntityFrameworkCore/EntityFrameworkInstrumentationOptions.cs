a4c9d8cb92f0b23379663bc8c012d0a6192268f7
alexvaluyskiy <alex.valuiskyi@gmail.com>
2021-03-12 22:46:23 +0200

    fix defaults

--- src/OpenTelemetry.Contrib.Instrumentation.EntityFrameworkCore/EntityFrameworkInstrumentationOptions.cs
+++ src/OpenTelemetry.Contrib.Instrumentation.EntityFrameworkCore/EntityFrameworkInstrumentationOptions.cs
@@ -32,6 +32,6 @@ namespace OpenTelemetry.Contrib.Instrumentation.EntityFrameworkCore
         /// <summary>
         /// Gets or sets a value indicating whether or not the <see cref="EntityFrameworkInstrumentation"/> should add the text of <see cref="CommandType.Text"/> commands as the <see cref="SemanticConventions.AttributeDbStatement"/> tag. Default value: False.
         /// </summary>
-        public bool SetDbStatementForText { get; set; }
+        public bool SetDbStatementForText { get; set; } = false;
     }
 }
