64ae7115fc1d6ba4c46a041af06147a402cd5286
Lu Peng <bolpeng@amazon.com>
2021-08-16 16:34:32 -0700

    Added resource detectors for AWS services.

---
+++ src/OpenTelemetry.Contrib.Extensions.AWSXRay/Resources/AWSEC2ResourceDetector.cs
@@ -0,0 +1,95 @@
+﻿// <copyright file="AWSEC2ResourceDetector.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Collections.Generic;
+using OpenTelemetry.Contrib.Extensions.AWSXRay.Resources.Models;
+
+namespace OpenTelemetry.Contrib.Extensions.AWSXRay.Resources
+{
+    /// <summary>
+    /// Resource detector for application running on AWS EC2 instance.
+    /// </summary>
+    public class AWSEC2ResourceDetector : IResourceDetector
+    {
+        private const string AWSEC2MetadataTokenTTLHeader = "X-aws-ec2-metadata-token-ttl-seconds";
+        private const string AWSEC2MetadataTokenHeader = "X-aws-ec2-metadata-token";
+        private const string AWSEC2MetadataTokenUrl = "http://169.254.169.254/latest/api/token";
+        private const string AWSEC2HostNameUrl = "http://169.254.169.254/latest/meta-data/hostname";
+        private const string AWSEC2IdentityDocumentUrl = "http://169.254.169.254/latest/dynamic/instance-identity/document";
+
+        /// <summary>
+        /// Detector the required and optional resource attributes from AWS EC2.
+        /// </summary>
+        /// <returns>List of key-value pairs of resource attributes.</returns>
+        public IEnumerable<KeyValuePair<string, object>> Detect()
+        {
+            List<KeyValuePair<string, object>> resourceAttributes = null;
+
+            try
+            {
+                var token = this.GetAWSEC2Token();
+                var identity = this.GetAWSEC2Identity(token);
+                var hostName = this.GetAWSEC2HostName(token);
+
+                resourceAttributes = new List<KeyValuePair<string, object>>()
+                {
+                    new KeyValuePair<string, object>(AWSSemanticConventions.AttributeCloudProvider, "aws"),
+                    new KeyValuePair<string, object>(AWSSemanticConventions.AttributeCloudPlatform, "aws_ec2"),
+                    new KeyValuePair<string, object>(AWSSemanticConventions.AttributeCloudAccountID, identity.AccountId),
+                    new KeyValuePair<string, object>(AWSSemanticConventions.AttributeCloudAvailableZone, identity.AvailabilityZone),
+                    new KeyValuePair<string, object>(AWSSemanticConventions.AttributeHostID, identity.InstanceId),
+                    new KeyValuePair<string, object>(AWSSemanticConventions.AttributeHostType, identity.InstanceType),
+                    new KeyValuePair<string, object>(AWSSemanticConventions.AttributeCloudRegion, identity.Region),
+                    new KeyValuePair<string, object>(AWSSemanticConventions.AttributeHostName, hostName),
+                };
+            }
+            catch
+            {
+                // TODO: add logging for exception
+            }
+
+            return resourceAttributes;
+        }
+
+        private string GetAWSEC2Token()
+        {
+            return ResourceDetectorUtils.SendOutRequest(AWSEC2MetadataTokenUrl, "PUT", new KeyValuePair<string, string>(AWSEC2MetadataTokenTTLHeader, "60")).Result;
+        }
+
+        private AWSEC2IdentityDocumentModel GetAWSEC2Identity(string token)
+        {
+            var identity = this.GetIdentityResponse(token);
+            var identityDocument = this.DeserializeResponse(identity);
+
+            return identityDocument;
+        }
+
+        private string GetIdentityResponse(string token)
+        {
+            return ResourceDetectorUtils.SendOutRequest(AWSEC2IdentityDocumentUrl, "GET", new KeyValuePair<string, string>(AWSEC2MetadataTokenHeader, token)).Result;
+        }
+
+        private AWSEC2IdentityDocumentModel DeserializeResponse(string response)
+        {
+            return ResourceDetectorUtils.DeserializeFromString<AWSEC2IdentityDocumentModel>(response);
+        }
+
+        private string GetAWSEC2HostName(string token)
+        {
+            return ResourceDetectorUtils.SendOutRequest(AWSEC2HostNameUrl, "GET", new KeyValuePair<string, string>(AWSEC2MetadataTokenHeader, token)).Result;
+        }
+    }
+}
---
+++ src/OpenTelemetry.Contrib.Extensions.AWSXRay/Resources/AWSECSResourceDetector.cs
@@ -0,0 +1,90 @@
+﻿// <copyright file="AWSECSResourceDetector.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+#if NETSTANDARD
+using System;
+using System.Collections.Generic;
+
+namespace OpenTelemetry.Contrib.Extensions.AWSXRay.Resources
+{
+    /// <summary>
+    /// Resource detector for application running in AWS ECS.
+    /// </summary>
+    public class AWSECSResourceDetector : IResourceDetector
+    {
+        private const string AWSECSMetadataPath = "/proc/self/cgroup";
+        private const string AWSECSMetadataURLKey = "ECS_CONTAINER_METADATA_URI";
+        private const string AWSECSMetadataURLV4Key = "ECS_CONTAINER_METADATA_URI_V4";
+
+        /// <summary>
+        /// Detector the required and optional resource attributes from AWS ECS.
+        /// </summary>
+        /// <returns>List of key-value pairs of resource attributes.</returns>
+        public IEnumerable<KeyValuePair<string, object>> Detect()
+        {
+            List<KeyValuePair<string, object>> resourceAttributes = null;
+
+            if (!this.IsECSProcess())
+            {
+                return resourceAttributes;
+            }
+
+            try
+            {
+                var containerId = this.GetECSContainerId();
+
+                resourceAttributes = new List<KeyValuePair<string, object>>()
+                {
+                    new KeyValuePair<string, object>(AWSSemanticConventions.AttributeCloudProvider, "aws"),
+                    new KeyValuePair<string, object>(AWSSemanticConventions.AttributeCloudPlatform, "aws_ecs"),
+                    new KeyValuePair<string, object>(AWSSemanticConventions.AttributeContainerID, containerId),
+                };
+            }
+            catch
+            {
+                // TODO: add logging for exception
+            }
+
+            return resourceAttributes;
+        }
+
+        private string GetECSContainerId()
+        {
+            string containerId = null;
+
+            using (var streamReader = ResourceDetectorUtils.GetStreamReader(AWSECSMetadataPath))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    var trimmedLine = streamReader.ReadLine().Trim();
+                    if (trimmedLine.Length > 64)
+                    {
+                        containerId = trimmedLine.Substring(trimmedLine.Length - 64);
+                        return containerId;
+                    }
+                }
+            }
+
+            return containerId;
+        }
+
+        private bool IsECSProcess()
+        {
+            return Environment.GetEnvironmentVariable(AWSECSMetadataURLKey) != null || Environment.GetEnvironmentVariable(AWSECSMetadataURLV4Key) != null;
+        }
+    }
+}
+#endif
