d6575d1547ae6bf45ee62df343d5ded956bbc1cb
tamilanban <anbantamizh@gmail.com>
2022-01-05 13:07:03 +0530

    action is assigned with null if operation name is not defined(#169)

--- src/OpenTelemetry.Contrib.Instrumentation.Wcf/TelemetryClientMessageInspector.cs
+++ src/OpenTelemetry.Contrib.Instrumentation.Wcf/TelemetryClientMessageInspector.cs
@@ -68,7 +68,7 @@ namespace OpenTelemetry.Contrib.Instrumentation.Wcf
 
             if (activity != null)
             {
-                string action = request.Headers.Action;
+                string action = request.Headers.Action ?? string.Empty;
                 activity.DisplayName = action;
 
                 Propagators.DefaultTextMapPropagator.Inject(
