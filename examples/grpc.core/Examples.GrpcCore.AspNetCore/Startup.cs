38fd4525925b94be0e7889a6250bf6215771cf7e
Utkarsh Umesan Pillai <utpilla@microsoft.com>
2022-03-18 15:28:35 -0700

    Remove "Contrib" from Grpc instrumentation (#255)
    * Rename GRPC instrumentation

--- examples/grpc.core/Examples.GrpcCore.AspNetCore/Startup.cs
+++ examples/grpc.core/Examples.GrpcCore.AspNetCore/Startup.cs
@@ -23,7 +23,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OpenTelemetry.Contrib.Instrumentation.GrpcCore;
+using OpenTelemetry.Instrumentation.GrpcCore;
 using OpenTelemetry.Trace;
 
 namespace Examples.GrpcCore.AspNetCore
