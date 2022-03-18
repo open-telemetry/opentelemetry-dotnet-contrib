fba664f28449e195a1b494bcf5a812a75fce6dbc
Utkarsh Umesan Pillai <utpilla@microsoft.com>
2022-03-17 18:04:10 -0700

    Add namespace

--- examples/grpc.core/Examples.GrpcCore.AspNetCore/Startup.cs
+++ examples/grpc.core/Examples.GrpcCore.AspNetCore/Startup.cs
@@ -23,6 +23,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OpenTelemetry.Instrumentation.GrpcCore;
 using OpenTelemetry.Trace;
 
 namespace Examples.GrpcCore.AspNetCore
