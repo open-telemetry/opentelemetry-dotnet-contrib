6ee5051ebe2e8a0a42abe41440cb8ef43d6a4a28
Oleksiy Dubinin <oleksiy.dubinin@dynatrace.com>
2022-04-14 17:41:39 +0200

    Made processor's constructor public, removed static methods with predefined setup (#296).

--- src/OpenTelemetry.Extensions/Trace/AutoFlushActivityProcessor.cs
+++ src/OpenTelemetry.Extensions/Trace/AutoFlushActivityProcessor.cs
@@ -37,7 +37,7 @@ namespace OpenTelemetry.Trace
         /// <exception cref="ArgumentOutOfRangeException">
         /// Thrown when the <c>timeoutMilliseconds</c> is smaller than -1.
         /// </exception>
-        internal AutoFlushActivityProcessor(Predicate<Activity> predicate, int timeoutMilliseconds)
+        public AutoFlushActivityProcessor(Predicate<Activity> predicate, int timeoutMilliseconds)
         {
             this.predicate = predicate ?? throw new ArgumentNullException(nameof(predicate));
             if (timeoutMilliseconds < Timeout.Infinite)
@@ -47,26 +47,6 @@ namespace OpenTelemetry.Trace
 
             this.timeoutMilliseconds = timeoutMilliseconds;
         }
-
-        /// <summary>
-        /// Creates a new activity processor that synchronously flushes its containing <see cref="TracerProvider"/> if an ended
-        /// activity has no in-process parent and kind <see cref="ActivityKind.Server"/> or <see cref="ActivityKind.Consumer"/>.
-        /// Beware: add this class *after* exporter related span processors.
-        /// </summary>
-        /// <returns>A new activity processor that flushes certain local root spans with a default timeout.</returns>
-        public static AutoFlushActivityProcessor FlushAfterLocalServerSideRootSpans() => FlushAfterEndOf(
-            a => a.Parent == null && (a.Kind == ActivityKind.Server || a.Kind == ActivityKind.Consumer), 5000);
-
-        /// <summary>
-        /// Creates a new activity processor that flushes its containing <see cref="TracerProvider"/> if an ended
-        /// activity matches a predicate <paramref name="predicate"/>, using <paramref name="timeoutMilliseconds"/> as flush timeout.
-        /// Beware: add this class *after* exporter related span processors.
-        /// </summary>
-        /// <param name="predicate">Predicate that should return <c>true</c> to initiate a flush.</param>
-        /// <param name="timeoutMilliseconds">Timeout (in milliseconds) to use for flushing.</param>
-        /// <returns>A new activity processor that flushes based on <paramref name="predicate"/>.</returns>
-        public static AutoFlushActivityProcessor FlushAfterEndOf(Predicate<Activity> predicate, int timeoutMilliseconds) =>
-            new(predicate, timeoutMilliseconds);
 
         /// <inheritdoc/>
         public override void OnEnd(Activity data)
