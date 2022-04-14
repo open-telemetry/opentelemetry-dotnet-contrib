10ba52cb40520dc84986e7572e05a94008bf567d
Michael Maxwell <micmax@microsoft.com>
2022-04-14 12:00:39 -0700

    Geneva license comments (#302)

--- src/OpenTelemetry.Exporter.Geneva/ServiceProviderExtensions.cs
+++ src/OpenTelemetry.Exporter.Geneva/ServiceProviderExtensions.cs
@@ -1,4 +1,20 @@
-﻿#if NET461 || NETSTANDARD2_0 || NETSTANDARD2_1 || NETCOREAPP3_1_OR_GREATER
+﻿// <copyright file="ServiceProviderExtensions.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+#if NET461 || NETSTANDARD2_0 || NETSTANDARD2_1 || NETCOREAPP3_1_OR_GREATER
 using Microsoft.Extensions.Options;
 #endif
 
