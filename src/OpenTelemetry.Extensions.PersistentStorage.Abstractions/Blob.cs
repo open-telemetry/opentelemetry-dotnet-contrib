c197a9ff7d4c50d44c6a4bf1f955947e19d9f7e2
Vishwesh Bankwar <vibankwa@microsoft.com>
2022-04-25 13:39:14 -0700

    reoslve PR comments

---
+++ src/OpenTelemetry.Extensions.PersistentStorage.Abstractions/Blob.cs
@@ -0,0 +1,82 @@
+﻿// <copyright file="Blob.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+
+namespace OpenTelemetry.Extensions.PersistentStorage.Abstractions
+{
+    /// <summary>
+    /// Represents a persistent blob.
+    /// </summary>
+    public abstract class Blob
+    {
+        /// <summary>
+        /// Reads the content from the blob.
+        /// </summary>
+        /// <param name="buffer">
+        /// The content to be read.
+        /// </param>
+        /// <returns>
+        /// true if read was successful or else false.
+        /// </returns>
+        /// <remarks>
+        /// Note to implementers: This function should never throw exception.
+        /// </remarks>
+        public abstract bool TryRead(out byte[] buffer);
+
+        /// <summary>
+        /// Attempts to write the given content to the blob.
+        /// </summary>
+        /// <param name="buffer">
+        /// The content to be written.
+        /// </param>
+        /// <param name="leasePeriodMilliseconds">
+        /// The number of milliseconds to lease after the write operation finished.
+        /// </param>
+        /// <returns>
+        /// true if the write operation succeeded, otherwise false.
+        /// </returns>
+        /// <remarks>
+        /// Note to implementers: This function should never throw exception.
+        /// </remarks>
+        public abstract bool TryWrite(byte[] buffer, int leasePeriodMilliseconds = 0);
+
+        /// <summary>
+        /// Attempts to acquire lease on the blob.
+        /// </summary>
+        /// <param name="leasePeriodMilliseconds">
+        /// The number of milliseconds to lease.
+        /// </param>
+        /// <returns>
+        /// true if lease is acquired or else false.
+        /// </returns>
+        /// <remarks>
+        /// Note to implementers: This function should never throw exception.
+        /// </remarks>
+        public abstract bool TryLease(int leasePeriodMilliseconds);
+
+        /// <summary>
+        /// Attempts to delete the blob.
+        /// </summary>
+        /// <returns>
+        /// true if delete was successful else false.
+        /// </returns>
+        /// <remarks>
+        /// Note to implementers: This function should never throw exception.
+        /// </remarks>
+        public abstract bool TryDelete();
+    }
+}
--- src/OpenTelemetry.Extensions.PersistentStorage.Abstractions/CHANGELOG.md
+++ src/OpenTelemetry.Extensions.PersistentStorage.Abstractions/CHANGELOG.md
@@ -2,7 +2,7 @@
 
 ## 1.0.0-alpha1
 
-This is the first release for the `OpenTelemetry.Contrib.Extensions.PersistentStorage.Abstractions`
+This is the first release for the `OpenTelemetry.Extensions.PersistentStorage.Abstractions`
 project.
 
 For more details, please refer to the
