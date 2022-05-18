c87eef630ea2d5b0b23c3dc1d76b7b72f29f104c
hananiel <hananiel@gmail.com>
2022-05-18 11:02:22 -0400

    Create a EventCounter Listener that subscribes to all event counters

---
+++ src/OpenTelemetry.Instrumentation.EventCounters/README.md
@@ -0,0 +1,53 @@
+﻿# Event counter instrumentation for OpenTelemetry
+
+This is an
+[Instrumentation Library](https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/glossary.md#instrumentation-library),
+which republishes EventCounters using Metrics Api.
+
+## Steps to enable OpenTelemetry.Instrumentation.EventCounters
+
+### Step 1: Install Package
+
+Add a reference to the
+[`OpenTelemetry.Contrib.Instrumentation.EventCounters`](https://www.nuget.org/packages/OpenTelemetry.Contrib.Instrumentation.EventCounters)
+package. Also, add any other instrumentations & exporters you will need.
+
+```shell
+dotnet add package OpenTelemetry.Instrumentation.EventCounters
+```
+
+### Step 2: Enable EventCounters Instrumentation at application startup
+
+EventCounters instrumentation must be enabled at application startup.
+
+The following example demonstrates adding EventCounter events to a
+console application. This example also sets up the OpenTelemetry Console
+exporter, which requires adding the package
+[`OpenTelemetry.Exporter.Console`](https://www.nuget.org/packages/OpenTelemetry.Exporter.Console)
+to the application.
+
+```csharp
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Instrumentation.EventCounters;
+
+namespace DotnetMetrics;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        using var meterprovider = Sdk.CreateMeterProviderBuilder()
+                .AddEventCounters(options =>
+                {
+                   options.RefreshIntervalSecs = 55;
+                })
+                .AddConsoleExporter()
+                .Build();
+    }
+}
+```
+
+## References
+
+* [OpenTelemetry Project](https://opentelemetry.io/)
---
+++ test/OpenTelemetry.Instrumentation.EventCounters.Tests/EventCounterListenerTests.cs
@@ -0,0 +1,53 @@
+﻿// <copyright file="EventCounterListenerTests.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OpenTelemetry.Metrics;
+using Xunit;
+
+namespace OpenTelemetry.Instrumentation.EventCounters.Tests
+{
+    public class EventCounterListenerTests
+    {
+        public class EventCountersInstrumentationTests
+        {
+            private const int MaxTimeToAllowForFlush = 10000;
+            private MeterProvider meterProvider = null;
+
+            [Fact]
+            public async Task MetricsAreCaptured()
+            {
+                var metricItems = new List<Metric>();
+
+                this.meterProvider = Sdk.CreateMeterProviderBuilder()
+                     .AddEventCounterMetrics(options =>
+                     {
+                         options.RefreshIntervalSecs = 1;
+                     })
+                     .AddInMemoryExporter(metricItems)
+                    .Build();
+
+                await Task.Delay(2000);
+                this.meterProvider.ForceFlush(MaxTimeToAllowForFlush);
+
+                this.meterProvider.Dispose();
+
+                Assert.True(metricItems.Count > 1);
+            }
+        }
+    }
+}
