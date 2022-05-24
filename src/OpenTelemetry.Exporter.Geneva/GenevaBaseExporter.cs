d72b4cd007b8b590e8997e09ab6a0e0925390ab5
Yun-Ting Lin <yunl@microsoft.com>
2022-05-24 14:54:16 -0700

    PassThru TableNameMappings using the logger category name. (#345)

--- src/OpenTelemetry.Exporter.Geneva/GenevaBaseExporter.cs
+++ src/OpenTelemetry.Exporter.Geneva/GenevaBaseExporter.cs
@@ -14,6 +14,7 @@
 // limitations under the License.
 // </copyright>
 
+using System;
 using System.Collections.Generic;
 
 namespace OpenTelemetry.Exporter.Geneva;
@@ -87,6 +88,21 @@ where T : class
         }
 
         cursor = MessagePackSerializer.Serialize(buffer, cursor, value);
+        return cursor;
+    }
+
+    internal static int AddPartAField(byte[] buffer, int cursor, string name, Span<byte> value)
+    {
+        if (V40_PART_A_MAPPING.TryGetValue(name, out string replacementKey))
+        {
+            cursor = MessagePackSerializer.SerializeAsciiString(buffer, cursor, replacementKey);
+        }
+        else
+        {
+            cursor = MessagePackSerializer.SerializeUnicodeString(buffer, cursor, name);
+        }
+
+        cursor = MessagePackSerializer.SerializeSpan(buffer, cursor, value);
         return cursor;
     }
 }
