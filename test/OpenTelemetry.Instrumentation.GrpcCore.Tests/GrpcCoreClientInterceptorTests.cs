dc3e0230ed7fa594c059c5553b42bcc4b46db344
Utkarsh Umesan Pillai <utpilla@microsoft.com>
2022-05-25 18:03:20 -0700

    Fix format

--- test/OpenTelemetry.Instrumentation.GrpcCore.Tests/GrpcCoreClientInterceptorTests.cs
+++ test/OpenTelemetry.Instrumentation.GrpcCore.Tests/GrpcCoreClientInterceptorTests.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="GrpcCoreClientInterceptorTests.cs" company="OpenTelemetry Authors">
+// <copyright file="GrpcCoreClientInterceptorTests.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
