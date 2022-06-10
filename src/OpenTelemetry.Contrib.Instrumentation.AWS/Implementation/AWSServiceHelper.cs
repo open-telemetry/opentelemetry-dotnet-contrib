d0931bfc50230f4242a94aec2f2c5db9b0cae24b
Oleksiy Dubinin <oleksiy.dubinin@dynatrace.com>
2022-06-10 14:55:15 +0200

    Added db.system attribute to the AWS SDK instrumentation (#415)

---
+++ src/OpenTelemetry.Contrib.Instrumentation.AWS/Implementation/AWSServiceHelper.cs
@@ -0,0 +1,38 @@
+﻿using Amazon.Runtime;
+using System;
+using System.Collections.Generic;
+
+namespace OpenTelemetry.Contrib.Instrumentation.AWS.Implementation
+{
+    internal class AWSServiceHelper
+    {
+        private const string DynamoDbService = "DynamoDBv2";
+        private const string SQSService = "SQS";
+
+        internal static IReadOnlyDictionary<string, string> ServiceParameterMap = new Dictionary<string, string>()
+        {
+            { DynamoDbService, "TableName" },
+            { SQSService , "QueueUrl" },
+        };
+
+        internal static readonly IReadOnlyDictionary<string, string> ParameterAttributeMap = new Dictionary<string, string>()
+        {
+            { "TableName", AWSSemanticConventions.AttributeAWSDynamoTableName },
+            { "QueueUrl", AWSSemanticConventions.AttributeAWSSQSQueueUrl },
+        };
+
+        internal static string GetAWSServiceName(IRequestContext requestContext) 
+            => Utils.RemoveAmazonPrefixFromServiceName(requestContext.Request.ServiceName);
+
+        internal static string GetAWSOperationName(IRequestContext requestContext)
+        {
+            string completeRequestName = requestContext.OriginalRequest.GetType().Name;
+            string suffix = "Request";
+            var operationName = Utils.RemoveSuffix(completeRequestName, suffix);
+            return operationName;
+        }
+
+        internal static bool IsDbService(string service) 
+            => DynamoDbService.Equals(service, StringComparison.OrdinalIgnoreCase);
+    }
+}
