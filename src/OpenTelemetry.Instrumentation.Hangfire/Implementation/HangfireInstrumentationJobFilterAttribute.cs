1b27e4ca81954ba08357d16b0cbf2ab36e311eed
marcindruzgala <100698932+marcindruzgala@users.noreply.github.com>
2022-06-13 15:17:31 +0200

    create parent context if traceparent is available

--- src/OpenTelemetry.Instrumentation.Hangfire/Implementation/HangfireInstrumentationJobFilterAttribute.cs
+++ src/OpenTelemetry.Instrumentation.Hangfire/Implementation/HangfireInstrumentationJobFilterAttribute.cs
@@ -14,13 +14,15 @@
 // limitations under the License.
 // </copyright>
 
+using Hangfire.Client;
+
 namespace OpenTelemetry.Instrumentation.Hangfire.Implementation
 {
     using System.Diagnostics;
     using global::Hangfire.Common;
     using global::Hangfire.Server;
 
-    internal class HangfireInstrumentationJobFilterAttribute : JobFilterAttribute, IServerFilter
+    internal class HangfireInstrumentationJobFilterAttribute : JobFilterAttribute, IServerFilter, IClientFilter
     {
         public void OnPerforming(PerformingContext performingContext)
         {
@@ -30,8 +32,12 @@ namespace OpenTelemetry.Instrumentation.Hangfire.Implementation
                 return;
             }
 
+            var traceParent = performingContext.GetJobParameter<string>(HangfireInstrumentationConstants.TraceParentKey);
+            var traceState = performingContext.GetJobParameter<string>(HangfireInstrumentationConstants.TraceStateKey);
+            ActivityContext.TryParse(traceParent, traceState, out var parentContext);
+
             var activity = HangfireInstrumentation.ActivitySource
-                .StartActivity(HangfireInstrumentationConstants.ActivityName, ActivityKind.Internal, parentContext: default);
+                .StartActivity(HangfireInstrumentationConstants.ActivityName, ActivityKind.Internal, parentContext: parentContext);
 
             if (activity != null)
             {
@@ -64,6 +70,25 @@ namespace OpenTelemetry.Instrumentation.Hangfire.Implementation
 
                 activity.Dispose();
             }
+        }
+
+        public void OnCreating(CreatingContext creatingContext)
+        {
+            // Short-circuit if nobody is listening
+            if (!HangfireInstrumentation.ActivitySource.HasListeners() || Activity.Current?.Id is null)
+            {
+                return;
+            }
+
+            creatingContext.SetJobParameter(HangfireInstrumentationConstants.TraceParentKey, Activity.Current.Id);
+            if (!string.IsNullOrWhiteSpace(Activity.Current.TraceStateString))
+            {
+                creatingContext.SetJobParameter(HangfireInstrumentationConstants.TraceStateKey, Activity.Current.TraceStateString);
+            }
+        }
+
+        public void OnCreated(CreatedContext filterContext)
+        {
         }
     }
 }
