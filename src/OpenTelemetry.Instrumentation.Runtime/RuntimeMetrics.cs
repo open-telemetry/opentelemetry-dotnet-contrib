0edfffc4ac4d3819c3dc30a6e516dd12c9ddf1c2
Reiley Yang <reyang@microsoft.com>
2022-06-24 17:45:49 -0700

    Update src/OpenTelemetry.Instrumentation.Runtime/RuntimeMetrics.cs
    Co-authored-by: Noah Falk <noahfalk@users.noreply.github.com>

--- src/OpenTelemetry.Instrumentation.Runtime/RuntimeMetrics.cs
+++ src/OpenTelemetry.Instrumentation.Runtime/RuntimeMetrics.cs
@@ -73,7 +73,7 @@ namespace OpenTelemetry.Instrumentation.Runtime
             if (options.IsJitEnabled)
             {
                 this.meter.CreateObservableCounter($"{metricPrefix}jit.il_compiled.size", () => System.Runtime.JitInfo.GetCompiledILBytes(), unit: "By", description: "Count of bytes of intermediate language that have been compiled since the process start.");
-                this.meter.CreateObservableCounter($"{metricPrefix}jit.methods_compiled", () => System.Runtime.JitInfo.GetCompiledMethodCount(), description: "Count of methods that have been compiled since the process start.");
+                this.meter.CreateObservableCounter($"{metricPrefix}jit.methods_compiled.count", () => System.Runtime.JitInfo.GetCompiledMethodCount(), description: "The number of times the JIT compiler was invoked to compile a method since the process start. The JIT compiler may be invoked multiple times for the same method to compile with different generic parameters, or because tiered compilation requested different optimization settings.");
                 this.meter.CreateObservableCounter($"{metricPrefix}jit.compilation_time", () => System.Runtime.JitInfo.GetCompilationTime().Ticks * NanosecondsPerTick, unit: "ns", description: "The amount of time the JIT compiler has spent compiling methods since the process start.");
             }
 #endif
