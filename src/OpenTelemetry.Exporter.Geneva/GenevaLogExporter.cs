8a933c102c5dcf7256960f312f352a5afae790e1
Microsoft-NiGoel <nigoel@microsoft.com>
2022-07-11 08:57:47 -0700

    Removed unnecessary using statement and fixed header comment.

--- src/OpenTelemetry.Exporter.Geneva/GenevaLogExporter.cs
+++ src/OpenTelemetry.Exporter.Geneva/GenevaLogExporter.cs
@@ -18,7 +18,6 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Net;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
