9a2c685eabf6512a2df918ace928b666695ffb3f
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-08-04 08:23:28 +0200

    [WCF.Intrumentation] Filescoped namespace

--- src/OpenTelemetry.Instrumentation.Wcf/TracerProviderBuilderExtensions.cs
+++ src/OpenTelemetry.Instrumentation.Wcf/TracerProviderBuilderExtensions.cs
@@ -18,34 +18,33 @@ using System;
 using OpenTelemetry.Instrumentation.Wcf;
 using OpenTelemetry.Internal;
 
-namespace OpenTelemetry.Trace
+namespace OpenTelemetry.Trace;
+
+/// <summary>
+/// Extension methods to simplify registering of dependency instrumentation.
+/// </summary>
+public static class TracerProviderBuilderExtensions
 {
     /// <summary>
-    /// Extension methods to simplify registering of dependency instrumentation.
+    /// Enables the outgoing requests automatic data collection for WCF.
     /// </summary>
-    public static class TracerProviderBuilderExtensions
+    /// <param name="builder"><see cref="TracerProviderBuilderExtensions"/> being configured.</param>
+    /// <param name="configure">Wcf configuration options.</param>
+    /// <returns>The instance of <see cref="TracerProviderBuilderExtensions"/> to chain the calls.</returns>
+    public static TracerProviderBuilder AddWcfInstrumentation(this TracerProviderBuilder builder, Action<WcfInstrumentationOptions> configure = null)
     {
-        /// <summary>
-        /// Enables the outgoing requests automatic data collection for WCF.
-        /// </summary>
-        /// <param name="builder"><see cref="TracerProviderBuilderExtensions"/> being configured.</param>
-        /// <param name="configure">Wcf configuration options.</param>
-        /// <returns>The instance of <see cref="TracerProviderBuilderExtensions"/> to chain the calls.</returns>
-        public static TracerProviderBuilder AddWcfInstrumentation(this TracerProviderBuilder builder, Action<WcfInstrumentationOptions> configure = null)
+        Guard.ThrowIfNull(builder);
+
+        if (WcfInstrumentationActivitySource.Options != null)
         {
-            Guard.ThrowIfNull(builder);
-
-            if (WcfInstrumentationActivitySource.Options != null)
-            {
-                throw new NotSupportedException("WCF instrumentation has already been registered and doesn't support multiple registrations.");
-            }
-
-            var options = new WcfInstrumentationOptions();
-            configure?.Invoke(options);
-
-            WcfInstrumentationActivitySource.Options = options;
-
-            return builder.AddSource(WcfInstrumentationActivitySource.ActivitySourceName);
+            throw new NotSupportedException("WCF instrumentation has already been registered and doesn't support multiple registrations.");
         }
+
+        var options = new WcfInstrumentationOptions();
+        configure?.Invoke(options);
+
+        WcfInstrumentationActivitySource.Options = options;
+
+        return builder.AddSource(WcfInstrumentationActivitySource.ActivitySourceName);
     }
 }
--- test/OpenTelemetry.Instrumentation.Wcf.Tests/TelemetryDispatchMessageInspectorForOneWayOperationsTests.netfx.cs
+++ test/OpenTelemetry.Instrumentation.Wcf.Tests/TelemetryDispatchMessageInspectorForOneWayOperationsTests.netfx.cs
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // </copyright>
+
 #if NETFRAMEWORK
 using System;
 using System.Collections.Generic;
@@ -25,134 +26,134 @@ using OpenTelemetry.Trace;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace OpenTelemetry.Instrumentation.Wcf.Tests
+namespace OpenTelemetry.Instrumentation.Wcf.Tests;
+
+[Collection("WCF")]
+public class TelemetryDispatchMessageInspectorForOneWayOperationsTests : IDisposable
 {
-    [Collection("WCF")]
-    public class TelemetryDispatchMessageInspectorForOneWayOperationsTests : IDisposable
+    private readonly ITestOutputHelper output;
+    private readonly Uri serviceBaseUri;
+    private readonly ServiceHost serviceHost;
+
+    private readonly EventWaitHandle thrownExceptionsHandle = new EventWaitHandle(false, EventResetMode.ManualReset);
+    private readonly List<Exception> thrownExceptions = new List<Exception>();
+
+    public TelemetryDispatchMessageInspectorForOneWayOperationsTests(ITestOutputHelper outputHelper)
     {
-        private readonly ITestOutputHelper output;
-        private readonly Uri serviceBaseUri;
-        private readonly ServiceHost serviceHost;
+        this.output = outputHelper;
 
-        private readonly EventWaitHandle thrownExceptionsHandle = new EventWaitHandle(false, EventResetMode.ManualReset);
-        private readonly List<Exception> thrownExceptions = new List<Exception>();
-
-        public TelemetryDispatchMessageInspectorForOneWayOperationsTests(ITestOutputHelper outputHelper)
+        Random random = new Random();
+        var retryCount = 5;
+        while (retryCount > 0)
         {
-            this.output = outputHelper;
-
-            Random random = new Random();
-            var retryCount = 5;
-            while (retryCount > 0)
-            {
-                try
-                {
-                    this.serviceBaseUri = new Uri($"net.tcp://localhost:{random.Next(2000, 5000)}/");
-                    this.serviceHost = new ServiceHost(new Service(), this.serviceBaseUri);
-
-                    var endpoint = this.serviceHost.AddServiceEndpoint(
-                        typeof(IServiceContract),
-                        new NetTcpBinding(),
-                        "/Service");
-                    endpoint.Behaviors.Add(new TelemetryEndpointBehavior());
-
-                    this.serviceHost.Description.Behaviors.Add(
-                        new ErrorHandlerServiceBehavior(this.thrownExceptionsHandle, ex => this.thrownExceptions.Add(ex)));
-
-                    this.serviceHost.Open();
-
-                    break;
-                }
-                catch (Exception ex)
-                {
-                    this.output.WriteLine(ex.ToString());
-                    if (this.serviceHost.State == CommunicationState.Faulted)
-                    {
-                        this.serviceHost.Abort();
-                    }
-                    else
-                    {
-                        this.serviceHost.Close();
-                    }
-
-                    this.serviceHost = null;
-                    retryCount--;
-                }
-            }
-
-            if (this.serviceHost == null)
-            {
-                throw new InvalidOperationException("ServiceHost could not be started.");
-            }
-        }
-
-        public void Dispose()
-        {
-            this.serviceHost?.Close();
-            this.thrownExceptionsHandle?.Dispose();
-        }
-
-        [Fact]
-        public void IncomingRequestOneWayOperationInstrumentationTest()
-        {
-            List<Activity> stoppedActivities = new List<Activity>();
-
-            using ActivityListener activityListener = new ActivityListener
-            {
-                ShouldListenTo = activitySource => true,
-                ActivityStopped = activity => stoppedActivities.Add(activity),
-            };
-
-            ActivitySource.AddActivityListener(activityListener);
-            TracerProvider tracerProvider = Sdk.CreateTracerProviderBuilder()
-                    .AddWcfInstrumentation()
-                    .Build();
-
-            ServiceClient client = new ServiceClient(
-               new NetTcpBinding(),
-               new EndpointAddress(new Uri(this.serviceBaseUri, "/Service")));
-
             try
             {
-                client.ExecuteWithOneWay(new ServiceRequest());
-                this.thrownExceptionsHandle.WaitOne(3000);
+                this.serviceBaseUri = new Uri($"net.tcp://localhost:{random.Next(2000, 5000)}/");
+                this.serviceHost = new ServiceHost(new Service(), this.serviceBaseUri);
+
+                var endpoint = this.serviceHost.AddServiceEndpoint(
+                    typeof(IServiceContract),
+                    new NetTcpBinding(),
+                    "/Service");
+                endpoint.Behaviors.Add(new TelemetryEndpointBehavior());
+
+                this.serviceHost.Description.Behaviors.Add(
+                    new ErrorHandlerServiceBehavior(this.thrownExceptionsHandle, ex => this.thrownExceptions.Add(ex)));
+
+                this.serviceHost.Open();
+
+                break;
             }
-            finally
+            catch (Exception ex)
             {
-                if (client.State == CommunicationState.Faulted)
+                this.output.WriteLine(ex.ToString());
+                if (this.serviceHost.State == CommunicationState.Faulted)
                 {
-                    client.Abort();
+                    this.serviceHost.Abort();
                 }
                 else
                 {
-                    client.Close();
+                    this.serviceHost.Close();
                 }
 
-                tracerProvider?.Shutdown();
-                tracerProvider?.Dispose();
-
-                WcfInstrumentationActivitySource.Options = null;
+                this.serviceHost = null;
+                retryCount--;
             }
+        }
 
-            // Assert
-            Assert.Empty(this.thrownExceptions);
-
-            Assert.NotEmpty(stoppedActivities);
-            Assert.Single(stoppedActivities);
-
-            Activity activity = stoppedActivities[0];
-            Assert.Equal("http://opentelemetry.io/Service/ExecuteWithOneWay", activity.DisplayName);
-            Assert.Equal("ExecuteWithOneWay", activity.TagObjects.FirstOrDefault(t => t.Key == WcfInstrumentationConstants.RpcMethodTag).Value);
-            Assert.DoesNotContain(activity.TagObjects, t => t.Key == WcfInstrumentationConstants.SoapReplyActionTag);
-
-            Assert.Equal(WcfInstrumentationActivitySource.IncomingRequestActivityName, activity.OperationName);
-            Assert.Equal(WcfInstrumentationConstants.WcfSystemValue, activity.TagObjects.FirstOrDefault(t => t.Key == WcfInstrumentationConstants.RpcSystemTag).Value);
-            Assert.Equal("http://opentelemetry.io/Service", activity.TagObjects.FirstOrDefault(t => t.Key == WcfInstrumentationConstants.RpcServiceTag).Value);
-            Assert.Equal(this.serviceBaseUri.Host, activity.TagObjects.FirstOrDefault(t => t.Key == WcfInstrumentationConstants.NetHostNameTag).Value);
-            Assert.Equal(this.serviceBaseUri.Port, activity.TagObjects.FirstOrDefault(t => t.Key == WcfInstrumentationConstants.NetHostPortTag).Value);
-            Assert.Equal("net.tcp", activity.TagObjects.FirstOrDefault(t => t.Key == WcfInstrumentationConstants.WcfChannelSchemeTag).Value);
-            Assert.Equal("/Service", activity.TagObjects.FirstOrDefault(t => t.Key == WcfInstrumentationConstants.WcfChannelPathTag).Value);
+        if (this.serviceHost == null)
+        {
+            throw new InvalidOperationException("ServiceHost could not be started.");
         }
     }
+
+    public void Dispose()
+    {
+        this.serviceHost?.Close();
+        this.thrownExceptionsHandle?.Dispose();
+    }
+
+    [Fact]
+    public void IncomingRequestOneWayOperationInstrumentationTest()
+    {
+        List<Activity> stoppedActivities = new List<Activity>();
+
+        using ActivityListener activityListener = new ActivityListener
+        {
+            ShouldListenTo = activitySource => true,
+            ActivityStopped = activity => stoppedActivities.Add(activity),
+        };
+
+        ActivitySource.AddActivityListener(activityListener);
+        TracerProvider tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddWcfInstrumentation()
+            .Build();
+
+        ServiceClient client = new ServiceClient(
+            new NetTcpBinding(),
+            new EndpointAddress(new Uri(this.serviceBaseUri, "/Service")));
+
+        try
+        {
+            client.ExecuteWithOneWay(new ServiceRequest());
+            this.thrownExceptionsHandle.WaitOne(3000);
+        }
+        finally
+        {
+            if (client.State == CommunicationState.Faulted)
+            {
+                client.Abort();
+            }
+            else
+            {
+                client.Close();
+            }
+
+            tracerProvider?.Shutdown();
+            tracerProvider?.Dispose();
+
+            WcfInstrumentationActivitySource.Options = null;
+        }
+
+        // Assert
+        Assert.Empty(this.thrownExceptions);
+
+        Assert.NotEmpty(stoppedActivities);
+        Assert.Single(stoppedActivities);
+
+        Activity activity = stoppedActivities[0];
+        Assert.Equal("http://opentelemetry.io/Service/ExecuteWithOneWay", activity.DisplayName);
+        Assert.Equal("ExecuteWithOneWay", activity.TagObjects.FirstOrDefault(t => t.Key == WcfInstrumentationConstants.RpcMethodTag).Value);
+        Assert.DoesNotContain(activity.TagObjects, t => t.Key == WcfInstrumentationConstants.SoapReplyActionTag);
+
+        Assert.Equal(WcfInstrumentationActivitySource.IncomingRequestActivityName, activity.OperationName);
+        Assert.Equal(WcfInstrumentationConstants.WcfSystemValue, activity.TagObjects.FirstOrDefault(t => t.Key == WcfInstrumentationConstants.RpcSystemTag).Value);
+        Assert.Equal("http://opentelemetry.io/Service", activity.TagObjects.FirstOrDefault(t => t.Key == WcfInstrumentationConstants.RpcServiceTag).Value);
+        Assert.Equal(this.serviceBaseUri.Host, activity.TagObjects.FirstOrDefault(t => t.Key == WcfInstrumentationConstants.NetHostNameTag).Value);
+        Assert.Equal(this.serviceBaseUri.Port, activity.TagObjects.FirstOrDefault(t => t.Key == WcfInstrumentationConstants.NetHostPortTag).Value);
+        Assert.Equal("net.tcp", activity.TagObjects.FirstOrDefault(t => t.Key == WcfInstrumentationConstants.WcfChannelSchemeTag).Value);
+        Assert.Equal("/Service", activity.TagObjects.FirstOrDefault(t => t.Key == WcfInstrumentationConstants.WcfChannelPathTag).Value);
+    }
 }
+
 #endif
