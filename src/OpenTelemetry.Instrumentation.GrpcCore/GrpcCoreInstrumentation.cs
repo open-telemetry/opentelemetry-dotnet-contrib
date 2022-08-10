e59599a259391275294b73bb8318afb199c9b1a6
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-08-10 09:27:23 +0200

    [Instrumenation.GrpcCore] File scoped namespace

--- src/OpenTelemetry.Instrumentation.GrpcCore/GrpcCoreInstrumentation.cs
+++ src/OpenTelemetry.Instrumentation.GrpcCore/GrpcCoreInstrumentation.cs
@@ -14,35 +14,34 @@
 // limitations under the License.
 // </copyright>
 
-namespace OpenTelemetry.Instrumentation.GrpcCore
+using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace OpenTelemetry.Instrumentation.GrpcCore;
+
+/// <summary>
+/// Instrumentation class Grpc.Core.
+/// </summary>
+internal static class GrpcCoreInstrumentation
 {
-    using System;
-    using System.Diagnostics;
-    using System.Reflection;
+    /// <summary>
+    /// The assembly name.
+    /// </summary>
+    internal static readonly AssemblyName AssemblyName = typeof(GrpcCoreInstrumentation).Assembly.GetName();
 
     /// <summary>
-    /// Instrumentation class Grpc.Core.
+    /// The activity source name.
     /// </summary>
-    internal static class GrpcCoreInstrumentation
-    {
-        /// <summary>
-        /// The assembly name.
-        /// </summary>
-        internal static readonly AssemblyName AssemblyName = typeof(GrpcCoreInstrumentation).Assembly.GetName();
+    internal static readonly string ActivitySourceName = AssemblyName.Name;
 
-        /// <summary>
-        /// The activity source name.
-        /// </summary>
-        internal static readonly string ActivitySourceName = AssemblyName.Name;
+    /// <summary>
+    /// The version.
+    /// </summary>
+    internal static readonly Version Version = AssemblyName.Version;
 
-        /// <summary>
-        /// The version.
-        /// </summary>
-        internal static readonly Version Version = AssemblyName.Version;
-
-        /// <summary>
-        /// The activity source.
-        /// </summary>
-        internal static readonly ActivitySource ActivitySource = new ActivitySource(ActivitySourceName, Version.ToString());
-    }
+    /// <summary>
+    /// The activity source.
+    /// </summary>
+    internal static readonly ActivitySource ActivitySource = new ActivitySource(ActivitySourceName, Version.ToString());
 }
