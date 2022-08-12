d1ab41db8e1e968207652d3eb0041a76b39ec6b1
Utkarsh Umesan Pillai <utpilla@microsoft.com>
2022-08-11 17:11:55 -0700

    Update Benchmark results

--- test/OpenTelemetry.Exporter.Geneva.Benchmark/Exporter/TLDTraceExporterBenchmarks.cs
+++ test/OpenTelemetry.Exporter.Geneva.Benchmark/Exporter/TLDTraceExporterBenchmarks.cs
@@ -19,33 +19,51 @@ using System.Diagnostics;
 using BenchmarkDotNet.Attributes;
 using OpenTelemetry.Trace;
 
+/*
+BenchmarkDotNet=v0.13.1, OS=Windows 10.0.22000
+Intel Core i7-9700 CPU 3.00GHz, 1 CPU, 8 logical and 8 physical cores
+.NET SDK=7.0.100-preview.6.22352.1
+  [Host]     : .NET 6.0.8 (6.0.822.36306), X64 RyuJIT
+  DefaultJob : .NET 6.0.8 (6.0.822.36306), X64 RyuJIT
+
+
+|            Method |      Mean |     Error |    StdDev |  Gen 0 | Allocated |
+|------------------ |----------:|----------:|----------:|-------:|----------:|
+|    ExportActivity | 11.204 us | 0.2211 us | 0.4759 us | 0.1526 |   1,016 B |
+| SerializeActivity |  1.310 us | 0.0258 us | 0.0371 us | 0.0210 |     136 B |
+*/
+
 namespace OpenTelemetry.Exporter.Geneva.Benchmark.Exporter
 {
     [MemoryDiagnoser]
     public class TLDTraceExporterBenchmarks
     {
-        private static readonly ActivitySource source = new("DemoSource");
-
-        private readonly TracerProvider tracerProvider;
+        private readonly Activity activity;
         private readonly TLDTraceExporter exporter;
-        private readonly Activity testActivity;
+        private readonly ActivitySource sourceTestData = new ActivitySource("OpenTelemetry.Exporter.Geneva.Benchmark.TestData");
+        private readonly ActivitySource activitySource = new ActivitySource("OpenTelemetry.Exporter.Geneva.Benchmark");
+        private readonly TracerProvider tracerProvider;
 
         public TLDTraceExporterBenchmarks()
         {
-            this.tracerProvider = Sdk.CreateTracerProviderBuilder()
-            .SetSampler(new AlwaysOnSampler())
-            .AddSource(source.Name)
-            .AddTLDTraceExporter(options =>
+            Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+
+            ActivitySource.AddActivityListener(new ActivityListener
             {
-                options.ConnectionString = "EtwSession=OpenTelemetry";
-                options.PrepopulatedFields = new Dictionary<string, object>
-                {
-                    ["cloud.role"] = "BusyWorker",
-                    ["cloud.roleInstance"] = "CY1SCH030021417",
-                    ["cloud.roleVer"] = "9.0.15289.2",
-                };
-            })
-            .Build();
+                ActivityStarted = null,
+                ActivityStopped = null,
+                ShouldListenTo = (activitySource) => activitySource.Name == this.sourceTestData.Name,
+                Sample = (ref ActivityCreationOptions<ActivityContext> options) => ActivitySamplingResult.AllDataAndRecorded,
+            });
+
+            using (var testActivity = this.sourceTestData.StartActivity("Benchmark"))
+            {
+                this.activity = testActivity;
+                this.activity?.SetTag("foo", 1);
+                this.activity?.SetTag("bar", "Hello, World!");
+                this.activity?.SetTag("baz", new int[] { 1, 2, 3 });
+                this.activity?.SetStatus(ActivityStatusCode.Ok);
+            }
 
             this.exporter = new TLDTraceExporter(new GenevaExporterOptions()
             {
@@ -58,27 +76,43 @@ namespace OpenTelemetry.Exporter.Geneva.Benchmark.Exporter
                 },
             });
 
-            using (var activity = source.StartActivity("SayHello"))
-            {
-                activity?.SetTag("foo", 1);
-                activity?.SetTag("bar", "Hello, World!");
-                activity?.SetTag("baz", new int[] { 1, 2, 3 });
-                activity?.SetStatus(ActivityStatusCode.Ok);
-                this.testActivity = activity;
-            }
+            this.tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .SetSampler(new AlwaysOnSampler())
+                .AddSource(this.activitySource.Name)
+                .AddTLDTraceExporter(options =>
+                {
+                    options.ConnectionString = "EtwSession=OpenTelemetry";
+                    options.PrepopulatedFields = new Dictionary<string, object>
+                    {
+                        ["cloud.role"] = "BusyWorker",
+                        ["cloud.roleInstance"] = "CY1SCH030021417",
+                        ["cloud.roleVer"] = "9.0.15289.2",
+                    };
+                })
+                .Build();
         }
 
         [Benchmark]
-        public void CreateActivity()
+        public void ExportActivity()
         {
             // this activity will be created and feed into the actual Geneva exporter
-            using var activity = source.StartActivity("Benchmark");
+            using var activity = this.activitySource.StartActivity("Benchmark");
         }
 
         [Benchmark]
         public void SerializeActivity()
         {
-            this.exporter.SerializeActivity(this.testActivity);
+            this.exporter.SerializeActivity(this.activity);
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            this.activity.Dispose();
+            this.sourceTestData.Dispose();
+            this.activitySource.Dispose();
+            this.exporter.Dispose();
+            this.tracerProvider.Dispose();
         }
     }
 }
