3a6c11581d28262a90de5a17ac02600eb64f06ea
Yun-Ting <yunl@microsoft.com>
2022-08-26 13:17:15 -0700

    refresh once

--- src/OpenTelemetry.Instrumentation.Process/ProcessMetrics.cs
+++ src/OpenTelemetry.Instrumentation.Process/ProcessMetrics.cs
@@ -14,7 +14,6 @@
 // limitations under the License.
 // </copyright>
 
-using System.Collections.Generic;
 using System.Diagnostics.Metrics;
 using System.Reflection;
 using Diagnostics = System.Diagnostics;
@@ -25,29 +24,22 @@ internal class ProcessMetrics
 {
     internal static readonly AssemblyName AssemblyName = typeof(ProcessMetrics).Assembly.GetName();
     internal static readonly Meter MeterInstance = new(AssemblyName.Name, AssemblyName.Version.ToString());
-    private static readonly Diagnostics.Process CurrentProcess = Diagnostics.Process.GetCurrentProcess();
 
     static ProcessMetrics()
     {
+        InstrumentsValues values = new InstrumentsValues();
+
         // TODO: change to ObservableUpDownCounter
         MeterInstance.CreateObservableGauge(
             "process.memory.usage",
-            () =>
-            {
-                CurrentProcess.Refresh();
-                return CurrentProcess.WorkingSet64;
-            },
+            () => InstrumentsValues.GetMemoryUsage(),
             unit: "By",
             description: "The amount of physical memory in use.");
 
         // TODO: change to ObservableUpDownCounter
         MeterInstance.CreateObservableGauge(
             "process.memory.virtual",
-            () =>
-            {
-                CurrentProcess.Refresh();
-                return CurrentProcess.VirtualMemorySize64;
-            },
+            () => InstrumentsValues.GetVirtualMemoryUsage(),
             unit: "By",
             description: "The amount of committed virtual memory.");
     }
@@ -58,73 +50,25 @@ internal class ProcessMetrics
     /// <param name="options">The options to define the metrics.</param>
     public ProcessMetrics(ProcessInstrumentationOptions options)
     {
-        // TODO: change to ObservableUpDownCounter
-        // MeterInstance.CreateObservableGauge(
-        //    $"process.cpu.utilization",
-        //    () => CurrentProcess.TotalProcessorTime.TotalMilliseconds / (Environment.ProcessorCount * (DateTime.Now - CurrentProcess.StartTime).Milliseconds),
-        //    unit: "1",
-        //    description: "Difference in process.cpu.time since the last measurement, divided by the elapsed time and number of CPUs available to the process.");
-
-        // What type should be used for the label?
-        // labels
-        // state, if specified, SHOULD be one of: system, user, wait. A process SHOULD be characterized either by data points with no state labels, or only data points with state labels.
-        // IEnumberable,
-        // List<Measurement<long>>
-        // wait time = totalprocessortime - Process.PrivilegedProcessorTime - Process.userProcessorTime
-
-        if (options.ExpandOnCpuStates == true)
-        {
-           MeterInstance.CreateObservableCounter(
-           $"process.cpu.time",
-           () =>
-           {
-               Measurement<long>[] measurements = new Measurement<long>[3];
-
-               CurrentProcess.Refresh();
-               var priviledgedCpuTime = CurrentProcess.PrivilegedProcessorTime.Seconds;
-               var userCpuTime = CurrentProcess.PrivilegedProcessorTime.Seconds;
-
-               measurements[(int)CPUState.System] = new(priviledgedCpuTime, new KeyValuePair<string, object>("state", CPUState.System.ToString()));
-               measurements[(int)CPUState.User] = new(userCpuTime, new KeyValuePair<string, object>("state", CPUState.User.ToString()));
-               measurements[(int)CPUState.Wait] = new(CurrentProcess.TotalProcessorTime.Seconds - priviledgedCpuTime - userCpuTime, new KeyValuePair<string, object>("state", CPUState.Wait.ToString()));
-
-               return measurements;
-           },
-           unit: "s",
-           description: "Total CPU seconds broken down by different states.");
-        }
-        else
-        {
-           MeterInstance.CreateObservableCounter(
-           $"process.cpu.time",
-           () =>
-           {
-               CurrentProcess.Refresh();
-               return CurrentProcess.TotalProcessorTime.Seconds;
-           },
-           unit: "s",
-           description: "Total CPU seconds broken down by different states.");
-        }
-
-        // TODO: change to ObservableUpDownCounter
-        MeterInstance.CreateObservableGauge(
-            "process.memory.usage",
-            () => CurrentProcess.WorkingSet64,
-            unit: "By",
-            description: "The amount of physical memory in use.");
-
-        // TODO: change to ObservableUpDownCounter
-        MeterInstance.CreateObservableGauge(
-            "process.memory.virtual",
-            () => CurrentProcess.VirtualMemorySize64,
-            unit: "By",
-            description: "The amount of committed virtual memory.");
     }
 
-    private enum CPUState
+    private class InstrumentsValues
     {
-        System,
-        User,
-        Wait,
+        private static readonly Diagnostics.Process CurrentProcess = Diagnostics.Process.GetCurrentProcess();
+
+        public InstrumentsValues()
+        {
+            CurrentProcess.Refresh();
+        }
+
+        public static long GetMemoryUsage()
+        {
+            return CurrentProcess.WorkingSet64;
+        }
+
+        public static long GetVirtualMemoryUsage()
+        {
+            return CurrentProcess.VirtualMemorySize64;
+        }
     }
 }
