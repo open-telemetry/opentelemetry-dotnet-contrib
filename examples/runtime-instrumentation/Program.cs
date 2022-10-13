5d970e08200b2bf263d5f9f21ed4d1107d4da11d
Yun-Ting <yunl@microsoft.com>
2022-10-13 16:27:54 -0700

    revert test files

--- examples/runtime-instrumentation/Program.cs
+++ examples/runtime-instrumentation/Program.cs
@@ -22,7 +22,7 @@ public class Program
     public static void Main()
     {
         using var meterProvider = Sdk.CreateMeterProviderBuilder()
-            .AddProcessInstrumentation()
+            .AddRuntimeInstrumentation()
             .AddPrometheusExporter(options =>
             {
                 options.StartHttpListener = true;
