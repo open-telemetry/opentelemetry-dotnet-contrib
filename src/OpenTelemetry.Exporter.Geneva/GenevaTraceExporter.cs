9157483ccf9974b1e493c93212e793c5b46db7e2
Utkarsh Umesan Pillai <utpilla@microsoft.com>
2022-10-20 16:21:20 -0700

    Remove unnecessary using statement

--- src/OpenTelemetry.Exporter.Geneva/GenevaTraceExporter.cs
+++ src/OpenTelemetry.Exporter.Geneva/GenevaTraceExporter.cs
@@ -17,7 +17,6 @@
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
-using OpenTelemetry.Exporter.Geneva.TLDExporter;
 using OpenTelemetry.Internal;
 
 namespace OpenTelemetry.Exporter.Geneva;
