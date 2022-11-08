53dcca8595d9e9eeefed07c383ee1297dbf69b4d
Yun-Ting Lin <yunl@microsoft.com>
2022-11-08 14:30:36 -0800

    initial commit

--- src/OpenTelemetry.Instrumentation.Process/ProcessMetrics.cs
+++ src/OpenTelemetry.Instrumentation.Process/ProcessMetrics.cs
@@ -53,7 +53,7 @@ internal sealed class ProcessMetrics
             "process.memory.virtual",
             () =>
             {
-                return Diagnostics.Process.GetCurrentProcess().PrivateMemorySize64;
+                return Diagnostics.Process.GetCurrentProcess().VirtualMemorySize64;
             },
             unit: "By",
             description: "The amount of virtual memory allocated for this process that cannot be shared with other processes.");
