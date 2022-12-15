9c69a495d36e3ee39ecfdfb3ed3a1d17326dfb5d
Jonathan Channon <jonathan.channon@gmail.com>
2022-12-15 11:15:08 +0000

    Add mongo instrumentation

---
+++ src/OpenTelemetry.Instrumentation.Mongo/TracerProviderBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace OpenTelemetry.Trace;
+
+/// <summary>
+/// Extension methods to simplify registering of Mongo instrumentation.
+/// </summary>
+public static class TracerProviderBuilderExtensions
+{
+    /// <summary>
+    /// Adds Mongo instrumentation to the tracer provider.
+    /// </summary>
+    /// <param name="builder"><see cref="TracerProviderBuilder"/> being configured.</param>
+    /// <returns>The instance of <see cref="TracerProviderBuilder"/> to chain the calls.</returns>
+    public static TracerProviderBuilder AddMongoDBInstrumentation(this TracerProviderBuilder builder)
+        => builder.AddSource("MongoDB.Driver.Core.Extensions.DiagnosticSources");
+}
