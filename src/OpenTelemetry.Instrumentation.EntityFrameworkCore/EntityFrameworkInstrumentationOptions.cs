3b5c4a526318cf9660343affd5dd23fa2d190ce2
Michael Murphy <5879863+mrblonde91@users.noreply.github.com>
2023-01-11 15:51:52 +0000

    Update description

--- src/OpenTelemetry.Instrumentation.EntityFrameworkCore/EntityFrameworkInstrumentationOptions.cs
+++ src/OpenTelemetry.Instrumentation.EntityFrameworkCore/EntityFrameworkInstrumentationOptions.cs
@@ -37,13 +37,11 @@ public class EntityFrameworkInstrumentationOptions
     public bool SetDbStatementForText { get; set; } = false;
 
     /// <summary>
-    /// Gets or sets an action to enrich an Activity.
+    /// Gets or sets an action to enrich an Activity from the db connection
     /// </summary>
     /// <remarks>
     /// <para><see cref="Activity"/>: the activity being enriched.</para>
-    /// <para>string: the name of the event.</para>
-    /// <para>object: the raw object from which additional information can be extracted to enrich the activity.
-    /// The type of this object depends on the event, which is given by the above parameter.</para>
+    /// <para><see cref="IDbConnection"/>: connection to allow access to contextual information</para>
     /// </remarks>
     public Action<Activity, IDbConnection> EnrichWithIDbConnection { get; set; }
 }
