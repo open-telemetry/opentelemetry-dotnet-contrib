33f399f5e97f92c5b5f84e418b77353187b8e6c5
Soheil Alizadeh <xsoheilalizadeh@gmail.com>
2023-02-04 23:58:06 +0100

    Add Cassandra metrics support

---
+++ src/OpenTelemetry.Instrumentation.Cassandra/DriverGauge.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="DriverGauge.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using System.Diagnostics.Metrics;
+using Cassandra.Metrics.Abstractions;
+
+namespace OpenTelemetry.Instrumentation.Cassandra;
+
+internal class DriverGauge : IDriverGauge
+{
+    public const string MeterName = nameof(DriverGauge);
+
+    private readonly ObservableGauge<double> gauge;
+
+    public DriverGauge(string name, Func<double> value)
+    {
+        var meter = new Meter(MeterName);
+
+        this.gauge = meter.CreateObservableGauge(name, value);
+    }
+}
