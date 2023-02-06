3940f9b420add729fb4bdf4f70aedaecfbbe693e
Yun-Ting Lin <yunl@microsoft.com>
2023-02-06 15:10:40 -0800

    removed unused path

--- src/OpenTelemetry.Instrumentation.Process/MeterProviderBuilderExtensions.cs
+++ src/OpenTelemetry.Instrumentation.Process/MeterProviderBuilderExtensions.cs
@@ -14,7 +14,6 @@
 // limitations under the License.
 // </copyright>
 
-using System;
 using OpenTelemetry.Instrumentation.Process;
 using OpenTelemetry.Internal;
 
@@ -30,23 +29,12 @@ public static class MeterProviderBuilderExtensions
     /// </summary>
     /// <param name="builder"><see cref="MeterProviderBuilder"/> being configured.</param>
     /// <returns>The instance of <see cref="MeterProviderBuilder"/> to chain the calls.</returns>
-    public static MeterProviderBuilder AddProcessInstrumentation(this MeterProviderBuilder builder) => AddProcessInstrumentation(builder, configure: null);
-
-    /// <summary>
-    /// Enables process instrumentation.
-    /// </summary>
-    /// <param name="builder"><see cref="MeterProviderBuilder"/> being configured.</param>
-    /// <param name="configure">Process metrics options.</param>
-    /// <returns>The instance of <see cref="MeterProviderBuilder"/> to chain the calls.</returns>
     public static MeterProviderBuilder AddProcessInstrumentation(
-        this MeterProviderBuilder builder,
-        Action<ProcessInstrumentationOptions>? configure)
+        this MeterProviderBuilder builder)
     {
         Guard.ThrowIfNull(builder);
 
         var options = new ProcessInstrumentationOptions();
-        configure?.Invoke(options);
-
         builder.AddMeter(ProcessMetrics.MeterName);
         return builder.AddInstrumentation(() => new ProcessMetrics(options));
     }
