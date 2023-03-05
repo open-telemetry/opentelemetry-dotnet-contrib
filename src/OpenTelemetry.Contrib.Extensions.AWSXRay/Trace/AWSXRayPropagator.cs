712efdc8237ba15022af29bfae3ca1340e7b548b
martincostello <martin@martincostello.com>
2023-03-05 17:04:35 +0000

    [Extensions.AWSXRay] Fix analysis warnings
    Fix/suppress code analysis warnings in OpenTelemetry.Contrib.Extensions.AWSXRay.
    Contributes to #950.

--- src/OpenTelemetry.Contrib.Extensions.AWSXRay/Trace/AWSXRayPropagator.cs
+++ src/OpenTelemetry.Contrib.Extensions.AWSXRay/Trace/AWSXRayPropagator.cs
@@ -303,7 +303,13 @@ public class AWSXRayPropagator : TextMapPropagator
     internal static string ToXRayTraceIdFormat(string traceId)
     {
         var sb = new StringBuilder();
+#if NETFRAMEWORK
+#pragma warning disable CA1834
         sb.Append(Version);
+#pragma warning restore CA1834
+#else
+        sb.Append(Version[0]);
+#endif
         sb.Append(TraceIdDelimiter);
         sb.Append(traceId.Substring(0, EpochHexDigits));
         sb.Append(TraceIdDelimiter);
