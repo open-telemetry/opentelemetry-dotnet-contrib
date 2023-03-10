9dd714ac7110da7ffc38357a3f0006a0bc811bb4
Artur Gordashnikov <gao.artur@gmail.com>
2023-03-10 21:22:28 +0200

    Add options support to EntityFrameworkCore instrumentation (#1020)

--- src/OpenTelemetry.Instrumentation.EntityFrameworkCore/CHANGELOG.md
+++ src/OpenTelemetry.Instrumentation.EntityFrameworkCore/CHANGELOG.md
@@ -2,6 +2,11 @@
 
 ## Unreleased
 
+* Added overloads which accept a name to the `TracerProviderBuilder`
+  `EntityFrameworkInstrumentationOptions` extension to allow for more fine-grained
+  options management
+   ([#1020](https://github.com/open-telemetry/opentelemetry-dotnet-contrib/pull/1020))
+
 ## 1.0.0-beta.5
 
 Released 2023-Feb-27
--- src/OpenTelemetry.Instrumentation.EntityFrameworkCore/TracerProviderBuilderExtensions.cs
+++ src/OpenTelemetry.Instrumentation.EntityFrameworkCore/TracerProviderBuilderExtensions.cs
@@ -15,6 +15,8 @@
 // </copyright>
 
 using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using OpenTelemetry.Instrumentation.EntityFrameworkCore;
 using OpenTelemetry.Instrumentation.EntityFrameworkCore.Implementation;
 using OpenTelemetry.Internal;
@@ -32,7 +34,8 @@ public static class TracerProviderBuilderExtensions
     /// <param name="builder"><see cref="TracerProviderBuilder"/> being configured.</param>
     /// <returns>The instance of <see cref="TracerProviderBuilder"/> to chain the calls.</returns>
     public static TracerProviderBuilder AddEntityFrameworkCoreInstrumentation(
-        this TracerProviderBuilder builder) => AddEntityFrameworkCoreInstrumentation(builder, configure: null);
+        this TracerProviderBuilder builder) =>
+        AddEntityFrameworkCoreInstrumentation(builder, name: null, configure: null);
 
     /// <summary>
     /// Enables Microsoft.EntityFrameworkCore instrumentation.
@@ -42,14 +45,35 @@ public static class TracerProviderBuilderExtensions
     /// <returns>The instance of <see cref="TracerProviderBuilder"/> to chain the calls.</returns>
     public static TracerProviderBuilder AddEntityFrameworkCoreInstrumentation(
         this TracerProviderBuilder builder,
+        Action<EntityFrameworkInstrumentationOptions> configure) =>
+        AddEntityFrameworkCoreInstrumentation(builder, name: null, configure);
+
+    /// <summary>
+    /// Enables Microsoft.EntityFrameworkCore instrumentation.
+    /// </summary>
+    /// <param name="builder"><see cref="TracerProviderBuilder"/> being configured.</param>
+    /// <param name="name">Name which is used when retrieving options.</param>
+    /// <param name="configure">EntityFrameworkCore configuration options.</param>
+    /// <returns>The instance of <see cref="TracerProviderBuilder"/> to chain the calls.</returns>
+    public static TracerProviderBuilder AddEntityFrameworkCoreInstrumentation(
+        this TracerProviderBuilder builder,
+        string name,
         Action<EntityFrameworkInstrumentationOptions> configure)
     {
         Guard.ThrowIfNull(builder);
 
-        var options = new EntityFrameworkInstrumentationOptions();
-        configure?.Invoke(options);
+        name ??= Options.DefaultName;
 
-        builder.AddInstrumentation(() => new EntityFrameworkInstrumentation(options));
+        if (configure != null)
+        {
+            builder.ConfigureServices(services => services.Configure(name, configure));
+        }
+
+        builder.AddInstrumentation(sp =>
+        {
+            var options = sp.GetRequiredService<IOptionsMonitor<EntityFrameworkInstrumentationOptions>>().Get(name);
+            return new EntityFrameworkInstrumentation(options);
+        });
 
         builder.AddSource(EntityFrameworkDiagnosticListener.ActivitySourceName);
 
