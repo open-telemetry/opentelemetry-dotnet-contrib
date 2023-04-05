cddc430ba689845f87130d50a3c0179370bcac04
Vishwesh Bankwar <vibankwa@microsoft.com>
2023-04-05 10:44:56 -0700

    rmv using

--- src/OpenTelemetry.PersistentStorage.FileSystem/PersistentStorageHelper.cs
+++ src/OpenTelemetry.PersistentStorage.FileSystem/PersistentStorageHelper.cs
@@ -19,8 +19,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
-using System.Text;
 using System.Threading;
 
 namespace OpenTelemetry.PersistentStorage.FileSystem;
