142dc7514d87ae29055d0593c3dce847f8d192c2
Mateusz Lach <mateusza@splunk.com>
2023-04-11 09:18:57 +0200

    format fix

--- src/OpenTelemetry.Instrumentation.StackExchangeRedis/IConnectionRegistry.cs
+++ src/OpenTelemetry.Instrumentation.StackExchangeRedis/IConnectionRegistry.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="IConnectionRegistry.cs" company="OpenTelemetry Authors">
+// <copyright file="IConnectionRegistry.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
