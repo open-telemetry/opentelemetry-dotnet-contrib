b4db88e473b6c86b2189646a016d0cf4f78fad7b
Havret <h4vret@gmail.com>
2023-05-16 18:40:53 +0200

    Get rid of NoWarn-CS1591

--- src/OpenTelemetry.Exporter.InfluxDB/MetricsSchema.cs
+++ src/OpenTelemetry.Exporter.InfluxDB/MetricsSchema.cs
@@ -21,7 +21,24 @@ namespace OpenTelemetry.Exporter.InfluxDB;
 /// </summary>
 public enum MetricsSchema
 {
+    /// <summary>
+    /// No schema selected. Default schema will be used.
+    /// </summary>
     None,
+
+    /// <summary>
+    /// Represents the Telegraf Prometheus V1 schema.
+    /// This option converts OpenTelemetry metrics to the first version of the Telegraf Prometheus schema.
+    /// - Telegraf/InfluxDB measurement per Prometheus metric
+    /// - Fields `count`/`sum`/`gauge`/etc contain metric values.
+    /// </summary>
     TelegrafPrometheusV1 = 1,
+
+    /// <summary>
+    /// Represents the Telegraf Prometheus V2 schema.
+    /// This option converts OpenTelemetry metrics to the second version of the Telegraf Prometheus schema.
+    /// - One measurement `prometheus`
+    /// - Fields (Prometheus metric name) + `_` + `count`/`sum`/`gauge`/etc contain metric values.
+    /// </summary>
     TelegrafPrometheusV2 = 2,
 }
