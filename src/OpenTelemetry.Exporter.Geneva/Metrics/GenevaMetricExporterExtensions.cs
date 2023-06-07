698e70a58f4a8e329933a83425de3e2fc2fb9daa
Utkarsh Umesan Pillai <66651184+utpilla@users.noreply.github.com>
2023-06-06 18:33:29 -0700

    [Exporter.Geneva] Add named options support for GenevaTraceExporter and GenevaMetricExporter (#1218)

--- src/OpenTelemetry.Exporter.Geneva/Metrics/GenevaMetricExporterExtensions.cs
+++ src/OpenTelemetry.Exporter.Geneva/Metrics/GenevaMetricExporterExtensions.cs
@@ -15,6 +15,8 @@
 // </copyright>
 
 using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using OpenTelemetry.Internal;
 using OpenTelemetry.Metrics;
 
@@ -26,15 +28,40 @@ public static class GenevaMetricExporterExtensions
     /// Adds <see cref="GenevaMetricExporter"/> to the <see cref="MeterProviderBuilder"/>.
     /// </summary>
     /// <param name="builder"><see cref="MeterProviderBuilder"/> builder to use.</param>
+    /// <returns>The instance of <see cref="MeterProviderBuilder"/> to chain the calls.</returns>
+    public static MeterProviderBuilder AddGenevaMetricExporter(this MeterProviderBuilder builder)
+        => AddGenevaMetricExporter(builder, name: null, configure: null);
+
+    /// <summary>
+    /// Adds <see cref="GenevaMetricExporter"/> to the <see cref="MeterProviderBuilder"/>.
+    /// </summary>
+    /// <param name="builder"><see cref="MeterProviderBuilder"/> builder to use.</param>
     /// <param name="configure">Exporter configuration options.</param>
     /// <returns>The instance of <see cref="MeterProviderBuilder"/> to chain the calls.</returns>
-    public static MeterProviderBuilder AddGenevaMetricExporter(this MeterProviderBuilder builder, Action<GenevaMetricExporterOptions> configure = null)
+    public static MeterProviderBuilder AddGenevaMetricExporter(this MeterProviderBuilder builder, Action<GenevaMetricExporterOptions> configure)
+        => AddGenevaMetricExporter(builder, name: null, configure);
+
+    /// <summary>
+    /// Adds <see cref="GenevaMetricExporter"/> to the <see cref="MeterProviderBuilder"/>.
+    /// </summary>
+    /// <param name="builder"><see cref="MeterProviderBuilder"/> builder to use.</param>
+    /// /// <param name="name">Name which is used when retrieving options.</param>
+    /// <param name="configure">Exporter configuration options.</param>
+    /// <returns>The instance of <see cref="MeterProviderBuilder"/> to chain the calls.</returns>
+    public static MeterProviderBuilder AddGenevaMetricExporter(this MeterProviderBuilder builder, string name, Action<GenevaMetricExporterOptions> configure)
     {
         Guard.ThrowIfNull(builder);
 
+        name ??= Options.DefaultName;
+
+        if (configure != null)
+        {
+            builder.ConfigureServices(services => services.Configure(name, configure));
+        }
+
         return builder.AddReader(sp =>
         {
-            var exporterOptions = sp.GetOptions<GenevaMetricExporterOptions>();
+            var exporterOptions = sp.GetRequiredService<IOptionsMonitor<GenevaMetricExporterOptions>>().Get(name);
 
             return BuildGenevaMetricExporter(exporterOptions, configure);
         });
--- test/OpenTelemetry.Exporter.Geneva.Tests/GenevaTraceExporterTests.cs
+++ test/OpenTelemetry.Exporter.Geneva.Tests/GenevaTraceExporterTests.cs
@@ -24,6 +24,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
+using Microsoft.Extensions.DependencyInjection;
 using OpenTelemetry.Trace;
 using Xunit;
 
@@ -512,6 +513,48 @@ public class GenevaTraceExporterTests
                 activity?.SetStatus(ActivityStatusCode.Ok);
             }
         }
+    }
+
+    [Fact]
+    public void AddGenevaTraceExporterNamedOptionsSupport()
+    {
+        string connectionString;
+        string connectionStringForNamedOptions;
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            connectionString = "EtwSession=OpenTelemetry";
+            connectionStringForNamedOptions = "EtwSession=OpenTelemetry-NamedOptions";
+        }
+        else
+        {
+            var path = GetRandomFilePath();
+            connectionString = "Endpoint=unix:" + path;
+            connectionStringForNamedOptions = "Endpoint=unix:" + path + "NamedOptions";
+        }
+
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .ConfigureServices(services =>
+            {
+                services.Configure<GenevaExporterOptions>(options =>
+                {
+                    options.ConnectionString = connectionString;
+                });
+                services.Configure<GenevaExporterOptions>("ExporterWithNamedOptions", options =>
+                {
+                    options.ConnectionString = connectionStringForNamedOptions;
+                });
+            })
+            .AddGenevaTraceExporter(options =>
+            {
+                // ConnectionString for the options is already set in `IServiceCollection Configure<TOptions>` calls above
+                Assert.Equal(connectionString, options.ConnectionString);
+            })
+            .AddGenevaTraceExporter("ExporterWithNamedOptions", options =>
+            {
+                // ConnectionString for the named options is already set in `IServiceCollection Configure<TOptions>` calls above
+                Assert.Equal(connectionStringForNamedOptions, options.ConnectionString);
+            })
+            .Build();
     }
 
     private static string GetRandomFilePath()
