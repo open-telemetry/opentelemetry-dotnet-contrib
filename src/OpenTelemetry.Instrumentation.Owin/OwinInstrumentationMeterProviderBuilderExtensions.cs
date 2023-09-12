b9c284c59071690c40b72b652455ea873128eb6a
Matthew Hensley <matthew.hensley@grafana.com>
2023-09-12 10:37:16 -0400

    correct name in file header

--- src/OpenTelemetry.Instrumentation.Owin/OwinInstrumentationMeterProviderBuilderExtensions.cs
+++ src/OpenTelemetry.Instrumentation.Owin/OwinInstrumentationMeterProviderBuilderExtensions.cs
@@ -1,4 +1,4 @@
-// <copyright file="MetricProviderBuilderExtensions.cs" company="OpenTelemetry Authors">
+// <copyright file="OwinInstrumentationMeterProviderBuilderExtensions.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
