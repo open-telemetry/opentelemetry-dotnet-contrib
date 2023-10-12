d1d7940f7d044d75195544c8390fb3be76a36d3c
Isaac Hili <18428880+isaachili@users.noreply.github.com>
2023-10-12 09:19:58 +0000

    Export metrics to InfluxDB at a configurable interval

--- src/OpenTelemetry.Exporter.InfluxDB/InfluxDBMetricsExporterOptions.cs
+++ src/OpenTelemetry.Exporter.InfluxDB/InfluxDBMetricsExporterOptions.cs
@@ -15,6 +15,7 @@
 // </copyright>
 
 using System;
+using OpenTelemetry.Internal;
 
 namespace OpenTelemetry.Exporter.InfluxDB;
 
@@ -23,6 +24,8 @@ namespace OpenTelemetry.Exporter.InfluxDB;
 /// </summary>
 public class InfluxDBMetricsExporterOptions
 {
+    private int metricExportIntervalMilliseconds = 60000;
+
     /// <summary>
     /// Gets or sets HTTP/S destination for line protocol.
     /// </summary>
@@ -52,4 +55,17 @@ public class InfluxDBMetricsExporterOptions
     /// Gets or sets the time to wait at most (milliseconds) with the write.
     /// </summary>
     public int FlushInterval { get; set; } = 1000;
+
+    /// <summary>
+    /// Gets or sets the metric export interval in milliseconds. The default value is 60000.
+    /// </summary>
+    public int MetricExportIntervalMilliseconds
+    {
+        get => this.metricExportIntervalMilliseconds;
+        set
+        {
+            Guard.ThrowIfOutOfRange(value, min: 1000);
+            this.metricExportIntervalMilliseconds = value;
+        }
+    }
 }
