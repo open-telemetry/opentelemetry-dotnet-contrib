39224da6bcf89f0b0fc58431349916e4d663b622
Artur Gordashnikov <gao.artur@gmail.com>
2023-11-22 00:31:26 +0200

    Fix ActivityTraceFlags assertion (#1441)

--- test/OpenTelemetry.Instrumentation.EntityFrameworkCore.Tests/EntityFrameworkDiagnosticListenerTests.cs
+++ test/OpenTelemetry.Instrumentation.EntityFrameworkCore.Tests/EntityFrameworkDiagnosticListenerTests.cs
@@ -153,7 +153,7 @@ public class EntityFrameworkDiagnosticListenerTests : IDisposable
         var activity = (Activity)activityProcessor.Invocations[1].Arguments[0];
 
         Assert.False(activity.IsAllDataRequested);
-        Assert.True(activity.ActivityTraceFlags.HasFlag(ActivityTraceFlags.None));
+        Assert.False(activity.ActivityTraceFlags.HasFlag(ActivityTraceFlags.Recorded));
     }
 
     [Fact]
@@ -220,7 +220,7 @@ public class EntityFrameworkDiagnosticListenerTests : IDisposable
         var activity = (Activity)activityProcessor.Invocations[1].Arguments[0];
 
         Assert.False(activity.IsAllDataRequested);
-        Assert.True(activity.ActivityTraceFlags.HasFlag(ActivityTraceFlags.None));
+        Assert.False(activity.ActivityTraceFlags.HasFlag(ActivityTraceFlags.Recorded));
     }
 
     [Fact]
@@ -323,9 +323,8 @@ public class EntityFrameworkDiagnosticListenerTests : IDisposable
             modelBuilder.Entity<Item>(
                 b =>
                 {
-                    b.Property("Id");
-                    b.HasKey("Id");
                     b.Property(e => e.Name);
+                    b.HasKey("Name");
                 });
         }
     }
