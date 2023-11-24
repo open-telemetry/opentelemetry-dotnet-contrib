f9aed1297758dd78871fde6aa91610ca4e17a163
Daniel Graveling <dgraveling@ruffer.co.uk>
2023-11-24 14:59:59 +0000

    add TracerProviderBuilderExtensions

---
+++ src/OpenTelemetry.Instrumentation.ServiceConnect/TracerProviderBuilderExtensions.cs
@@ -0,0 +1,54 @@
+// <copyright file="TracerProviderBuilderExtensions.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using OpenTelemetry.Instrumentation.ServiceConnect.Implementation;
+using OpenTelemetry.Internal;
+using OpenTelemetry.Trace;
+
+namespace OpenTelemetry.Instrumentation.ServiceConnect;
+
+public static class TracerProviderBuilderExtensions
+{
+    public static TracerProviderBuilder AddServiceConnectInstrumentation(this TracerProviderBuilder builder)
+    {
+        return AddServiceConnectInstrumentation(builder, null, null);
+    }
+
+    public static TracerProviderBuilder AddServiceConnectInstrumentation(this TracerProviderBuilder builder, string? name, Action<ServiceConnectInstrumentationOptions>? configure)
+    {
+        Guard.ThrowIfNull(builder);
+
+        name ??= Options.DefaultName;
+
+        if (configure is not null)
+        {
+            builder.ConfigureServices(services => services.Configure(name, configure));
+        }
+
+        builder.AddInstrumentation(sp =>
+        {
+            var options = sp.GetRequiredService<IOptionsMonitor<ServiceConnectInstrumentationOptions>>().Get(name);
+            return new ServiceConnectInstrumentation(options);
+        });
+
+        builder.AddSource(ServiceConnectDiagnosticListener.ActivitySourceName);
+
+        return builder;
+    }
+}
