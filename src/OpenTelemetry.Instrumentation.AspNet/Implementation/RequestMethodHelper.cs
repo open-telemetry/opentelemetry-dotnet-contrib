706f14fc364562f2a9ff237cb6e9fc9ece32659c
Christoffer Hjalmarsson <b.christoffer.hjalmarsson@gmail.com>
2023-12-09 17:05:59 +0100

    Ordering of fields

--- src/OpenTelemetry.Instrumentation.AspNet/Implementation/RequestMethodHelper.cs
+++ src/OpenTelemetry.Instrumentation.AspNet/Implementation/RequestMethodHelper.cs
@@ -10,11 +10,12 @@ namespace OpenTelemetry.Instrumentation.AspNet.Implementation;
 internal sealed class RequestMethodHelper
 {
     private const string KnownHttpMethodsEnvironmentVariable = "OTEL_INSTRUMENTATION_HTTP_KNOWN_METHODS";
-    private static readonly char[] SplitChars = new[] { ',' };
 
     // The value "_OTHER" is used for non-standard HTTP methods.
     // https://github.com/open-telemetry/semantic-conventions/blob/v1.23.0/docs/http/http-spans.md#common-attributes
     private const string OtherHttpMethod = "_OTHER";
+
+    private static readonly char[] SplitChars = new[] { ',' };
 
     // List of known HTTP methods as per spec.
     private readonly Dictionary<string, string> knownHttpMethods;
