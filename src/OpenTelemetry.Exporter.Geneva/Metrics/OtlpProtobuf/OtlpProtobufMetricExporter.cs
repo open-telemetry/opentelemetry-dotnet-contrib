74ad69ed8117cb23572f25054ced4a4daaa1b06c
Vishwesh Bankwar <Vishwesh.Bankwar@gmail.com>
2024-02-26 10:33:38 -0800

    rmv http

--- src/OpenTelemetry.Exporter.Geneva/Metrics/OtlpProtobuf/OtlpProtobufMetricExporter.cs
+++ src/OpenTelemetry.Exporter.Geneva/Metrics/OtlpProtobuf/OtlpProtobufMetricExporter.cs
@@ -2,10 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using System;
-#if NET6_0_OR_GREATER
-using System.Net.Http;
-using System.Net.Http.Headers;
-#endif
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 
@@ -51,40 +47,6 @@ internal class OtlpProtobufMetricExporter : BaseExporter<Metric>
             ExporterEventSource.Log.ExporterException("metric batch failed", ex);
         }
 
-#if NET6_0_OR_GREATER
-        byte[] arr = new byte[this.buffer.Length - currentPosition];
-
-        Buffer.BlockCopy(this.buffer, currentPosition, arr, 0, arr.Length);
-        SendRequest(arr);
-#endif
         return result;
     }
-
-#if NET6_0_OR_GREATER
-    private static void SendRequest(byte[] buffer)
-    {
-        HttpClient httpClient = new HttpClient();
-
-        // Create a new HttpRequestMessage
-        var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:4318/v1/metrics");
-
-        // Set the Content to a ByteArrayContent containing the serialized data
-        request.Content = new ByteArrayContent(buffer);
-        request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-protobuf");
-
-        // Version on message is needed
-        // Without it HttpClient will not identify this as Http/2 request.
-        // request.Version = new Version(2, 0);
-        request.VersionPolicy = HttpVersionPolicy.RequestVersionExact;
-
-        // Send the request
-        var response = httpClient.SendAsync(request).Result;
-
-        // Ensure the request was successful
-        if (!response.IsSuccessStatusCode)
-        {
-            throw new InvalidOperationException($"Request failed with status code {response.StatusCode}");
-        }
-    }
-#endif
 }
