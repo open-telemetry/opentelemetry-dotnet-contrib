8558356d99d8340a196fcec31b5ec036c687719e
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2024-02-29 07:17:50 +0100

    Drop unused method

--- src/Shared/SpanHelper.cs
+++ src/Shared/SpanHelper.cs
@@ -32,25 +32,6 @@ internal static class SpanHelper
     }
 
     /// <summary>
-    /// Helper method that populates span properties from http status code according
-    /// to https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/http.md#status.
-    /// </summary>
-    /// <param name="kind">The span kind.</param>
-    /// <param name="httpStatusCode">Http status code.</param>
-    /// <returns>Resolved span <see cref="Status"/> for the Http status code.</returns>
-    public static Status ResolveSpanStatusForHttpStatusCode(ActivityKind kind, int httpStatusCode)
-    {
-        var lowerBound = kind == ActivityKind.Client ? 400 : 500;
-        var upperBound = 599;
-        if (httpStatusCode >= lowerBound && httpStatusCode <= upperBound)
-        {
-            return Status.Error;
-        }
-
-        return Status.Unset;
-    }
-
-    /// <summary>
     /// Helper method that populates Activity Status from http status code according
     /// to https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/http.md#status.
     /// </summary>
