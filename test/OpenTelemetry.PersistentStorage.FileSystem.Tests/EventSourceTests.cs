3162b3db7889f22c7805906e95be3f8e5edfe559
Timothy Mothra Lee <tilee@microsoft.com>
2024-03-14 16:22:13 -0700

    fix eventsource. add tests

---
+++ test/OpenTelemetry.PersistentStorage.FileSystem.Tests/EventSourceTests.cs
@@ -0,0 +1,17 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+using OpenTelemetry.Tests;
+using Xunit;
+
+namespace OpenTelemetry.PersistentStorage.FileSystem.Tests;
+
+public class EventSourceTests
+{
+
+    [Fact]
+    public void EventSourceTest_PersistentStorageEventSource()
+    {
+        EventSourceTestHelper.MethodsAreImplementedConsistentlyWithTheirAttributes(PersistentStorageEventSource.Log);
+    }
+}
