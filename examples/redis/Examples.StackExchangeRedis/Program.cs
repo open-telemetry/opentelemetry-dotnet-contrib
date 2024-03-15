219e41848a810479c2024c2e48b8cb7ae3a5d3e6
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2024-03-15 20:10:53 +0100

    [Instrumentation.Redis] Fix and modernize example (#1612)

--- examples/redis/Examples.StackExchangeRedis/Program.cs
+++ examples/redis/Examples.StackExchangeRedis/Program.cs
@@ -5,35 +5,29 @@ using OpenTelemetry;
 using OpenTelemetry.Trace;
 using StackExchange.Redis;
 
-public class Program
-{
-    public static void Main()
+// Prerequisite:
+/*
+ * Setup redis service inside local docker.
+ * docker run --name opentelemetry-redis-test -d -p 6379:6379 redis
+ */
+
+// connect to the redis server. The default port 6379 will be used.
+var connection = ConnectionMultiplexer.Connect("localhost");
+
+// Configure exporter to export traces to Zipkin
+using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+    .AddConsoleExporter()
+    .AddRedisInstrumentation(connection, options =>
     {
-        // Prerequisite:
-        /*
-         * Setup redis service inside local docker.
-         * docker run --name opentelemetry-redis-test -d -p 6379:6379 redis
-         */
+        // changing flush interval from 10s to 5s
+        options.FlushInterval = TimeSpan.FromSeconds(5);
+    })
+    .Build();
 
-        // connect to the redis server. The default port 6379 will be used.
-        var connection = ConnectionMultiplexer.Connect("localhost");
+// select a database (by default, DB = 0)
+var db = connection.GetDatabase();
+db.StringSet("key", "value " + DateTime.Now.ToLongDateString());
+Thread.Sleep(1000);
 
-        // Configure exporter to export traces to Zipkin
-        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .AddConsoleExporter()
-                .AddRedisInstrumentation(connection, options =>
-                {
-                    // changing flush interval from 10s to 5s
-                    options.FlushInterval = TimeSpan.FromSeconds(5);
-                })
-                .Build();
-
-        // select a database (by default, DB = 0)
-        var db = connection.GetDatabase();
-        db.StringSet("key", "value " + DateTime.Now.ToLongDateString());
-        Thread.Sleep(1000);
-
-        // run a command, in this case a GET
-        var myVal = db.StringGet("key");
-    }
-}
+// run a command, in this case a GET
+var myVal = db.StringGet("key");
