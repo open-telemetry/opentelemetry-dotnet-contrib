61b89088c1fcb8a13b14a724046c8cae5f8fe988
Gunnar Liljas <gunnar.liljas@gmail.com>
2024-04-17 11:00:32 +0200

    Singleton Meter and Histogram, instead of creating new for each request

--- src/OpenTelemetry.Instrumentation.Owin/Implementation/OwinInstrumentationMetrics.cs
+++ src/OpenTelemetry.Instrumentation.Owin/Implementation/OwinInstrumentationMetrics.cs
@@ -1,6 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+using System;
 using System.Diagnostics.Metrics;
 using System.Reflection;
 using OpenTelemetry.Internal;
@@ -11,10 +12,7 @@ internal static class OwinInstrumentationMetrics
 {
     internal static readonly Assembly Assembly = typeof(OwinInstrumentationMetrics).Assembly;
     internal static readonly AssemblyName AssemblyName = Assembly.GetName();
-
-    public static string MeterName => AssemblyName.Name;
-
-    public static Meter Instance => new(MeterName, Assembly.GetPackageVersion());
-
-    public static Histogram<double> HttpServerDuration => Instance.CreateHistogram<double>("http.server.request.duration", "s", "Duration of HTTP server requests.");
+    internal static readonly string MeterName = AssemblyName.Name;
+    internal static readonly Meter Instance = new(MeterName, Assembly.GetPackageVersion());
+    internal static readonly Histogram<double> HttpServerDuration = Instance.CreateHistogram<double>("http.server.request.duration", "s", "Duration of HTTP server requests.");
 }
