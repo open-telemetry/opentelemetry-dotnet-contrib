76226d51b7b60cac94d2222e091bae0e3014b0c3
Mike Goldsmth <goldsmith.mike@gmail.com>
2024-04-18 12:21:56 +0100

    feat: Add baggage span processor

---
+++ src/OpenTelemetry.SpanProcessor.Baggage/BaggageSpanProcessor.cs
@@ -0,0 +1,23 @@
+﻿// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+using OpenTelemetry;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace OpenTelemetry.SpanProcessor.Baggage;
+
+/// <summary>
+/// Span processor that adds <see cref="Baggage"/> fields to every new span
+/// </summary>
+public class BaggageSpanProcessor : OpenTelemetry.<Activity>
+{
+    /// <inheritdoc />
+    public override void OnStart(Activity activity)
+    {
+        foreach (var entry in Baggage.Current)
+        {
+            activity.SetTag(entry.Key, entry.Value);
+        }
+    }
+}
