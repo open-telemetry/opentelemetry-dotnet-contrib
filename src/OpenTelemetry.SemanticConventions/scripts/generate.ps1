053aae4a749e0aa0e509135de955e4a2b48ea4e1
joegoldman2 <147369450+joegoldman@users.noreply.github.com>
2024-04-21 10:23:19 +0000

    Rollback to open-telemetry/opentelemetry-specification v1.13.0

--- src/OpenTelemetry.SemanticConventions/scripts/generate.ps1
+++ src/OpenTelemetry.SemanticConventions/scripts/generate.ps1
@@ -2,8 +2,9 @@ $SCRIPT_DIR = $PSScriptRoot
 $ROOT_DIR = "${SCRIPT_DIR}/../"
 
 # freeze the spec & generator tools versions to make SemanticAttributes generation reproducible
-$SPEC_VERSION = "1.25.0"
+$SPEC_VERSION = "1.13.0"
 $SCHEMA_URL = "https://opentelemetry.io/schemas/$SPEC_VERSION"
+$GENERATOR_VERSION = "0.16.0"
 
 Set-Location $SCRIPT_DIR
 
@@ -12,29 +13,29 @@ mkdir semantic-conventions
 Set-Location semantic-conventions
 
 git init
-git remote add origin https://github.com/open-telemetry/semantic-conventions.git
+git remote add origin https://github.com/open-telemetry/opentelemetry-specification.git
 git fetch origin v$SPEC_VERSION
 git reset --hard FETCH_HEAD
 Set-Location ${SCRIPT_DIR}
 
 docker run --rm `
-  -v ${SCRIPT_DIR}/semantic-conventions/model/trace:/source `
+  -v ${SCRIPT_DIR}/semantic-conventions/semantic_conventions/logs:/source `
   -v ${SCRIPT_DIR}/templates:/templates `
-  -v ${ROOT_DIR}/Trace:/output `
-  otel/semconvgen:latest `
+  -v ${ROOT_DIR}/Logs:/output `
+  otel/semconvgen:$GENERATOR_VERSION `
   -f /source code `
   --template /templates/SemanticConventions.cs.j2 `
-  --output /output/TraceSemanticConventions.cs `
+  --output /output/LogsSemanticConventions.cs `
   --trim-whitespace `
-  -D class=TraceSemanticConventions `
+  -D class=LogsSemanticConventions `
   -D schemaUrl=$SCHEMA_URL `
-  -D pkg=OpenTelemetry.Trace
+  -D pkg=OpenTelemetry.Logs
 
 docker run --rm `
-  -v ${SCRIPT_DIR}/semantic-conventions/model/resource:/source `
+  -v ${SCRIPT_DIR}/semantic-conventions/semantic_conventions/resource:/source `
   -v ${SCRIPT_DIR}/templates:/templates `
   -v ${ROOT_DIR}/Resource:/output `
-  otel/semconvgen:latest `
+  otel/semconvgen:$GENERATOR_VERSION `
   -f /source code `
   --template /templates/SemanticConventions.cs.j2 `
   --output /output/ResourceSemanticConventions.cs `
@@ -43,4 +44,15 @@ docker run --rm `
   -D schemaUrl=$SCHEMA_URL `
   -D pkg=OpenTelemetry.Resources
 
-Set-Location ${ROOT_DIR}
+docker run --rm `
+  -v ${SCRIPT_DIR}/semantic-conventions/semantic_conventions/trace:/source `
+  -v ${SCRIPT_DIR}/templates:/templates `
+  -v ${ROOT_DIR}/Trace:/output `
+  otel/semconvgen:$GENERATOR_VERSION `
+  -f /source code `
+  --template /templates/SemanticConventions.cs.j2 `
+  --output /output/TraceSemanticConventions.cs `
+  --trim-whitespace `
+  -D class=TraceSemanticConventions `
+  -D schemaUrl=$SCHEMA_URL `
+  -D pkg=OpenTelemetry.Trace
--- src/OpenTelemetry.SemanticConventions/scripts/generate.sh
+++ src/OpenTelemetry.SemanticConventions/scripts/generate.sh
@@ -5,8 +5,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="${SCRIPT_DIR}/../"
 
 # freeze the spec & generator tools versions to make SemanticAttributes generation reproducible
-SPEC_VERSION=1.25.0
+SPEC_VERSION=1.13.0
 SCHEMA_URL=https://opentelemetry.io/schemas/$SPEC_VERSION
+GENERATOR_VERSION=0.16.0
 
 cd ${SCRIPT_DIR}
 
@@ -15,29 +16,29 @@ mkdir semantic-conventions
 cd semantic-conventions
 
 git init
-git remote add origin https://github.com/open-telemetry/semantic-conventions.git
+git remote add origin https://github.com/open-telemetry/opentelemetry-specification.git
 git fetch origin "v$SPEC_VERSION"
 git reset --hard FETCH_HEAD
 cd ${SCRIPT_DIR}
 
 docker run --rm \
-  -v ${SCRIPT_DIR}/semantic-conventions/model/trace:/source \
+  -v ${SCRIPT_DIR}/semantic-conventions/semantic_conventions/logs:/source \
   -v ${SCRIPT_DIR}/templates:/templates \
-  -v ${ROOT_DIR}/Trace:/output \
-  otel/semconvgen:latest \
+  -v ${ROOT_DIR}/Logs:/output \
+  otel/semconvgen:$GENERATOR_VERSION \
   -f /source code \
   --template /templates/SemanticConventions.cs.j2 \
-  --output /output/TraceSemanticConventions.cs \
+  --output /output/LogsSemanticConventions.cs \
   --trim-whitespace \
-  -Dclass=TraceSemanticConventions \
+  -Dclass=LogsSemanticConventions \
   -DschemaUrl=$SCHEMA_URL \
-  -Dpkg=OpenTelemetry.Trace
+  -Dpkg=OpenTelemetry.Logs
 
 docker run --rm \
-  -v ${SCRIPT_DIR}/semantic-conventions/model/resource:/source \
+  -v ${SCRIPT_DIR}/semantic-conventions/semantic_conventions/resource:/source \
   -v ${SCRIPT_DIR}/templates:/templates \
   -v ${ROOT_DIR}/Resource:/output \
-  otel/semconvgen:latest \
+  otel/semconvgen:$GENERATOR_VERSION \
   -f /source code \
   --template /templates/SemanticConventions.cs.j2 \
   --output /output/ResourceSemanticConventions.cs \
@@ -45,3 +46,16 @@ docker run --rm \
   -Dclass=ResourceSemanticConventions \
   -DschemaUrl=$SCHEMA_URL \
   -Dpkg=OpenTelemetry.Resources
+
+docker run --rm \
+  -v ${SCRIPT_DIR}/semantic-conventions/semantic_conventions/trace:/source \
+  -v ${SCRIPT_DIR}/templates:/templates \
+  -v ${ROOT_DIR}/Trace:/output \
+  otel/semconvgen:$GENERATOR_VERSION \
+  -f /source code \
+  --template /templates/SemanticConventions.cs.j2 \
+  --output /output/TraceSemanticConventions.cs \
+  --trim-whitespace \
+  -Dclass=TraceSemanticConventions \
+  -DschemaUrl=$SCHEMA_URL \
+  -Dpkg=OpenTelemetry.Trace
