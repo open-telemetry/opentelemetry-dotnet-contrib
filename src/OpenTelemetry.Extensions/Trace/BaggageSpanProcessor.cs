51d1d0e0b475d7bce5da2ba4624e1371b64bc80f
Mike Goldsmth <goldsmith.mike@gmail.com>
2024-04-22 14:35:02 +0100

    make class internal

--- src/OpenTelemetry.Extensions/Trace/BaggageSpanProcessor.cs
+++ src/OpenTelemetry.Extensions/Trace/BaggageSpanProcessor.cs
@@ -1,16 +1,14 @@
 ﻿// Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-using OpenTelemetry;
-using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace OpenTelemetry.Trace;
 
 /// <summary>
-/// Span processor that adds <see cref="Baggage"/> fields to every new span
+/// Span processor that adds <see cref="Baggage"/> fields to every new span.
 /// </summary>
-public class BaggageSpanProcessor : OpenTelemetry.<Activity>
+internal sealed class BaggageSpanProcessor : BaseProcessor<Activity>
 {
     /// <inheritdoc />
     public override void OnStart(Activity activity)
