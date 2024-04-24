837470d1a1ad567e80acb263d15b89c7eeb22f49
Timothy Mothra Lee <tilee@microsoft.com>
2024-04-24 09:04:08 -0700

    cleanup

--- src/OpenTelemetry.Instrumentation.SqlClient/Implementation/SqlActivitySourceHelper.cs
+++ src/OpenTelemetry.Instrumentation.SqlClient/Implementation/SqlActivitySourceHelper.cs
@@ -19,7 +19,7 @@ internal sealed class SqlActivitySourceHelper
     public static readonly Assembly Assembly = typeof(SqlActivitySourceHelper).Assembly;
     public static readonly AssemblyName AssemblyName = Assembly.GetName();
     public static readonly string ActivitySourceName = AssemblyName.Name!;
-    public static readonly ActivitySource ActivitySource = new(ActivitySourceName!, Assembly.GetPackageVersion());
+    public static readonly ActivitySource ActivitySource = new(ActivitySourceName, Assembly.GetPackageVersion());
     public static readonly string ActivityName = ActivitySourceName + ".Execute";
 
     public static readonly IEnumerable<KeyValuePair<string, object?>> CreationTags = new[]
