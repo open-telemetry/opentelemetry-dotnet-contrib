c0310cb9aa7aac6a62b8ffe8b51916ec39f7dbc5
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2024-05-07 11:12:54 +0200

    Fix CA1810

--- src/OpenTelemetry.Instrumentation.AspNetCore/Implementation/TelemetryHelper.cs
+++ src/OpenTelemetry.Instrumentation.AspNetCore/Implementation/TelemetryHelper.cs
@@ -5,16 +5,7 @@ namespace OpenTelemetry.Instrumentation.AspNetCore.Implementation;
 
 internal static class TelemetryHelper
 {
-    public static readonly object[] BoxedStatusCodes;
-
-    static TelemetryHelper()
-    {
-        BoxedStatusCodes = new object[500];
-        for (int i = 0, c = 100; i < BoxedStatusCodes.Length; i++, c++)
-        {
-            BoxedStatusCodes[i] = c;
-        }
-    }
+    public static readonly object[] BoxedStatusCodes = InitializeBoxedStatusCodes();
 
     public static object GetBoxedStatusCode(int statusCode)
     {
@@ -24,5 +15,16 @@ internal static class TelemetryHelper
         }
 
         return statusCode;
+    }
+
+    private static object[] InitializeBoxedStatusCodes()
+    {
+        var boxedStatusCodes = new object[500];
+        for (int i = 0, c = 100; i < boxedStatusCodes.Length; i++, c++)
+        {
+            boxedStatusCodes[i] = c;
+        }
+
+        return boxedStatusCodes;
     }
 }
