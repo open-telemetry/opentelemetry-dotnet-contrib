6d13d994abee876ba89fb691298f6adf80b6a2c7
Reiley Yang <reyang@microsoft.com>
2024-05-10 13:45:05 -0700

    Simplify issue template and auto add component labels

---
+++ build/add-labels.ps1
@@ -0,0 +1,12 @@
+param(
+  [Parameter(Mandatory=$true)][int]$issueNumber,
+  [Parameter(Mandatory=$true)][string]$issueBody
+)
+
+$match = [regex]::Match($issueBody, '^[#]+ Component\s*(OpenTelemetry(?:\.\w+)+)')
+if ($match.Success -eq $false)
+{
+    Return
+}
+
+gh issue edit $issueNumber --add-label $("comp:" + $match.Groups[1].Value.ToLower())
