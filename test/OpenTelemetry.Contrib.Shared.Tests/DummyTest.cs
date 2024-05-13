e1a58854691fde7bbc2592b7974200b0f36470cc
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2024-05-13 13:59:51 +0200

    Shared code testing - scaffolding

---
+++ test/OpenTelemetry.Contrib.Shared.Tests/DummyTest.cs
@@ -0,0 +1,15 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+using Xunit;
+
+namespace OpenTelemetry.Contrib.Shared.Tests;
+
+public class DummyTest
+{
+    [Fact]
+    public void DummyTestMethod()
+    {
+        Assert.True(true);
+    }
+}
