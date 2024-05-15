2147e02a54612f263fcdf76808cb0f12853d3719
Mikel Blanchard <mblanchard@macrosssoftware.com>
2024-05-15 12:15:17 -0700

    [repo] Automate PackageValidationBaselineVersion update after stable releases (#1803)

--- build/scripts/post-release.psm1
+++ build/scripts/post-release.psm1
@@ -93,3 +93,109 @@ $content
 
 Export-ModuleMember -Function CreateRelease
 
+function CreatePackageValidationBaselineVersionUpdatePullRequest {
+  param(
+    [Parameter(Mandatory=$true)][string]$tag,
+    [Parameter()][string]$gitUserName=$gitHubBotUserName,
+    [Parameter()][string]$gitUserEmail=$gitHubBotEmail,
+    [Parameter()][string]$targetBranch="main"
+  )
+
+  $match = [regex]::Match($tag, '^(.*?-)(.*)$')
+  if ($match.Success -eq $false)
+  {
+      throw 'Could not parse prefix from tag'
+  }
+
+  $tagPrefix = $match.Groups[1].Value
+  $version = $match.Groups[2].Value
+
+  $branch="release/post-stable-${tag}-update"
+
+  git config user.name $gitUserName
+  git config user.email $gitUserEmail
+
+  git switch --create $branch origin/$targetBranch --no-track 2>&1 | % ToString
+  if ($LASTEXITCODE -gt 0)
+  {
+      throw 'git switch failure'
+  }
+
+  $versionTagRegex = "<PackageValidationBaselineVersion>.*</PackageValidationBaselineVersion>"
+
+  $projects = Get-ChildItem -Path src/**/*.csproj |
+    where { $_ | Select-String "<MinVerTagPrefix>$tagPrefix</MinVerTagPrefix>" } |
+    where { $_ | Select-String $versionTagRegex }
+
+  if ($projects.Length -ne 0)
+  {
+    foreach ($project in $projects) {
+      (Get-Content $project) `
+          -replace $versionTagRegex, "<PackageValidationBaselineVersion>$version</PackageValidationBaselineVersion>" |
+        Set-Content $project
+
+      git add $project 2>&1 | % ToString
+      if ($LASTEXITCODE -gt 0)
+      {
+          throw 'git add failure'
+      }
+    }
+  }
+
+  $diabledTag = "<DisablePackageBaselineValidation>true</DisablePackageBaselineValidation>"
+
+  $disabledProjects = Get-ChildItem -Path src/**/*.csproj |
+    where { $_ | Select-String "<MinVerTagPrefix>$tagPrefix</MinVerTagPrefix>" } |
+    where { $_ | Select-String $diabledTag }
+
+  if ($disabledProjects.Length -ne 0)
+  {
+    foreach ($project in $disabledProjects) {
+      $content = (Get-Content $project -Raw)
+
+      $content = $content -replace $diabledTag, "<PackageValidationBaselineVersion>$version</PackageValidationBaselineVersion>"
+
+      $content = $content -replace "<!--\s*?Do not run Package Baseline Validation[\S\s]*?-->\s*", ""
+
+      Set-Content $project -Value $content.TrimEnd()
+
+      git add $project 2>&1 | % ToString
+      if ($LASTEXITCODE -gt 0)
+      {
+          throw 'git add failure'
+      }
+    }
+  }
+
+  git commit -m "Update PackageValidationBaselineVersion in $tagPrefix projects to $version." 2>&1 | % ToString
+  if ($LASTEXITCODE -gt 0)
+  {
+      throw 'git commit failure'
+  }
+
+  git push -u origin $branch 2>&1 | % ToString
+  if ($LASTEXITCODE -gt 0)
+  {
+      throw 'git push failure'
+  }
+
+  $body =
+@"
+Note: This PR was opened automatically by the [package workflow](https://github.com/$gitRepository/actions/workflows/Component.Package.yml).
+
+Merge once packages are available on NuGet and the build passes.
+
+## Changes
+
+* Sets ``PackageValidationBaselineVersion`` in ``$tagPrefix`` projects to ``$version``.
+"@
+
+  gh pr create `
+    --title "[repo] $tagPrefix stable release $version updates" `
+    --body $body `
+    --base $targetBranch `
+    --head $branch `
+    --label infra
+}
+
+Export-ModuleMember -Function CreatePackageValidationBaselineVersionUpdatePullRequest
