7665900a51ea7fecd6e5461a9a7da8cebcb9c805
Matt Hensley <130569+matt-hensley@users.noreply.github.com>
2024-05-21 08:03:50 -0400

    [Resources.Host] Rename host detector namespace (#1820)
    Co-authored-by: Piotr Kiełkowicz <pkiekowicz@splunk.com>

---
+++ src/OpenTelemetry.Resources.Host/HostResourceBuilderExtensions.cs
@@ -0,0 +1,24 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+using OpenTelemetry.Internal;
+using OpenTelemetry.Resources.Host;
+
+namespace OpenTelemetry.Resources;
+
+/// <summary>
+/// Extension methods to simplify registering of host resource detectors.
+/// </summary>
+public static class HostResourceBuilderExtensions
+{
+    /// <summary>
+    /// Enables host resource detector.
+    /// </summary>
+    /// <param name="builder"><see cref="ResourceBuilder" /> being configured.</param>
+    /// <returns>The instance of <see cref="ResourceBuilder" /> being configured.</returns>
+    public static ResourceBuilder AddHostDetector(this ResourceBuilder builder)
+    {
+        Guard.ThrowIfNull(builder);
+        return builder.AddDetector(new HostDetector());
+    }
+}
