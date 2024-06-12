4ade0e24615184d3f56999b3a62a859068fc51f8
yiranw <wangyiran29@gmail.com>
2024-06-11 21:56:41 -0700

    [Instrumentation.AWS] Update AWS SDK Activity Tags (#1857) (#1865)

--- test/OpenTelemetry.Instrumentation.AWS.Tests/TestAWSClientInstrumentation.cs
+++ test/OpenTelemetry.Instrumentation.AWS.Tests/TestAWSClientInstrumentation.cs
@@ -217,6 +217,9 @@ public class TestAWSClientInstrumentation
         Assert.Equal("us-east-1", Utils.GetTagValue(ddb_activity, "aws.region"));
         Assert.Equal("SampleProduct", Utils.GetTagValue(ddb_activity, "aws.table_name"));
         Assert.Equal("dynamodb", Utils.GetTagValue(ddb_activity, "db.system"));
+        Assert.Equal("aws-api", Utils.GetTagValue(ddb_activity, "rpc.system"));
+        Assert.Equal("DynamoDB", Utils.GetTagValue(ddb_activity, "rpc.service"));
+        Assert.Equal("Scan", Utils.GetTagValue(ddb_activity, "rpc.method"));
     }
 
     private void ValidateSqsActivityTags(Activity sqs_activity)
@@ -226,5 +229,8 @@ public class TestAWSClientInstrumentation
         Assert.Equal("SendMessage", Utils.GetTagValue(sqs_activity, "aws.operation"));
         Assert.Equal("us-east-1", Utils.GetTagValue(sqs_activity, "aws.region"));
         Assert.Equal("https://sqs.us-east-1.amazonaws.com/123456789/MyTestQueue", Utils.GetTagValue(sqs_activity, "aws.queue_url"));
+        Assert.Equal("aws-api", Utils.GetTagValue(sqs_activity, "rpc.system"));
+        Assert.Equal("SQS", Utils.GetTagValue(sqs_activity, "rpc.service"));
+        Assert.Equal("SendMessage", Utils.GetTagValue(sqs_activity, "rpc.method"));
     }
 }
