495ec402e68a243f21afb2d5a188feb3c6d593f6
joegoldman2 <147369450+joegoldman@users.noreply.github.com>
2024-06-24 19:49:27 +0000

    Fix

--- src/OpenTelemetry.Instrumentation.AspNetCore/AspNetCoreInstrumentationMeterProviderBuilderExtensions.cs
+++ src/OpenTelemetry.Instrumentation.AspNetCore/AspNetCoreInstrumentationMeterProviderBuilderExtensions.cs
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-#if !NET
+#if !NET8_0_OR_GREATER
 using OpenTelemetry.Instrumentation.AspNetCore;
 using OpenTelemetry.Instrumentation.AspNetCore.Implementation;
 #endif
@@ -24,7 +24,7 @@ public static class AspNetCoreInstrumentationMeterProviderBuilderExtensions
     {
         Guard.ThrowIfNull(builder);
 
-#if NET
+#if NET8_0_OR_GREATER
         return builder.ConfigureMeters();
 #else
         // Note: Warm-up the status code and method mapping.
