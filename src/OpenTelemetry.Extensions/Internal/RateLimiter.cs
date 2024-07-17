9d2100346677b306e76181951ab48dddff329731
Sam Spencer <samsp@microsoft.com>
2024-07-17 16:17:08 -0700

    Adds a RateLimiting sampler based on the Jaeger sampler for Java.
    https://github.com/open-telemetry/opentelemetry-java/blob/main/sdk-extensions/jaeger-remote-sampler/src/main/java/io/opentelemetry/sdk/extension/trace/jaeger/sampler/RateLimitingSampler.java

---
+++ src/OpenTelemetry.Extensions/Internal/RateLimiter.cs
@@ -0,0 +1,46 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+using System.Diagnostics;
+
+namespace OpenTelemetry;
+
+internal class RateLimiter
+{
+    private readonly Stopwatch stopwatch = Stopwatch.StartNew();
+    private readonly double creditsPerNanosecond;
+    private readonly long maxBalance; // max balance in ticks
+    private long currentBalance; // last op ticks less remaining balance, using long directly with Interlocked for thread safety
+
+    public RateLimiter(double creditsPerSecond, double maxBalance)
+    {
+        this.creditsPerNanosecond = creditsPerSecond / Stopwatch.Frequency;
+        this.maxBalance = (long)(maxBalance / this.creditsPerNanosecond);
+        this.currentBalance = this.stopwatch.ElapsedTicks - this.maxBalance;
+    }
+
+    public bool TrySpend(double itemCost)
+    {
+        long cost = (long)(itemCost / this.creditsPerNanosecond);
+        long currentNanos;
+        long currentBalanceNanos;
+        long availableBalanceAfterWithdrawal;
+        do
+        {
+            currentBalanceNanos = Interlocked.Read(ref this.currentBalance);
+            currentNanos = this.stopwatch.ElapsedTicks;
+            long currentAvailableBalance = currentNanos - currentBalanceNanos;
+            if (currentAvailableBalance > this.maxBalance)
+            {
+                currentAvailableBalance = this.maxBalance;
+            }
+
+            availableBalanceAfterWithdrawal = currentAvailableBalance - cost;
+            if (availableBalanceAfterWithdrawal < 0)
+            {
+                return false;
+            }
+        }
+        while (Interlocked.CompareExchange(ref this.currentBalance, currentNanos - availableBalanceAfterWithdrawal, currentBalanceNanos) != currentBalanceNanos);
+        return true;
+    }
+}
