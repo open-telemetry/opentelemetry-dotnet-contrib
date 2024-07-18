c898dcd0a3dc816a618af1b2bedbb9af185e2192
Yevhenii Solomchenko <ysolomchenko@splunk.com>
2024-07-18 11:15:51 +0200

    Update Operating System Detector Tests

--- test/OpenTelemetry.Resources.OperatingSystem.Tests/OperatingSystemDetectorTests.cs
+++ test/OpenTelemetry.Resources.OperatingSystem.Tests/OperatingSystemDetectorTests.cs
@@ -1,6 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+using System.Runtime.InteropServices;
 using Xunit;
 
 namespace OpenTelemetry.Resources.OperatingSystem.Test;
@@ -21,10 +22,18 @@ public class OperatingSystemDetectorTests
             OperatingSystemSemanticConventions.OperatingSystemsValues.Darwin,
         };
 
+        var expectedPlatform =
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? OperatingSystemSemanticConventions.OperatingSystemsValues.Windows :
+            RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? OperatingSystemSemanticConventions.OperatingSystemsValues.Linux :
+            RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? OperatingSystemSemanticConventions.OperatingSystemsValues.Darwin :
+            throw new PlatformNotSupportedException("Unknown platform");
+
         Assert.Single(resourceAttributes);
 
         Assert.True(resourceAttributes.ContainsKey(OperatingSystemSemanticConventions.AttributeOperatingSystemType));
 
         Assert.Contains(resourceAttributes[OperatingSystemSemanticConventions.AttributeOperatingSystemType], operatingSystems);
+
+        Assert.Equal(resourceAttributes[OperatingSystemSemanticConventions.AttributeOperatingSystemType], expectedPlatform);
     }
 }
