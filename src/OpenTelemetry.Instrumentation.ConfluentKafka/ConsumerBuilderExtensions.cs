78cbef6f3b0fef3a7ce64d34fe7ac86729f09200
g7ed6e <guillaume.delahaye@outlook.com>
2024-07-25 10:52:36 +0200

    Add AsInstrumentedProducerBuilder

--- src/OpenTelemetry.Instrumentation.ConfluentKafka/ConsumerBuilderExtensions.cs
+++ src/OpenTelemetry.Instrumentation.ConfluentKafka/ConsumerBuilderExtensions.cs
@@ -2,12 +2,38 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using Confluent.Kafka;
-using OpenTelemetry.Trace;
 
 namespace OpenTelemetry.Instrumentation.ConfluentKafka;
 
+/// <summary>
+/// Extensions for <see cref="ConsumerBuilder{TKey, TValue}"/>.
+/// </summary>
 public static class ConsumerBuilderExtensions
 {
+    /// <summary>
+    /// Converts a <see cref="ConsumerBuilder{TKey, TValue}"/> to an <see cref="InstrumentedConsumerBuilder{TKey, TValue}"/>.
+    /// </summary>
+    /// <typeparam name="TKey">Type of the key.</typeparam>
+    /// <typeparam name="TValue">Type of the value.</typeparam>
+    /// <param name="consumerBuilder">The <see cref="ConsumerBuilder{TKey, TValue}"/> instance.</param>
+    /// <returns>An <see cref="InstrumentedConsumerBuilder{TKey, TValue}"/> instance.</returns>
+    public static InstrumentedConsumerBuilder<TKey, TValue> AsInstrumentedConsumerBuilder<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder)
+    {
+        InstrumentedConsumerBuilder<TKey, TValue> result = new InstrumentedConsumerBuilder<TKey, TValue>(consumerBuilder.GetInternalConfig() ?? Enumerable.Empty<KeyValuePair<string, string>>());
+        result.SetInternalErrorHandler(consumerBuilder.GetInternalErrorHandler());
+        result.SetInternalLogHandler(consumerBuilder.GetInternalLogHandler());
+        result.SetInternalStatisticsHandler(consumerBuilder.GetInternalStatisticsHandler());
+        result.SetInternalOAuthBearerTokenRefreshHandler(consumerBuilder.GetInternalOAuthBearerTokenRefreshHandler());
+        result.SetInternalOffsetsCommittedHandler(consumerBuilder.GetInternalOffsetsCommittedHandler());
+        result.SetInternalPartitionsAssignedHandler(consumerBuilder.GetInternalPartitionsAssignedHandler());
+        result.SetInternalPartitionsRevokedHandler(consumerBuilder.GetInternalPartitionsRevokedHandler());
+        result.SetInternalPartitionsLostHandler(consumerBuilder.GetInternalPartitionsLostHandler());
+        result.SetInternalRevokedOrLostHandlerIsFunc(consumerBuilder.GetInternalRevokedOrLostHandlerIsFunc() ?? false);
+        result.SetInternalKeyDeserializer(consumerBuilder.GetInternalKeyDeserializer());
+        result.SetInternalValueDeserializer(consumerBuilder.GetInternalValueDeserializer());
+        return result;
+    }
+
     internal static IEnumerable<KeyValuePair<string, string>>? GetInternalConfig<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder)
         => ReflectionHelpers.GetProperty(consumerBuilder, "Config") as IEnumerable<KeyValuePair<string, string>>;
 
@@ -44,53 +70,36 @@ public static class ConsumerBuilderExtensions
     internal static bool? GetInternalRevokedOrLostHandlerIsFunc<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder)
         => ReflectionHelpers.GetField(consumerBuilder, "RevokedOrLostHandlerIsFunc") as bool?;
 
-    internal static void SetInternalErrorHandler<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, Action<IConsumer<TKey, TValue>, Error> value)
+    internal static void SetInternalErrorHandler<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, Action<IConsumer<TKey, TValue>, Error>? value)
         => ReflectionHelpers.SetProperty(consumerBuilder, "ErrorHandler", value);
 
-    internal static void SetInternalLogHandler<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, Action<IConsumer<TKey, TValue>, LogMessage> value)
+    internal static void SetInternalLogHandler<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, Action<IConsumer<TKey, TValue>, LogMessage>? value)
         => ReflectionHelpers.SetProperty(consumerBuilder, "LogHandler", value);
 
-    internal static void SetInternalStatisticsHandler<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, Action<IConsumer<TKey, TValue>, string> value)
+    internal static void SetInternalStatisticsHandler<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, Action<IConsumer<TKey, TValue>, string>? value)
         => ReflectionHelpers.SetProperty(consumerBuilder, "StatisticsHandler", value);
 
-    internal static void SetInternalOAuthBearerTokenRefreshHandler<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, Action<IConsumer<TKey, TValue>, string> value)
+    internal static void SetInternalOAuthBearerTokenRefreshHandler<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, Action<IConsumer<TKey, TValue>, string>? value)
         => ReflectionHelpers.SetProperty(consumerBuilder, "OAuthBearerTokenRefreshHandler", value);
 
-    internal static void SetInternalOffsetsCommittedHandler<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, Action<IConsumer<TKey, TValue>, CommittedOffsets> value)
+    internal static void SetInternalOffsetsCommittedHandler<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, Action<IConsumer<TKey, TValue>, CommittedOffsets>? value)
         => ReflectionHelpers.SetProperty(consumerBuilder, "OffsetsCommittedHandler", value);
 
-    internal static void SetInternalPartitionsAssignedHandler<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, Func<IConsumer<TKey, TValue>, List<TopicPartition>, IEnumerable<TopicPartitionOffset>> value)
+    internal static void SetInternalPartitionsAssignedHandler<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, Func<IConsumer<TKey, TValue>, List<TopicPartition>, IEnumerable<TopicPartitionOffset>>? value)
         => ReflectionHelpers.SetProperty(consumerBuilder, "PartitionsAssignedHandler", value);
 
-    internal static void SetInternalPartitionsRevokedHandler<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, Func<IConsumer<TKey, TValue>, List<TopicPartitionOffset>, IEnumerable<TopicPartitionOffset>> value)
+    internal static void SetInternalPartitionsRevokedHandler<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, Func<IConsumer<TKey, TValue>, List<TopicPartitionOffset>, IEnumerable<TopicPartitionOffset>>? value)
         => ReflectionHelpers.SetProperty(consumerBuilder, "PartitionsRevokedHandler", value);
 
-    internal static void SetInternalPartitionsLostHandler<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, Action<IConsumer<TKey, TValue>, List<TopicPartitionOffset>, IEnumerable<TopicPartitionOffset>> value)
+    internal static void SetInternalPartitionsLostHandler<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, Action<IConsumer<TKey, TValue>, List<TopicPartitionOffset>, IEnumerable<TopicPartitionOffset>>? value)
         => ReflectionHelpers.SetProperty(consumerBuilder, "PartitionsLostHandler", value);
 
-    internal static void SetInternalKeyDeserializer<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, IDeserializer<TKey> value)
+    internal static void SetInternalKeyDeserializer<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, IDeserializer<TKey>? value)
         => ReflectionHelpers.SetProperty(consumerBuilder, "KeyDeserializer", value);
 
-    internal static void SetInternalValueDeserializer<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, IDeserializer<TValue> value)
+    internal static void SetInternalValueDeserializer<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, IDeserializer<TValue>? value)
         => ReflectionHelpers.SetProperty(consumerBuilder, "ValueDeserializer", value);
 
-    internal static void SetInternalRevokedOrLostHandlerIsFunc<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, bool value)
+    internal static void SetInternalRevokedOrLostHandlerIsFunc<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder, bool? value)
         => ReflectionHelpers.SetField(consumerBuilder, "RevokedOrLostHandlerIsFunc", value);
-
-    public static InstrumentedConsumerBuilder<TKey, TValue> AsInstrumentedConsumerBuilder<TKey, TValue>(this ConsumerBuilder<TKey, TValue> consumerBuilder)
-    {
-        InstrumentedConsumerBuilder<TKey, TValue> result = new InstrumentedConsumerBuilder<TKey, TValue>(consumerBuilder.GetInternalConfig());
-        result.SetInternalErrorHandler(consumerBuilder.GetInternalErrorHandler());
-        result.SetInternalLogHandler(consumerBuilder.GetInternalLogHandler());
-        result.SetInternalStatisticsHandler(consumerBuilder.GetInternalStatisticsHandler());
-        result.SetInternalOAuthBearerTokenRefreshHandler(consumerBuilder.GetInternalOAuthBearerTokenRefreshHandler());
-        result.SetInternalOffsetsCommittedHandler(consumerBuilder.GetInternalOffsetsCommittedHandler());
-        result.SetInternalPartitionsAssignedHandler(consumerBuilder.GetInternalPartitionsAssignedHandler());
-        result.SetInternalPartitionsRevokedHandler(consumerBuilder.GetInternalPartitionsRevokedHandler());
-        result.SetInternalPartitionsLostHandler(consumerBuilder.GetInternalPartitionsLostHandler());
-        result.SetInternalRevokedOrLostHandlerIsFunc(consumerBuilder.GetInternalRevokedOrLostHandlerIsFunc() ?? false);
-        result.SetInternalKeyDeserializer(consumerBuilder.GetInternalKeyDeserializer());
-        result.SetInternalValueDeserializer(consumerBuilder.GetInternalValueDeserializer());
-        return result;
-    }
 }
