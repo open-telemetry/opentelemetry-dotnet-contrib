61a18642b3d530c91eb42c1cbc41cda10e4214ff
Yevhenii Solomchenko <ysolomchenko@splunk.com>
2024-07-29 11:55:52 +0200

    Implement additional osdetector attributes

--- src/OpenTelemetry.Resources.OperatingSystem/OperatingSystemDetector.cs
+++ src/OpenTelemetry.Resources.OperatingSystem/OperatingSystemDetector.cs
@@ -2,7 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #if !NETFRAMEWORK
+using System.Diagnostics;
 using System.Runtime.InteropServices;
+
 #endif
 using static OpenTelemetry.Resources.OperatingSystem.OperatingSystemSemanticConventions;
 
@@ -19,17 +21,50 @@ internal sealed class OperatingSystemDetector : IResourceDetector
     /// <returns>Resource with key-value pairs of resource attributes.</returns>
     public Resource Detect()
     {
+        var attributes = new List<KeyValuePair<string, object>>();
         var osType = GetOSType();
-
         if (osType == null)
         {
             return Resource.Empty;
         }
 
-        return new Resource(
-        [
-            new(AttributeOperatingSystemType, osType),
-        ]);
+        attributes.Add(new KeyValuePair<string, object>(AttributeOperatingSystemType, osType));
+
+        AddAttributeIfNotNullOrEmpty(attributes, AttributeOperatingSystemDescription, GetOSDescription());
+
+        switch (osType)
+        {
+            case OperatingSystemsValues.Windows:
+            AddAttributeIfNotNullOrEmpty(attributes, AttributeOperatingSystemBuildId, GetWindowsBuildId());
+            AddAttributeIfNotNullOrEmpty(attributes, AttributeOperatingSystemName, "Windows");
+            AddAttributeIfNotNullOrEmpty(attributes, AttributeOperatingSystemVersion, GetWindowsVersion());
+            break;
+#if !NETFRAMEWORK
+            case OperatingSystemsValues.Linux:
+            AddAttributeIfNotNullOrEmpty(attributes, AttributeOperatingSystemBuildId, GetLinuxBuildId());
+            AddAttributeIfNotNullOrEmpty(attributes, AttributeOperatingSystemName, GetLinuxDistributionName());
+            AddAttributeIfNotNullOrEmpty(attributes, AttributeOperatingSystemVersion, GetLinuxVersion());
+            break;
+            case OperatingSystemsValues.Darwin:
+            AddAttributeIfNotNullOrEmpty(attributes, AttributeOperatingSystemBuildId, GetMacOSBuildId());
+            AddAttributeIfNotNullOrEmpty(attributes, AttributeOperatingSystemName, "MacOS");
+            AddAttributeIfNotNullOrEmpty(attributes, AttributeOperatingSystemVersion, GetMacOSVersion());
+            break;
+#endif
+        }
+
+        return new Resource(attributes);
+    }
+
+    private static void AddAttributeIfNotNullOrEmpty(List<KeyValuePair<string, object>> attributes, string key, object? value)
+    {
+        if (value is string strValue && string.IsNullOrEmpty(strValue))
+        {
+            OperatingSystemResourcesEventSource.Log.FailedToValidateValue("The provided value string is null or empty.");
+            return;
+        }
+
+        attributes.Add(new KeyValuePair<string, object>(key, value!));
     }
 
     private static string? GetOSType()
@@ -55,4 +90,211 @@ internal sealed class OperatingSystemDetector : IResourceDetector
         }
 #endif
     }
+
+    private static string? GetWindowsBuildId()
+    {
+        var registryKey = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion";
+        var buildLabValue = "BuildLab";
+        var buildLabExValue = "BuildLabEx";
+
+        try
+        {
+            using (var key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(registryKey))
+            {
+                if (key != null)
+                {
+                    var buildLab = key.GetValue(buildLabValue)?.ToString();
+                    var buildLabEx = key.GetValue(buildLabExValue)?.ToString();
+                    return !string.IsNullOrEmpty(buildLabEx) ? buildLabEx : buildLab;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            OperatingSystemResourcesEventSource.Log.ResourceAttributesExtractException("Failed to get Windows build ID", ex);
+        }
+
+        return null;
+    }
+
+#if !NETFRAMEWORK
+    private static string? GetLinuxBuildId()
+    {
+        try
+        {
+            var osReleaseContent = File.ReadAllText("/etc/os-release");
+            foreach (var line in osReleaseContent.Split('\n'))
+            {
+                if (line.StartsWith("BUILD_ID=", StringComparison.Ordinal))
+                {
+                    return line.Substring("BUILD_ID=".Length).Trim('"');
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            OperatingSystemResourcesEventSource.Log.ResourceAttributesExtractException("Failed to get Linux build ID", ex);
+
+        }
+
+        return null;
+    }
+
+    private static string? GetMacOSBuildId()
+    {
+        try
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "sw_vers",
+                Arguments = "-buildVersion",
+                RedirectStandardOutput = true,
+            };
+            using (var process = Process.Start(psi))
+            {
+                if (process != null)
+                {
+                    var output = process.StandardOutput.ReadToEnd().Trim();
+                    return output;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            OperatingSystemResourcesEventSource.Log.ResourceAttributesExtractException("Failed to get MacOS build ID", ex);
+        }
+
+        return null;
+    }
+#endif
+
+    private static string GetOSDescription()
+    {
+#if !NETFRAMEWORK
+        return RuntimeInformation.OSDescription;
+#else
+        return Environment.OSVersion.ToString();
+#endif
+    }
+
+    private static string GetLinuxDistributionName()
+    {
+        try
+        {
+            var osReleaseFile = "/etc/os-release";
+            if (File.Exists(osReleaseFile))
+            {
+                var lines = File.ReadAllLines(osReleaseFile);
+                foreach (var line in lines)
+                {
+                    if (line.StartsWith("NAME=", StringComparison.Ordinal))
+                    {
+                        return line.Substring("NAME=".Length).Trim('"');
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            OperatingSystemResourcesEventSource.Log.ResourceAttributesExtractException("Failed to get Linux distribution name", ex);
+        }
+
+        return "Linux";
+    }
+
+    private static string? GetWindowsVersion()
+    {
+        try
+        {
+            var registryKey = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion";
+            using (var key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(registryKey))
+            {
+                if (key != null)
+                {
+                    var currentVersion = key.GetValue("CurrentVersion")?.ToString();
+                    var currentBuild = key.GetValue("CurrentBuild")?.ToString();
+                    var ubr = key.GetValue("UBR")?.ToString();
+                    return $"{currentVersion}.{currentBuild}.{ubr}";
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            OperatingSystemResourcesEventSource.Log.ResourceAttributesExtractException("Failed to get Windows version", ex);
+        }
+
+        return null;
+    }
+
+#if !NETFRAMEWORK
+    private static string? GetMacOSVersion()
+    {
+        try
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "sw_vers",
+                Arguments = "-productVersion",
+                RedirectStandardOutput = true,
+            };
+            using (var process = Process.Start(psi))
+            {
+                if (process != null)
+                {
+                    var output = process.StandardOutput.ReadToEnd().Trim();
+                    return output;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            OperatingSystemResourcesEventSource.Log.ResourceAttributesExtractException("Failed to get MacOS version", ex);
+        }
+
+        return null;
+    }
+
+    private static string? GetLinuxVersion()
+    {
+        try
+        {
+            var osReleaseFile = "/etc/os-release";
+            if (File.Exists(osReleaseFile))
+            {
+                var lines = File.ReadAllLines(osReleaseFile);
+                foreach (var line in lines)
+                {
+                    if (line.StartsWith("VERSION_ID=", StringComparison.Ordinal))
+                    {
+                        return line.Substring("VERSION_ID=".Length).Trim('"');
+                    }
+                }
+            }
+
+            using (var process = new Process())
+            {
+                process.StartInfo = new ProcessStartInfo
+                {
+                    FileName = "uname",
+                    Arguments = "-r",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                };
+
+                process.Start();
+                var output = process.StandardOutput.ReadToEnd().Trim();
+                process.WaitForExit();
+                return output;
+            }
+        }
+        catch (Exception ex)
+        {
+            OperatingSystemResourcesEventSource.Log.ResourceAttributesExtractException("Failed to get Linux version", ex);
+        }
+
+        return null;
+    }
+
+#endif
 }
--- test/OpenTelemetry.Resources.OperatingSystem.Tests/OperatingSystemDetectorTests.cs
+++ test/OpenTelemetry.Resources.OperatingSystem.Tests/OperatingSystemDetectorTests.cs
@@ -12,32 +12,48 @@ public class OperatingSystemDetectorTests
     public void TestOperatingSystemAttributes()
     {
         var resource = ResourceBuilder.CreateEmpty().AddOperatingSystemDetector().Build();
-
         var resourceAttributes = resource.Attributes.ToDictionary(x => x.Key, x => (string)x.Value);
 
         string expectedPlatform;
+        string expectedDescription;
+        string expectedName;
 
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
             expectedPlatform = OperatingSystemSemanticConventions.OperatingSystemsValues.Windows;
+            expectedDescription = "Windows";
+            expectedName = "Windows";
         }
         else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
         {
             expectedPlatform = OperatingSystemSemanticConventions.OperatingSystemsValues.Linux;
+            expectedDescription = "Linux";
+            expectedName = "Linux";
         }
         else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
         {
             expectedPlatform = OperatingSystemSemanticConventions.OperatingSystemsValues.Darwin;
+            expectedDescription = "Darwin";
+            expectedName = "MacOS";
         }
         else
         {
             throw new PlatformNotSupportedException("Unknown platform");
         }
 
-        Assert.Single(resourceAttributes);
-
+        Assert.Equal(5, resourceAttributes.Count);
+        Assert.True(resourceAttributes.ContainsKey(OperatingSystemSemanticConventions.AttributeOperatingSystemDescription));
+        Assert.True(resourceAttributes.ContainsKey(OperatingSystemSemanticConventions.AttributeOperatingSystemName));
         Assert.True(resourceAttributes.ContainsKey(OperatingSystemSemanticConventions.AttributeOperatingSystemType));
+        Assert.True(resourceAttributes.ContainsKey(OperatingSystemSemanticConventions.AttributeOperatingSystemBuildId));
+        Assert.True(resourceAttributes.ContainsKey(OperatingSystemSemanticConventions.AttributeOperatingSystemVersion));
 
-        Assert.Equal(resourceAttributes[OperatingSystemSemanticConventions.AttributeOperatingSystemType], expectedPlatform);
+        if (expectedName != "Linux")
+        {
+            Assert.Contains(expectedDescription, resourceAttributes[OperatingSystemSemanticConventions.AttributeOperatingSystemDescription]);
+            Assert.Equal(expectedName, resourceAttributes[OperatingSystemSemanticConventions.AttributeOperatingSystemName]);
+        }
+
+        Assert.Equal(expectedPlatform, resourceAttributes[OperatingSystemSemanticConventions.AttributeOperatingSystemType]);
     }
 }
