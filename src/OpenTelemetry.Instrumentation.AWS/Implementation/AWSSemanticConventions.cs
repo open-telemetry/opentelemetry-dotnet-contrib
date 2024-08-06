7042e6d29320e8f8f6fe47e326690027f3250360
Eric Zhang <zhaez@amazon.com>
2024-08-06 22:20:24 +0000

    add implementation for Bedrock Agent

--- src/OpenTelemetry.Instrumentation.AWS/Implementation/AWSSemanticConventions.cs
+++ src/OpenTelemetry.Instrumentation.AWS/Implementation/AWSSemanticConventions.cs
@@ -16,8 +16,8 @@ internal static class AWSSemanticConventions
     // AWS Bedrock service attributes not yet defined in semantic conventions
     public const string AttributeAWSBedrockGuardrailId = "aws.bedrock.guardrail.id";
     public const string AttributeAWSBedrockAgentId = "aws.bedrock.agent.id";
-    public const string AttributeAWSBedrockKnowledgeBaseId = "aws.bedrock.knowledgebase.id";
-    public const string AttributeAWSBedrockDataSourceId = "aws.bedrock.datasource.id";
+    public const string AttributeAWSBedrockKnowledgeBaseId = "aws.bedrock.knowledge_base.id";
+    public const string AttributeAWSBedrockDataSourceId = "aws.bedrock.data_source.id";
 
     // should be global convention for Gen AI attributes
     public const string AttributeGenAiModelId = "gen_ai.request.model";
