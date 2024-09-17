27c3bfb33c31bac4a8c4fa19a6f0483b9a208ee4
Yevhenii Solomchenko <ysolomchenko@splunk.com>
2024-09-17 12:33:20 +0200

    DI support for configuring EventCounters

--- src/OpenTelemetry.Instrumentation.EventCounters/MeterProviderBuilderExtensions.cs
+++ src/OpenTelemetry.Instrumentation.EventCounters/MeterProviderBuilderExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+using Microsoft.Extensions.Configuration;
 using OpenTelemetry.Instrumentation.EventCounters;
 using OpenTelemetry.Internal;
 
@@ -15,16 +16,41 @@ public static class MeterProviderBuilderExtensions
     /// Enables EventCounter instrumentation.
     /// </summary>
     /// <param name="builder"><see cref="MeterProviderBuilder"/> being configured.</param>
+    /// <param name="configuration">The configuration section used to configure <see cref="EventCountersInstrumentationOptions"/>.</param>
+    /// <returns>The instance of <see cref="MeterProviderBuilder"/> to chain the calls.</returns>
+    public static MeterProviderBuilder AddEventCountersInstrumentation(
+        this MeterProviderBuilder builder,
+        IConfigurationSection configuration)
+    => builder.AddEventCountersInstrumentation(null, configuration);
+
+    /// <summary>
+    /// Enables EventCounter instrumentation.
+    /// </summary>
+    /// <param name="builder"><see cref="MeterProviderBuilder"/> being configured.</param>
     /// <param name="configure">EventCounters instrumentation options.</param>
     /// <returns>The instance of <see cref="MeterProviderBuilder"/> to chain the calls.</returns>
     public static MeterProviderBuilder AddEventCountersInstrumentation(
         this MeterProviderBuilder builder,
-        Action<EventCountersInstrumentationOptions>? configure = null)
+        Action<EventCountersInstrumentationOptions> configure)
+    => builder.AddEventCountersInstrumentation(configure, null);
+
+    /// <summary>
+    /// Enables EventCounter instrumentation using configuration.
+    /// </summary>
+    /// <param name="builder">The <see cref="MeterProviderBuilder"/> being configured.</param>
+    /// <param name="configure">EventCounters instrumentation options.</param>
+    /// <param name="configuration">The configuration section used to configure <see cref="EventCountersInstrumentationOptions"/>.</param>
+    /// <returns>The instance of <see cref="MeterProviderBuilder"/> to chain the calls.</returns>
+    private static MeterProviderBuilder AddEventCountersInstrumentation(
+        this MeterProviderBuilder builder,
+        Action<EventCountersInstrumentationOptions>? configure = null,
+        IConfigurationSection? configuration = null)
     {
         Guard.ThrowIfNull(builder);
 
         var options = new EventCountersInstrumentationOptions();
         configure?.Invoke(options);
+        configuration?.Bind(options);
 
         builder.AddMeter(EventCountersMetrics.MeterInstance.Name);
         return builder.AddInstrumentation(() => new EventCountersMetrics(options));
