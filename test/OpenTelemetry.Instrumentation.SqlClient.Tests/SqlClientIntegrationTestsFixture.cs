e9e5f3d0134d6b95b2ed8dba4ee6fbba7c06ca69
Mikel Blanchard <mblanchard@macrosssoftware.com>
2024-09-20 14:06:31 -0700

    Tweaks.

--- test/OpenTelemetry.Instrumentation.SqlClient.Tests/SqlClientIntegrationTestsFixture.cs
+++ test/OpenTelemetry.Instrumentation.SqlClient.Tests/SqlClientIntegrationTestsFixture.cs
@@ -12,7 +12,7 @@ namespace OpenTelemetry.Instrumentation.SqlClient.Tests;
 public sealed class SqlClientIntegrationTestsFixture : IAsyncLifetime
 {
     // The Microsoft SQL Server Docker image is not compatible with ARM devices, such as Macs with Apple Silicon.
-    private readonly IContainer databaseContainer = Architecture.Arm64.Equals(RuntimeInformation.ProcessArchitecture) ? new SqlEdgeBuilder().Build() : new MsSqlBuilder().Build();
+    private readonly IContainer databaseContainer = Architecture.Arm64.Equals(RuntimeInformation.ProcessArchitecture) ? CreateSqlEdge() : CreateMsSql();
 
     public IContainer DatabaseContainer => this.databaseContainer;
 
@@ -24,5 +24,24 @@ public sealed class SqlClientIntegrationTestsFixture : IAsyncLifetime
     public Task DisposeAsync()
     {
         return this.databaseContainer.DisposeAsync().AsTask();
+    }
+
+    private static SqlEdgeContainer CreateSqlEdge()
+    {
+        // Note: The Testcontainers.SqlEdge package has been deprecated. Seems
+        // it will not work with newer GItHub-hosted runners. Need to find an
+        // alternative solution. See:
+        // https://github.com/testcontainers/testcontainers-dotnet/pull/1265
+        return new SqlEdgeBuilder().Build();
+    }
+
+    private static MsSqlContainer CreateMsSql()
+    {
+        // Note: This "WithImage" line can most likely be removed when there is
+        // a new version (>3.10.0) of Testcontainers.MsSql released. See:
+        // https://github.com/testcontainers/testcontainers-dotnet/pull/1265
+        return new MsSqlBuilder()
+            .WithImage("mcr.microsoft.com/mssql/server:2022-CU14-ubuntu-22.04")
+            .Build();
     }
 }
