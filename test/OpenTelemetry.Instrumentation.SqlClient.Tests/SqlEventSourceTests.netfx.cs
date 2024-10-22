ca6de997553352a3ab1d6c5b9713c3f7436abea9
Alan West <3676547+alanwest@users.noreply.github.com>
2024-10-21 17:23:36 -0700

    I :heart: .NET Framework

--- test/OpenTelemetry.Instrumentation.SqlClient.Tests/SqlEventSourceTests.netfx.cs
+++ test/OpenTelemetry.Instrumentation.SqlClient.Tests/SqlEventSourceTests.netfx.cs
@@ -130,7 +130,8 @@ public class SqlEventSourceTests
 
         int objectId = Guid.NewGuid().GetHashCode();
 
-        fakeSqlEventSource.WriteBeginExecuteEvent(objectId, "127.0.0.1\\instanceName,port", "master", commandType == CommandType.StoredProcedure ? commandText : string.Empty);
+        var dataSource = "127.0.0.1\\instanceName,port";
+        fakeSqlEventSource.WriteBeginExecuteEvent(objectId, dataSource, "master", commandType == CommandType.StoredProcedure ? commandText : string.Empty);
 
         // success is stored in the first bit in compositeState 0b001
         int successFlag = !isFailure ? 1 : 0;
@@ -149,7 +150,7 @@ public class SqlEventSourceTests
 
         var activity = exportedItems[0];
 
-        VerifyActivityData(commandText, captureText, isFailure, "127.0.0.1", activity, enableConnectionLevelAttributes, emitOldAttributes, emitNewAttributes);
+        VerifyActivityData(commandText, captureText, isFailure, dataSource, activity, enableConnectionLevelAttributes, emitOldAttributes, emitNewAttributes);
     }
 
     [Theory]
