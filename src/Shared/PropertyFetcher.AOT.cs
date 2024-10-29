174edc949207c2261d77c55d022af4948555fb06
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2024-10-29 08:43:54 +0100

    Fix CA2263 - property fetcher

--- src/Shared/PropertyFetcher.AOT.cs
+++ src/Shared/PropertyFetcher.AOT.cs
@@ -184,7 +184,11 @@ internal sealed class PropertyFetcher<T>
             public PropertyFetchInstantiated(PropertyInfo property)
             {
                 this.propertyName = property.Name;
+#if NET
+                this.propertyFetch = property.GetMethod!.CreateDelegate<Func<TDeclaredObject, T>>();
+#else
                 this.propertyFetch = (Func<TDeclaredObject, T>)property.GetMethod!.CreateDelegate(typeof(Func<TDeclaredObject, T>));
+#endif
             }
 
             public override int NumberOfInnerFetchers => this.innerFetcher == null
