63bd3f4f4c6d575c758a79ce022e503e089b9b7c
philip pittle <pittle@amazon.com>
2024-12-03 23:24:45 -0800

    Alternate strategy to simplify implementation

--- src/OpenTelemetry.Instrumentation.AWS/TracerProviderBuilderExtensions.cs
+++ src/OpenTelemetry.Instrumentation.AWS/TracerProviderBuilderExtensions.cs
@@ -3,6 +3,7 @@
 
 using Amazon;
 using Amazon.Runtime.Telemetry;
+using OpenTelemetry.AWS;
 using OpenTelemetry.Instrumentation.AWS;
 using OpenTelemetry.Instrumentation.AWS.Implementation;
 using OpenTelemetry.Instrumentation.AWS.Implementation.Tracing;
@@ -37,6 +38,8 @@ public static class TracerProviderBuilderExtensions
 
         var awsClientOptions = new AWSClientInstrumentationOptions();
         configure?.Invoke(awsClientOptions);
+
+        AWSSemanticConventions.SemanticConventionVersion = awsClientOptions.SemanticConventionVersion;
 
         _ = new AWSClientsInstrumentation(awsClientOptions);
 
