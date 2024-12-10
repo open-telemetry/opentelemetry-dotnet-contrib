efe6eda4375ebf64cb652bf18669d6cd53f8ab11
Alan West <3676547+alanwest@users.noreply.github.com>
2024-12-10 14:59:56 -0800

    [aspnetcore] Restore metrics instrumentation in netstandard builds (#2403)

---
+++ src/OpenTelemetry.Instrumentation.AspNetCore/AspNetCoreMetrics.cs
@@ -0,0 +1,41 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+#if !NET
+using OpenTelemetry.Instrumentation.AspNetCore.Implementation;
+
+namespace OpenTelemetry.Instrumentation.AspNetCore;
+
+/// <summary>
+/// Asp.Net Core Requests instrumentation.
+/// </summary>
+internal sealed class AspNetCoreMetrics : IDisposable
+{
+    private static readonly HashSet<string> DiagnosticSourceEvents =
+    [
+        "Microsoft.AspNetCore.Hosting.HttpRequestIn",
+        "Microsoft.AspNetCore.Hosting.HttpRequestIn.Start",
+        "Microsoft.AspNetCore.Hosting.HttpRequestIn.Stop",
+        "Microsoft.AspNetCore.Diagnostics.UnhandledException",
+        "Microsoft.AspNetCore.Hosting.UnhandledException"
+    ];
+
+    private readonly Func<string, object?, object?, bool> isEnabled = (eventName, _, _)
+        => DiagnosticSourceEvents.Contains(eventName);
+
+    private readonly DiagnosticSourceSubscriber diagnosticSourceSubscriber;
+
+    internal AspNetCoreMetrics()
+    {
+        var metricsListener = new HttpInMetricsListener("Microsoft.AspNetCore");
+        this.diagnosticSourceSubscriber = new DiagnosticSourceSubscriber(metricsListener, this.isEnabled, AspNetCoreInstrumentationEventSource.Log.UnknownErrorProcessingEvent);
+        this.diagnosticSourceSubscriber.Subscribe();
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        this.diagnosticSourceSubscriber?.Dispose();
+    }
+}
+#endif
--- src/OpenTelemetry.Instrumentation.AspNetCore/CHANGELOG.md
+++ src/OpenTelemetry.Instrumentation.AspNetCore/CHANGELOG.md
@@ -2,13 +2,16 @@
 
 ## Unreleased
 
+* Metric support for the .NET Standard target was removed by mistake in 1.10.0.
+  This functionality has been restored.
+  ([#2403](https://github.com/open-telemetry/opentelemetry-dotnet-contrib/pull/2403))
+
 ## 1.10.0
 
 Released 2024-Dec-09
 
 * Drop support for .NET 6 as this target is no longer supported.
-  ([#2138](https://github.com/open-telemetry/opentelemetry-dotnet-contrib/pull/2138),
-  ([#2360](https://github.com/open-telemetry/opentelemetry-dotnet-contrib/pull/2360))
+  ([#2138](https://github.com/open-telemetry/opentelemetry-dotnet-contrib/pull/2138))
 
 * Updated OpenTelemetry core component version(s) to `1.10.0`.
   ([#2317](https://github.com/open-telemetry/opentelemetry-dotnet-contrib/pull/2317))
