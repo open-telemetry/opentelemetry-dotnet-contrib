85256a41f25e2ae4e77ac078740b22d11503cd11
RassK <rkuusmann@splunk.com>
2025-06-12 16:32:27 +0300

    update tests

--- test/OpenTelemetry.OpAMPClient.Tests/AnyValueUnionTests.cs
+++ test/OpenTelemetry.OpAMPClient.Tests/AnyValueUnionTests.cs
@@ -11,16 +11,23 @@ public class AnyValueUnionTests
     [Fact]
     public void AnyValueUnion_EqualityTests()
     {
-        var intValue1 = new AnyValueUnion(AnyValueType.Integer, intValue: 42);
+        var intValue = new AnyValueUnion(AnyValueType.Integer, intValue: 42);
         var intValue2 = new AnyValueUnion(AnyValueType.Integer, intValue: 42);
         var boolValue = new AnyValueUnion(AnyValueType.Boolean, boolValue: true);
+        var boolValue2 = new AnyValueUnion(AnyValueType.Boolean, boolValue: true);
         var stringValue = new AnyValueUnion(AnyValueType.String, stringValue: "test");
+        var stringValue2 = new AnyValueUnion(AnyValueType.String, stringValue: "test");
         var doubleValue = new AnyValueUnion(AnyValueType.Double, doubleValue: 3.14);
+        var doubleValue2 = new AnyValueUnion(AnyValueType.Double, doubleValue: 3.14);
 
-        Assert.Equal(intValue1, intValue2);
-        Assert.NotEqual(intValue1, boolValue);
-        Assert.NotEqual(intValue1, stringValue);
-        Assert.NotEqual(intValue1, doubleValue);
+        Assert.Equal(intValue, intValue2);
+        Assert.Equal(boolValue, boolValue2);
+        Assert.Equal(stringValue, stringValue2);
+        Assert.Equal(doubleValue, doubleValue2);
+
+        Assert.NotEqual(intValue, boolValue);
+        Assert.NotEqual(intValue, stringValue);
+        Assert.NotEqual(intValue, doubleValue);
         Assert.NotEqual(boolValue, stringValue);
         Assert.NotEqual(boolValue, doubleValue);
         Assert.NotEqual(stringValue, doubleValue);
@@ -33,10 +40,12 @@ public class AnyValueUnionTests
         var boolValue = new AnyValueUnion(AnyValueType.Boolean, boolValue: true);
         var stringValue = new AnyValueUnion(AnyValueType.String, stringValue: "test");
         var doubleValue = new AnyValueUnion(AnyValueType.Double, doubleValue: 3.14);
+
         Assert.Equal(intValue.GetHashCode(), new AnyValueUnion(AnyValueType.Integer, intValue: 42).GetHashCode());
         Assert.Equal(boolValue.GetHashCode(), new AnyValueUnion(AnyValueType.Boolean, boolValue: true).GetHashCode());
         Assert.Equal(stringValue.GetHashCode(), new AnyValueUnion(AnyValueType.String, stringValue: "test").GetHashCode());
         Assert.Equal(doubleValue.GetHashCode(), new AnyValueUnion(AnyValueType.Double, doubleValue: 3.14).GetHashCode());
+
         Assert.NotEqual(intValue.GetHashCode(), boolValue.GetHashCode());
         Assert.NotEqual(intValue.GetHashCode(), stringValue.GetHashCode());
         Assert.NotEqual(intValue.GetHashCode(), doubleValue.GetHashCode());
