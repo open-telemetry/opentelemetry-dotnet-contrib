0d9fab4a478600a9113dde2fd91635b44f7f462b
James Thompson <thompson.tomo@outlook.com>
2025-07-17 18:12:32 +1000

    Update logic and tests to decrease string manipulation

--- test/OpenTelemetry.Resources.ProcessRuntime.Tests/ProcessRuntimeDetectorTests.cs
+++ test/OpenTelemetry.Resources.ProcessRuntime.Tests/ProcessRuntimeDetectorTests.cs
@@ -17,13 +17,19 @@ public class ProcessRuntimeDetectorTests
         Assert.Equal(3, resourceAttributes.Count);
 
 #if NETFRAMEWORK
+        Assert.IsType<string>(resourceAttributes[ProcessRuntimeSemanticConventions.AttributeProcessRuntimeDescription]);
         Assert.Matches(@"^\.NET Framework \d+\.\d+\.\d+\.\d+$", (string)resourceAttributes[ProcessRuntimeSemanticConventions.AttributeProcessRuntimeDescription]);
+        Assert.IsType<string>(resourceAttributes[ProcessRuntimeSemanticConventions.AttributeProcessRuntimeName]);
         Assert.Equal(".NET Framework", resourceAttributes[ProcessRuntimeSemanticConventions.AttributeProcessRuntimeName]);
-        Assert.Matches(@"^4.[1-9](.[1-2])?$", (string)resourceAttributes[ProcessRuntimeSemanticConventions.AttributeProcessRuntimeVersion]);
+        Assert.IsType<string>(resourceAttributes[ProcessRuntimeSemanticConventions.AttributeProcessRuntimeVersion]);
+        Assert.Matches(@"^4.(0.0|[5-9](.[1-2]))?$", (string)resourceAttributes[ProcessRuntimeSemanticConventions.AttributeProcessRuntimeVersion]);
 #else
+        Assert.IsType<string>(resourceAttributes[ProcessRuntimeSemanticConventions.AttributeProcessRuntimeDescription]);
         Assert.Matches(@"^\.NET \d+\.\d+\.\d+(\-(preview|rc)\.\d+\.\d+\.\d+)?$", (string)resourceAttributes[ProcessRuntimeSemanticConventions.AttributeProcessRuntimeDescription]);
+        Assert.IsType<string>(resourceAttributes[ProcessRuntimeSemanticConventions.AttributeProcessRuntimeName]);
         Assert.Equal(".NET", resourceAttributes[ProcessRuntimeSemanticConventions.AttributeProcessRuntimeName]);
-        Assert.Matches(@"^\d+\.\d+\.\d+$", (string)resourceAttributes[ProcessRuntimeSemanticConventions.AttributeProcessRuntimeVersion]);
+        Assert.IsType<string>(resourceAttributes[ProcessRuntimeSemanticConventions.AttributeProcessRuntimeVersion]);
+        Assert.Matches(@"^(([1-3,5-9])|(\d{2,}))\.\d+\.\d+$", (string)resourceAttributes[ProcessRuntimeSemanticConventions.AttributeProcessRuntimeVersion]);
 #endif
     }
 }
