0faa969f1e3ce95a29b296977378491fa25bb2f8
Arkadiusz Biel <bielu@bielu.com.pl>
2025-07-18 11:57:00 +0100

    Update src/OpenTelemetry.Exporter.Stackdriver/StackdriverTraceExporter.cs
    Co-authored-by: Martin Costello <martin@martincostello.com>

--- src/OpenTelemetry.Exporter.Stackdriver/StackdriverTraceExporter.cs
+++ src/OpenTelemetry.Exporter.Stackdriver/StackdriverTraceExporter.cs
@@ -14,7 +14,7 @@ namespace OpenTelemetry.Exporter.Stackdriver;
 /// <summary>
 /// Exports a group of spans to Stackdriver.
 /// </summary>
-#if NET // todo: link to GCP .net documentation when becomes available
+#if NET // TODO: link to GCP .net documentation when becomes available
 [Obsolete("This exporter is deprecated and will be removed in a future version. Please use the native OTLP  within GCP.", UrlFormat="https://cloud.google.com/stackdriver/docs/reference/telemetry/overview")]
 #else
 [Obsolete("This exporter is deprecated and will be removed in a future version. Please use the native OTLP  within GCP.")]
