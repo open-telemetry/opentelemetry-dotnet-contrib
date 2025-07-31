861fced391360f7e7f2dae3e411a6bb159e5ea27
Austin Drenski <austin@austindrenski.io>
2025-07-31 17:32:40 -0400

    Make everything a little more concrete
    Notable changes:
    
    1. No more custom ItemGroupDefinition for OtelResourceAttributes
    
    2. Users can still pass their own resource attributes using the
       official AssemblyMetadata item with `otel:`-prefixed keys
    
    3. Users can explicitly pass an `Assembly` to the resource builder
       or pass nothing to default to `Assembly.GetEntryAssembly()`
    
    4. Lots of rework in the `.targets` file:
    
       1. Define an in-the-box `GetOtelVcsResourceAttributes` target
          Additional custom targets can be added by following the same
          pattern.
    
       2. We can provide some in-the-box, but users can also add their
          own to fit their needs. For now we can probably focus on the
          `vcs.*` resource attributes since those line up icely with
          existing MSBuild properties populated defined by SourceLink
          and/or ambiently available in GitHub Actions.
    
       3. Make it easy for users to disable specific targets:
          - vcs.*  -> GetOtelVcsResourceAttributes=false
    
       4. Populate from OTEL_* environment variable first, then fallback
          to detecting from other sources (SourceLink, GitHub Actions)

--- test/OpenTelemetry.Resources.AssemblyMetadata.Tests/AssemblyMetadataDetectorTests.cs
+++ test/OpenTelemetry.Resources.AssemblyMetadata.Tests/AssemblyMetadataDetectorTests.cs
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using System.Reflection;
+using OpenTelemetry.SemanticConventions;
+using OpenTelemetry.Tests;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -66,5 +68,96 @@ public sealed class AssemblyMetadataDetectorTests(ITestOutputHelper outputHelper
         var resource = ResourceBuilder.CreateEmpty().AddAssemblyMetadataDetector().Build();
 
         Assert.Empty(resource.Attributes);
+    }
+
+    [Fact]
+    public void TestAssemblyMetadataAttributesWithLocalSourceLink()
+    {
+        // Check if our `.targets` applied SourceLink variables as expected. This test is only expected to run locally.
+        if (Environment.GetEnvironmentVariable("CI") is null)
+        {
+            return;
+        }
+
+        var assembly = typeof(AssemblyMetadataDetectorTests).Assembly;
+        var resource = ResourceBuilder.CreateEmpty().AddAssemblyMetadataDetector(assembly).Build();
+        var attributes = resource.Attributes.ToDictionary();
+
+        Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsOwnerName, out var vcsOwnerName));
+        Assert.Empty(Assert.IsType<string>(vcsOwnerName));
+
+        Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsProviderName, out var vcsProviderName));
+        Assert.Empty(Assert.IsType<string>(vcsProviderName));
+
+        Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsRefHeadName, out var vcsRefHeadName));
+        Assert.Empty(Assert.IsType<string>(vcsRefHeadName));
+
+        Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsRefHeadRevision, out var vcsRefHeadRevision));
+        Assert.NotEmpty(Assert.IsType<string>(vcsRefHeadRevision));
+
+        Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsRefHeadType, out var vcsRefHeadType));
+        Assert.True(Assert.IsType<string>(vcsRefHeadType) is VcsAttributes.VcsRefHeadTypeValues.Branch or VcsAttributes.VcsRefHeadTypeValues.Tag);
+
+        Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsRefType, out var vcsRefType));
+        Assert.True(Assert.IsType<string>(vcsRefType) is VcsAttributes.VcsRefTypeValues.Branch or VcsAttributes.VcsRefTypeValues.Tag);
+
+        Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsRepositoryName, out var vcsRepositoryName));
+        Assert.Empty(Assert.IsType<string>(vcsRepositoryName));
+
+        Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsRepositoryUrlFull, out var vcsRepositoryUrlFull));
+        Assert.Empty(Assert.IsType<string>(vcsRepositoryUrlFull));
+    }
+
+    [SkipUnlessEnvVarFoundFact("GITHUB_ACTIONS")]
+    public void TestAssemblyMetadataAttributesWithGitHubActions()
+    {
+        var assembly = typeof(AssemblyMetadataDetectorTests).Assembly;
+        var resource = ResourceBuilder.CreateEmpty().AddAssemblyMetadataDetector(assembly).Build();
+        var attributes = resource.Attributes.ToDictionary();
+
+        if (Environment.GetEnvironmentVariable("GITHUB_EVENT_NAME") is "pull_request")
+        {
+            Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsChangeId, out var vcsChangeId));
+            Assert.Equal(Environment.GetEnvironmentVariable("GITHUB_REF"), $"refs/pull/{Assert.IsType<string>(vcsChangeId)}/merge");
+        }
+
+        Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsOwnerName, out var vcsOwnerName));
+        Assert.Equal(Environment.GetEnvironmentVariable("GITHUB_REPOSITORY_OWNER"), Assert.IsType<string>(vcsOwnerName));
+
+        Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsProviderName, out var vcsProviderName));
+        Assert.Equal(VcsAttributes.VcsProviderNameValues.Github, Assert.IsType<string>(vcsProviderName));
+
+        if (Environment.GetEnvironmentVariable("GITHUB_BASE_REF") is [_, ..] githubBaseRef)
+        {
+            Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsRefBaseName, out var vcsRefBaseName));
+            Assert.Equal(githubBaseRef, Assert.IsType<string>(vcsRefBaseName));
+
+            Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsRefBaseType, out var vcsRefBaseType));
+            Assert.True(Assert.IsType<string>(vcsRefBaseType) is VcsAttributes.VcsRefBaseTypeValues.Branch or VcsAttributes.VcsRefBaseTypeValues.Tag);
+        }
+
+        Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsRefHeadName, out var vcsRefHeadName));
+        Assert.Equal(
+            Environment.GetEnvironmentVariable("GITHUB_HEAD_REF") ?? Environment.GetEnvironmentVariable("GITHUB_REF_NAME"),
+            Assert.IsType<string>(vcsRefHeadName));
+
+        Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsRefHeadRevision, out var vcsRefHeadRevision));
+        Assert.Equal(Environment.GetEnvironmentVariable("GITHUB_SHA"), Assert.IsType<string>(vcsRefHeadRevision));
+
+        Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsRefHeadType, out var vcsRefHeadType));
+        Assert.Equal(Environment.GetEnvironmentVariable("GITHUB_REF_TYPE"), Assert.IsType<string>(vcsRefHeadType));
+        Assert.True(Assert.IsType<string>(vcsRefHeadType) is VcsAttributes.VcsRefHeadTypeValues.Branch or VcsAttributes.VcsRefHeadTypeValues.Tag);
+
+        Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsRefType, out var vcsRefType));
+        Assert.Equal(Environment.GetEnvironmentVariable("GITHUB_REF_TYPE"), Assert.IsType<string>(vcsRefType));
+        Assert.True(Assert.IsType<string>(vcsRefType) is VcsAttributes.VcsRefTypeValues.Branch or VcsAttributes.VcsRefTypeValues.Tag);
+
+        Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsRepositoryName, out var vcsRepositoryName));
+        Assert.Equal(Environment.GetEnvironmentVariable("GITHUB_REPOSITORY")?.Split('/')[1], Assert.IsType<string>(vcsRepositoryName));
+
+        Assert.True(attributes.TryGetValue(VcsAttributes.AttributeVcsRepositoryUrlFull, out var vcsRepositoryUrlFull));
+        Assert.Equal(
+            $"{Environment.GetEnvironmentVariable("GITHUB_SERVER_URL")}/{Environment.GetEnvironmentVariable("GITHUB_REPOSITORY")}",
+            Assert.IsType<string>(vcsRepositoryUrlFull));
     }
 }
