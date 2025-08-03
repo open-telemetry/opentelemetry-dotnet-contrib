de05286f81738ad2fc09101b14bdcfe429f78575
James Thompson <thompson.tomo@outlook.com>
2025-08-04 00:45:01 +1000

    further cleanup

--- src/OpenTelemetry.Resources.Azure/AzureVmMetadataResponse.cs
+++ src/OpenTelemetry.Resources.Azure/AzureVmMetadataResponse.cs
@@ -17,17 +17,8 @@ internal sealed class AzureVmMetadataResponse
     [JsonPropertyName("osType")]
     public string? OsType { get; set; }
 
-    [JsonPropertyName("resourceGroupName")]
-    public string? ResourceGroupName { get; set; }
-
     [JsonPropertyName("resourceId")]
     public string? ResourceId { get; set; }
-
-    [JsonPropertyName("sku")]
-    public string? Sku { get; set; }
-
-    [JsonPropertyName("subscriptionId")]
-    public string? SubscriptionId { get; set; }
 
     [JsonPropertyName("version")]
     public string? Version { get; set; }
