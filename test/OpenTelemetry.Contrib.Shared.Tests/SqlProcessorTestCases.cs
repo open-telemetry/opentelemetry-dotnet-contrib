ff8b1c5df18599a548fcb272d09e7e44c14315d9
Steve Gordon <sgordon@hotmail.co.uk>
2025-08-21 19:01:37 +0100

    Sql summary tests (#3023)

--- test/OpenTelemetry.Contrib.Shared.Tests/SqlProcessorTestCases.cs
+++ test/OpenTelemetry.Contrib.Shared.Tests/SqlProcessorTestCases.cs
@@ -4,6 +4,8 @@
 using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace OpenTelemetry.Instrumentation.Tests;
 
@@ -15,36 +17,84 @@ public static class SqlProcessorTestCases
         Converters = { new JsonStringEnumConverter() },
     };
 
-    public static IEnumerable<object[]> GetTestCases()
+    private static readonly HashSet<string> DbSystemTestCasesToExecute = ["other_sql"];
+
+    public static TheoryData<TestCase> GetSemanticConventionsTestCases()
     {
         var assembly = Assembly.GetExecutingAssembly();
         var input = JsonSerializer.Deserialize<TestCase[]>(
             assembly.GetManifestResourceStream("SqlProcessorTestCases.json")!,
             JsonSerializerOptions)!;
 
-        foreach (var testCase in input)
+        var data = new TheoryData<TestCase>();
+
+        if (input is not null)
         {
-            yield return new object[] { testCase };
+            foreach (var testCase in input)
+            {
+                if (DbSystemTestCasesToExecute.Contains(testCase.Input.DbSystemName))
+                {
+                    data.Add(testCase);
+                }
+            }
         }
+
+        return data;
     }
 
 #pragma warning disable CA1034 // Nested types should not be visible
-    public class TestCase
-#pragma warning restore CA1034
+    public class TestCase : IXunitSerializable
     {
         public string Name { get; set; } = string.Empty;
 
-        public string Sql { get; set; } = string.Empty;
+        public TestCaseInput Input { get; set; } = new();
 
-        public string Sanitized { get; set; } = string.Empty;
+        public TestCaseExpected Expected { get; set; } = new();
 
-        public string? Summary { get; set; }
-
-        public IEnumerable<string> Dialects { get; set; } = [];
-
-        public override string ToString()
+        public void Deserialize(IXunitSerializationInfo info)
         {
-            return this.Name;
+            var json = info.GetValue<string>("json") ?? string.Empty;
+
+            var proxy = JsonSerializer.Deserialize<TestCase>(json, JsonSerializerOptions);
+
+            if (proxy is not null)
+            {
+                this.Name = proxy.Name;
+                this.Input = proxy.Input;
+                this.Expected = proxy.Expected;
+            }
+            else
+            {
+                throw new InvalidOperationException("Deserialized TestCase is null.");
+            }
         }
+
+        public void Serialize(IXunitSerializationInfo info)
+        {
+            var json = JsonSerializer.Serialize(this, JsonSerializerOptions);
+            info.AddValue("json", json);
+        }
+
+        public override string ToString() => this.Name;
     }
+
+    public class TestCaseInput
+    {
+        [JsonPropertyName("db.system.name")]
+        public string DbSystemName { get; set; } = string.Empty;
+
+        public string Query { get; set; } = string.Empty;
+    }
+
+    public class TestCaseExpected
+    {
+        [JsonPropertyName("db.query.text")]
+#pragma warning disable CA1819 // Properties should not return arrays
+        public string[] SanitizedQueryText { get; set; } = [];
+#pragma warning restore CA1819
+
+        [JsonPropertyName("db.query.summary")]
+        public string? Summary { get; set; }
+    }
+#pragma warning restore CA1034
 }
