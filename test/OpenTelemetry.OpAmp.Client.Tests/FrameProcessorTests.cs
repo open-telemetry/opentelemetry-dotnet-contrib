5704be8c96008bcec3047c5618a1f70f638fd71d
RassK <rkuusmann@splunk.com>
2025-09-01 12:50:41 +0300

    Fix opamp client concurrency test

--- test/OpenTelemetry.OpAmp.Client.Tests/FrameProcessorTests.cs
+++ test/OpenTelemetry.OpAmp.Client.Tests/FrameProcessorTests.cs
@@ -20,7 +20,7 @@ public class FrameProcessorTests
         processor.OnServerFrame(mockFrame.Frame);
 
         Assert.Single(listener.Messages);
-        Assert.Equal(mockFrame.ExptectedContent, listener.Messages[0].CustomMessage.Data.ToStringUtf8());
+        Assert.Equal(mockFrame.ExptectedContent, listener.Messages.First().CustomMessage.Data.ToStringUtf8());
     }
 
     [Fact]
