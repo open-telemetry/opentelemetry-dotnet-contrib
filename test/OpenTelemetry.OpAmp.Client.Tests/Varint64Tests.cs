73491d0cacb53781d545bc8bf42bdfc582097f30
RassK <rkuusmann@splunk.com>
2025-09-09 12:43:13 +0300

    fix tests parameters language

--- test/OpenTelemetry.OpAmp.Client.Tests/Varint64Tests.cs
+++ test/OpenTelemetry.OpAmp.Client.Tests/Varint64Tests.cs
@@ -17,12 +17,12 @@ public class Varint64Tests
     [InlineData(128, new byte[] { 0x80, 0x01 })]
     [InlineData(987654321, new byte[] { 0xB1, 0xD1, 0xF9, 0xD6, 0x03 })]
     [InlineData(ulong.MaxValue, new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01 })]
-    public void OpAmpHeaderHelper_Encode(ulong value, byte[] verifiedEncoded)
+    public void OpAmpHeaderHelper_Encode(ulong value, byte[] expectedEncoded)
     {
         var encoded = Varint64.Encode(value);
 
-        Assert.Equal(verifiedEncoded.Length, encoded.Length);
-        Assert.Equal(verifiedEncoded, encoded);
+        Assert.Equal(expectedEncoded.Length, encoded.Length);
+        Assert.Equal(expectedEncoded, encoded);
     }
 
     [Theory]
@@ -32,13 +32,13 @@ public class Varint64Tests
     [InlineData(128, 2, new byte[] { 0x80, 0x01, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF })]
     [InlineData(987654321, 5, new byte[] { 0xB1, 0xD1, 0xF9, 0xD6, 0x03, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF })]
     [InlineData(ulong.MaxValue, 10, new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01 })]
-    public void OpAmpHeaderHelper_Decode(ulong value, int verifiedLength, byte[] buffer)
+    public void OpAmpHeaderHelper_Decode(ulong expectedValue, int expectedLength, byte[] buffer)
     {
         var sequence = new ReadOnlySequence<byte>(buffer);
         var decodeResult = Varint64.TryDecode(sequence, out int bytesRead, out ulong result, out string errorMessage);
 
         Assert.True(decodeResult, errorMessage);
-        Assert.Equal(value, result);
-        Assert.Equal(verifiedLength, bytesRead);
+        Assert.Equal(expectedValue, result);
+        Assert.Equal(expectedLength, bytesRead);
     }
 }
