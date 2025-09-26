b9e3bac9f3e6cb052e25edc8fa9d15d736b4b151
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2025-09-26 11:58:13 +0200

    Activity is created by instrumentation
    It is done to add url attribute on startup -> it depends on IConfiguration not available on IIS side

--- src/OpenTelemetry.Instrumentation.AspNet.TelemetryHttpModule/TelemetryHttpModuleOptions.cs
+++ src/OpenTelemetry.Instrumentation.AspNet.TelemetryHttpModule/TelemetryHttpModuleOptions.cs
@@ -35,9 +35,9 @@ public class TelemetryHttpModuleOptions
     }
 
     /// <summary>
-    /// Gets or sets a callback action to be fired when a request is started.
+    /// Gets or sets a callback function to be fired when a request is started.
     /// </summary>
-    public Action<Activity?, HttpContextBase>? OnRequestStartedCallback { get; set; }
+    public Func<HttpContextBase, ActivityContext, Activity?>? OnRequestStartedCallback { get; set; }
 
     /// <summary>
     /// Gets or sets a callback action to be fired when a request is stopped.
