49af439cc72cb4487bbe9f5db22726c307c2205b
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2025-11-13 08:45:20 +0100

    Fix BDN1301

--- test/OpenTelemetry.Instrumentation.Http.Benchmarks/Instrumentation/HttpClientInstrumentationBenchmarks.cs
+++ test/OpenTelemetry.Instrumentation.Http.Benchmarks/Instrumentation/HttpClientInstrumentationBenchmarks.cs
@@ -54,7 +54,7 @@ public class HttpClientInstrumentationBenchmarks
         Metrics = 2,
     }
 
-    [Params(0, 1, 2, 3)]
+    [Params(EnableInstrumentationOption.None, EnableInstrumentationOption.Traces, EnableInstrumentationOption.Metrics, EnableInstrumentationOption.Traces | EnableInstrumentationOption.Metrics)]
     public EnableInstrumentationOption EnableInstrumentation { get; set; }
 
     [GlobalSetup(Target = nameof(HttpClientRequest))]
