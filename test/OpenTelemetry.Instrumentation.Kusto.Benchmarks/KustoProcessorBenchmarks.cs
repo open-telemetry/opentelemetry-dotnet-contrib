8f2603d61f6e2836fb1159300606bb8cbf72de2b
Matt Kotsenas <51421+MattKotsenas@users.noreply.github.com>
2025-11-20 16:55:11 -0800

    Add Benchmarks project

---
+++ test/OpenTelemetry.Instrumentation.Kusto.Benchmarks/KustoProcessorBenchmarks.cs
@@ -0,0 +1,23 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnosers;
+using OpenTelemetry.Instrumentation.Kusto.Implementation;
+
+namespace OpenTelemetry.Instrumentation.Kusto.Benchmarks;
+
+[MemoryDiagnoser]
+public class KustoProcessorBenchmarks
+{
+    public string Query { get; set; } = "StormEvents | join kind=inner (PopulationData) on State | project State, EventType, Population";
+
+    [Benchmark]
+    public void ProcessSummarizeAndSanitize() => KustoProcessor.Process(shouldSummarize: true, shouldSanitize: true, this.Query);
+
+    [Benchmark]
+    public void ProcessSummarizeOnly() => KustoProcessor.Process(shouldSummarize: true, shouldSanitize: false, this.Query);
+
+    [Benchmark]
+    public void ProcessSanitizeOnly() => KustoProcessor.Process(shouldSummarize: false, shouldSanitize: true, this.Query);
+}
