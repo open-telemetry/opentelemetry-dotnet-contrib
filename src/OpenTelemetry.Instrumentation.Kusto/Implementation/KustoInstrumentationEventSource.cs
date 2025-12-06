8ad211e5d810117bc619b303700ba98d771d7e4a
Matt Kotsenas <matt.kotsenas@gmail.com>
2025-12-05 16:58:27 -0800

    Add additional logging for errors

--- src/OpenTelemetry.Instrumentation.Kusto/Implementation/KustoInstrumentationEventSource.cs
+++ src/OpenTelemetry.Instrumentation.Kusto/Implementation/KustoInstrumentationEventSource.cs
@@ -28,4 +28,31 @@ internal sealed class KustoInstrumentationEventSource : EventSource
     {
         this.WriteEvent(1, exception);
     }
+
+    [Event(2, Message = "Trace record payload is NULL or has NULL message, record will not be processed.", Level = EventLevel.Warning)]
+    public void NullPayload()
+    {
+        this.WriteEvent(2);
+    }
+
+    [Event(3, Message = "Failed to find context for activity ID '{0}', operation data will not be recorded.", Level = EventLevel.Warning)]
+    public void ContextNotFound(string activityId)
+    {
+        this.WriteEvent(3, activityId);
+    }
+
+    [NonEvent]
+    public void UnknownErrorProcessingTraceRecord(Exception ex)
+    {
+        if (this.IsEnabled(EventLevel.Error, EventKeywords.All))
+        {
+            this.UnknownErrorProcessingTraceRecord(ex.ToInvariantString());
+        }
+    }
+
+    [Event(4, Message = "Unknown error processing trace record, Exception: {0}", Level = EventLevel.Error)]
+    public void UnknownErrorProcessingTraceRecord(string exception)
+    {
+        this.WriteEvent(4, exception);
+    }
 }
