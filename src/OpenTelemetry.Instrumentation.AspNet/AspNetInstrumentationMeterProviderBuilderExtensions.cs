16d4f68ac5a656c9bb80b044af0683f7a61b10be
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2026-01-16 13:05:39 +0100

    [AspNet] Instrumentation reports telemetry schema URL v1.36.0 (#3686)

--- src/OpenTelemetry.Instrumentation.AspNet/AspNetInstrumentationMeterProviderBuilderExtensions.cs
+++ src/OpenTelemetry.Instrumentation.AspNet/AspNetInstrumentationMeterProviderBuilderExtensions.cs
@@ -49,7 +49,7 @@ public static class AspNetInstrumentationMeterProviderBuilderExtensions
                 {
                     return AspNetInstrumentation.Instance.HandleManager.AddMetricHandle();
                 });
-                meterProviderBuilder.AddMeter(AspNetInstrumentation.MeterName);
+                meterProviderBuilder.AddMeter(AspNetInstrumentation.Meter.Name);
             });
         });
     }
