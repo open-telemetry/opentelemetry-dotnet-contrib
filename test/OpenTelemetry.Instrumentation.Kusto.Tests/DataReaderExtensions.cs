a6516516fe7514807a78d6772243d60412d887fc
Matt Kotsenas <matt.kotsenas@gmail.com>
2026-01-21 15:40:48 -0800

    Address code review feedback

--- test/OpenTelemetry.Instrumentation.Kusto.Tests/DataReaderExtensions.cs
+++ test/OpenTelemetry.Instrumentation.Kusto.Tests/DataReaderExtensions.cs
@@ -7,10 +7,19 @@ namespace OpenTelemetry.Instrumentation.Kusto.Tests;
 
 internal static class DataReaderExtensions
 {
+    /// <summary>
+    /// Consumes all results from an <see cref="IDataReader"/>.
+    /// </summary>
+    /// <param name="reader">The <see cref="IDataReader"/> to consume.</param>
     public static void Consume(this IDataReader reader)
     {
-        while (reader.Read())
+        do
         {
+            while (reader.Read())
+            {
+                // Intentionally no-op
+            }
         }
+        while (reader.NextResult());
     }
 }
