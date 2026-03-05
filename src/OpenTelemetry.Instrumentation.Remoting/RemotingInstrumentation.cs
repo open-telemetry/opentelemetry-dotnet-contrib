8e8c1a0a2f540cec7d73dbe03c1f131e07d2de5d
Lewis Munro <l.munro@cmcmarkets.com>
2026-03-05 15:21:09 +1100

    [Remoting] fix: use Interlocked for dynamic sink registration count

--- src/OpenTelemetry.Instrumentation.Remoting/RemotingInstrumentation.cs
+++ src/OpenTelemetry.Instrumentation.Remoting/RemotingInstrumentation.cs
@@ -9,29 +9,24 @@ namespace OpenTelemetry.Instrumentation.Remoting;
 internal sealed class RemotingInstrumentation : IDisposable
 {
     internal static int RegCount;
-    private static readonly object LockObj = new();
     private static TelemetryDynamicSinkProvider? provider;
 
     public RemotingInstrumentation(RemotingInstrumentationOptions options)
     {
         // Just in case we are called multiple times, make sure we register
         // the dynamic sink only once per AppDomain
-        lock (LockObj)
+        int count = Interlocked.Increment(ref RegCount);
+        if (count == 1)
         {
-            if (RegCount == 0)
-            {
-                // See https://docs.microsoft.com/dotnet/api/system.runtime.remoting.contexts.context.registerdynamicproperty
-                // This will register our dynamic sink to listen to all calls leaving or entering
-                // current AppDomain.
-                provider = new TelemetryDynamicSinkProvider(options);
+            // See https://docs.microsoft.com/dotnet/api/system.runtime.remoting.contexts.context.registerdynamicproperty
+            // This will register our dynamic sink to listen to all calls leaving or entering
+            // current AppDomain.
+            provider = new TelemetryDynamicSinkProvider(options);
 
-                RemotingContext.RegisterDynamicProperty(
-                    provider,
-                    null,
-                    RemotingContext.DefaultContext);
-            }
-
-            RegCount++;
+            RemotingContext.RegisterDynamicProperty(
+                provider,
+                null,
+                RemotingContext.DefaultContext);
         }
     }
 
@@ -39,20 +34,17 @@ internal sealed class RemotingInstrumentation : IDisposable
     {
         // If there were multiple registration attempts, assume that each
         // of those registrations will also try to un-register.
-        lock (LockObj)
+        int count = Interlocked.Decrement(ref RegCount);
+        if (count == 0)
         {
-            RegCount--;
-            if (RegCount == 0)
-            {
-                // When the last registration disposes, remove the property.
-                RemotingContext.UnregisterDynamicProperty(
-                    TelemetryDynamicSinkProvider.DynamicPropertyName,
-                    null,
-                    RemotingContext.DefaultContext);
+            // When the last registration disposes, remove the property.
+            RemotingContext.UnregisterDynamicProperty(
+                TelemetryDynamicSinkProvider.DynamicPropertyName,
+                null,
+                RemotingContext.DefaultContext);
 
-                provider?.Dispose();
-                provider = null;
-            }
+            provider?.Dispose();
+            provider = null;
         }
     }
 }
