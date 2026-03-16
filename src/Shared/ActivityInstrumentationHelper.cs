85a5d8293464cb468a2f8400e8d60fc6741374f4
martincostello <martin@martincostello.com>
2026-03-16 14:57:26 +0000

    [Shared] Fix new code analysis warnings
    Cherry-pick fixes for new code analysis warnings identified by the .NET 11 preview 2 SDK in #3936.

--- src/Shared/ActivityInstrumentationHelper.cs
+++ src/Shared/ActivityInstrumentationHelper.cs
@@ -10,25 +10,21 @@ internal static class ActivityInstrumentationHelper
     internal static readonly Action<Activity, ActivityKind> SetKindProperty = CreateActivityKindSetter();
     internal static readonly Action<Activity, ActivitySource> SetActivitySourceProperty = CreateActivitySourceSetter();
 
-    private static Action<Activity, ActivitySource> CreateActivitySourceSetter()
-    {
+    private static Action<Activity, ActivitySource> CreateActivitySourceSetter() =>
 #if NET
-        return typeof(Activity).GetProperty("Source")!
+        typeof(Activity).GetProperty("Source")!
             .SetMethod!.CreateDelegate<Action<Activity, ActivitySource>>();
 #else
-        return (Action<Activity, ActivitySource>)typeof(Activity).GetProperty("Source")!
-            .SetMethod!.CreateDelegate(typeof(Action<Activity, ActivitySource>));
+        (Action<Activity, ActivitySource>)typeof(Activity).GetProperty("Source")
+            .SetMethod.CreateDelegate(typeof(Action<Activity, ActivitySource>));
 #endif
-    }
 
-    private static Action<Activity, ActivityKind> CreateActivityKindSetter()
-    {
+    private static Action<Activity, ActivityKind> CreateActivityKindSetter() =>
 #if NET
-        return typeof(Activity).GetProperty("Kind")!
+        typeof(Activity).GetProperty("Kind")!
             .SetMethod!.CreateDelegate<Action<Activity, ActivityKind>>();
 #else
-        return (Action<Activity, ActivityKind>)typeof(Activity).GetProperty("Kind")!
-            .SetMethod!.CreateDelegate(typeof(Action<Activity, ActivityKind>));
+        (Action<Activity, ActivityKind>)typeof(Activity).GetProperty("Kind")
+            .SetMethod.CreateDelegate(typeof(Action<Activity, ActivityKind>));
 #endif
-    }
 }
