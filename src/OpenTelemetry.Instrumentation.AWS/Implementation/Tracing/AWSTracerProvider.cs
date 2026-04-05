4472bbf2ee7400f3f37adf36946a7d000d1aef5d
martincostello <martin@martincostello.com>
2026-04-05 16:37:06 +0100

    [Instrumentation.AWS] Add schema URL
    - Add schema URL and version to AWS traces.
    - Add test to validate new version additions.

--- src/OpenTelemetry.Instrumentation.AWS/Implementation/Tracing/AWSTracerProvider.cs
+++ src/OpenTelemetry.Instrumentation.AWS/Implementation/Tracing/AWSTracerProvider.cs
@@ -4,24 +4,54 @@
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using Amazon.Runtime.Telemetry.Tracing;
+using OpenTelemetry.Internal;
 
 namespace OpenTelemetry.Instrumentation.AWS.Implementation.Tracing;
 
-internal sealed class AWSTracerProvider : TracerProvider
+internal sealed class AWSTracerProvider(SemanticConventionVersion version) : TracerProvider
 {
+    private static readonly string ActivitySourceVersion = GetActivitySourceVersion();
     private static readonly ConcurrentDictionary<string, AWSTracer> TracersDictionary = new();
+
+    private readonly string telemetrySchemaUrl = GetTelemetrySchemaUrl(version);
 
     public override Tracer GetTracer(string scope)
     {
-        if (TracersDictionary.TryGetValue(scope, out var awsTracer))
+        if (!TracersDictionary.TryGetValue(scope, out var awsTracer))
         {
-            return awsTracer;
-        }
+            awsTracer = TracersDictionary.GetOrAdd(
+                scope,
+                (name) =>
+                {
+                    var options = new ActivitySourceOptions(name)
+                    {
+                        TelemetrySchemaUrl = this.telemetrySchemaUrl,
+                        Version = ActivitySourceVersion,
+                    };
 
-        awsTracer = TracersDictionary.GetOrAdd(
-            scope,
-            new AWSTracer(new ActivitySource(scope)));
+                    return new AWSTracer(new ActivitySource(options));
+                });
+        }
 
         return awsTracer;
     }
+
+    private static string GetActivitySourceVersion()
+    {
+        var assembly = typeof(AWSTracerProvider).Assembly;
+        return assembly.GetPackageVersion();
+    }
+
+    private static string GetTelemetrySchemaUrl(SemanticConventionVersion version)
+    {
+        var versionString = GetSemanticConventionVersion(version);
+        return $"https://opentelemetry.io/schemas/{versionString}";
+    }
+
+    private static string GetSemanticConventionVersion(SemanticConventionVersion version) => version switch
+    {
+        SemanticConventionVersion.V1_29_0 or SemanticConventionVersion.Latest => "1.29.0",
+        SemanticConventionVersion.V1_28_0 => "1.28.0",
+        _ => throw new ArgumentOutOfRangeException(nameof(version), version, null),
+    };
 }
