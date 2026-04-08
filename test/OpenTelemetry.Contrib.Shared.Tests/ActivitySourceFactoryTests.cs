613fd8254269ffb3241c460fadaff66a2d4012a5
martincostello <martin@martincostello.com>
2026-04-08 16:47:30 +0100

    [Infra] Add factory for ActivitySource and Meter
    Add shared code to minimise duplication when creating instances of `ActivitySource` and `Meter` to help ensure that telemetry schema URLs are set.
    
    Contributes to #4064.

---
+++ test/OpenTelemetry.Contrib.Shared.Tests/ActivitySourceFactoryTests.cs
@@ -0,0 +1,27 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+using OpenTelemetry.Trace;
+using Xunit;
+
+namespace OpenTelemetry.Contrib.Shared.Tests;
+
+public class ActivitySourceFactoryTests
+{
+    [Fact]
+    public void Create_ReturnsActivitySourceWithGivenName()
+    {
+        // Arrange
+        var semanticConventionsVersion = new Version(1, 2, 3, 4);
+
+        // Act
+        var actual = ActivitySourceFactory.Create<ActivitySourceFactoryTests>(semanticConventionsVersion);
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.Equal("OpenTelemetry.Contrib.Shared.Tests", actual.Name);
+        Assert.NotNull(actual.Version);
+        Assert.NotEmpty(actual.Version);
+        Assert.Equal("https://opentelemetry.io/schemas/1.2.3", actual.TelemetrySchemaUrl);
+    }
+}
