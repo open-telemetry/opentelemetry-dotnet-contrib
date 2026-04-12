7f0f2a40975c502072b5eed13b177fefdaa8c55f
martincostello <martin@martincostello.com>
2026-04-12 09:42:32 +0100

    [Redis] Add benchmarks
    Add benchmarks for the Redis instrumentation.

---
+++ test/OpenTelemetry.Instrumentation.StackExchangeRedis.Benchmarks/RedisProfilerEntryToActivityConverterBenchmarks.cs
@@ -0,0 +1,58 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+using OpenTelemetry.Instrumentation.StackExchangeRedis.Implementation;
+
+namespace OpenTelemetry.Instrumentation.StackExchangeRedis.Benchmarks;
+
+[MemoryDiagnoser]
+public class RedisProfilerEntryToActivityConverterBenchmarks
+{
+    private ActivityListener? activityListener;
+    private Activity? parentActivity;
+    private StackExchangeRedisInstrumentationOptions? options;
+    private BenchmarkProfiledCommand? profiledCommand;
+
+    [Params(false, true)]
+    public bool EnrichActivityWithTimingEvents { get; set; }
+
+    [Params(false, true)]
+    public bool HasParentActivity { get; set; }
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        this.activityListener = new ActivityListener()
+        {
+            Sample = (ref _) => ActivitySamplingResult.AllDataAndRecorded,
+            SampleUsingParentId = (ref _) => ActivitySamplingResult.AllDataAndRecorded,
+            ShouldListenTo = (source) => source.Name == StackExchangeRedisConnectionInstrumentation.ActivitySourceName,
+        };
+
+        ActivitySource.AddActivityListener(this.activityListener);
+
+        this.options = new StackExchangeRedisInstrumentationOptions()
+        {
+            EnrichActivityWithTimingEvents = this.EnrichActivityWithTimingEvents,
+        };
+
+        this.profiledCommand = BenchmarkProfiledCommand.Create(DateTime.UtcNow, index: 0);
+
+        if (this.HasParentActivity)
+        {
+            this.parentActivity = new Activity("redis-parent");
+            this.parentActivity.SetIdFormat(ActivityIdFormat.W3C);
+            this.parentActivity.Start();
+            this.parentActivity.Stop();
+        }
+    }
+
+    [GlobalCleanup]
+    public void GlobalCleanup() => this.activityListener?.Dispose();
+
+    [Benchmark]
+    public Activity? ConvertCommand() =>
+        RedisProfilerEntryToActivityConverter.ProfilerCommandToActivity(this.parentActivity, this.profiledCommand!, this.options!);
+}
