a0c7656620de2d6bb5bab980ea07999dd28c4a13
martincostello <martin@martincostello.com>
2026-04-14 15:48:15 +0100

    [AWS] Fix IDE0005 warning
    Fix IDE0005 after rebase.

--- src/OpenTelemetry.Instrumentation.SqlClient/Implementation/SqlTelemetryHelper.cs
+++ src/OpenTelemetry.Instrumentation.SqlClient/Implementation/SqlTelemetryHelper.cs
@@ -3,7 +3,6 @@
 
 using System.Diagnostics;
 using System.Diagnostics.Metrics;
-using OpenTelemetry.Metrics;
 using OpenTelemetry.Trace;
 
 namespace OpenTelemetry.Instrumentation.SqlClient.Implementation;
