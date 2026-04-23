1fc9aad4939b5fb0b57251076212bd60d9869779
Santiago Blanco <sblancoleis2@outlook.com>
2026-04-23 12:23:34 -0700

    [Instrumentation.ServiceFabricRemoting] Revert ActivitySource refactor; will be addressed in a separate PR

--- src/OpenTelemetry.Instrumentation.ServiceFabricRemoting/ServiceFabricRemotingActivitySource.cs
+++ src/OpenTelemetry.Instrumentation.ServiceFabricRemoting/ServiceFabricRemotingActivitySource.cs
@@ -2,21 +2,23 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using System.Diagnostics;
-using OpenTelemetry.Trace;
+using System.Reflection;
+using OpenTelemetry.Internal;
 
 namespace OpenTelemetry.Instrumentation.ServiceFabricRemoting;
 
 internal class ServiceFabricRemotingActivitySource
 {
-    // Version of the OpenTelemetry RPC semantic conventions this instrumentation targets for tracing.
-    internal static readonly Version SemanticConventionsVersion = new(1, 40, 0);
+    internal static readonly Assembly Assembly = typeof(ServiceFabricRemotingActivitySource).Assembly;
+    internal static readonly AssemblyName AssemblyName = Assembly.GetName();
+#pragma warning disable IDE0370 // Suppression is unnecessary
+    internal static readonly string ActivitySourceName = AssemblyName.Name!;
+#pragma warning restore IDE0370 // Suppression is unnecessary
 
-    internal static readonly ActivitySource ActivitySource = ActivitySourceFactory.Create(
-        typeof(ServiceFabricRemotingActivitySource),
-        SemanticConventionsVersion);
+    internal static readonly string IncomingRequestActivityName = ActivitySourceName + ".IncomingRequest";
+    internal static readonly string OutgoingRequestActivityName = ActivitySourceName + ".OutgoingRequest";
 
-    internal static readonly string IncomingRequestActivityName = ActivitySource.Name + ".IncomingRequest";
-    internal static readonly string OutgoingRequestActivityName = ActivitySource.Name + ".OutgoingRequest";
+    public static ActivitySource ActivitySource { get; } = new(ActivitySourceName, Assembly.GetPackageVersion());
 
-    internal static ServiceFabricRemotingInstrumentationOptions? Options { get; set; }
+    public static ServiceFabricRemotingInstrumentationOptions? Options { get; set; }
 }
