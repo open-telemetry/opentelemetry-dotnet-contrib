b44118075b63297236cd193ce65bafc85815573f
martincostello <martin@martincostello.com>
2026-04-24 14:58:31 +0100

    [SqlClient] Polyfill Stopwatch.GetElapsedTime
    Add a polyfill for `Stopwatch.GetElapsedTime()` for `netstandard2.0` and `net462` for reuse in other instrumentation.

---
+++ src/Shared/StopwatchExtensions.cs
@@ -0,0 +1,25 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+#if !NET
+
+using System.Diagnostics;
+
+internal static class StopwatchExtensions
+{
+    extension(Stopwatch)
+    {
+        public static TimeSpan GetElapsedTime(long begin)
+        {
+            var end = Stopwatch.GetTimestamp();
+
+            var timestampToTicks = TimeSpan.TicksPerSecond / (double)Stopwatch.Frequency;
+            var delta = end - begin;
+            var ticks = (long)(timestampToTicks * delta);
+
+            return new TimeSpan(ticks);
+        }
+    }
+}
+
+#endif
