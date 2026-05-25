3c5dec3ef30a35e6b3a0a77ad8b1e9ff90421c40
Martin Tomašovič <tomasovicm@seznam.cz>
2026-05-25 13:41:30 +0200

    Fix postgresql datasource parsing for server address and port tags in span.

--- test/OpenTelemetry.Contrib.Shared.Tests/SqlConnectionDetailsTests.cs
+++ test/OpenTelemetry.Contrib.Shared.Tests/SqlConnectionDetailsTests.cs
@@ -20,6 +20,8 @@ public class SqlConnectionDetailsTests
     [InlineData("tcp:localhost", "localhost", null, null, null)]
     [InlineData("tcp:[::1]", null, "[::1]", null, null)]
     [InlineData("tcp : localhost", "localhost", null, null, null)]
+    [InlineData("tcp://some.domain.local:5432", "some.domain.local", null, null, 5432)]
+    [InlineData("tcp://some.domain.local", "some.domain.local", null, null, null)]
     [InlineData("np : localhost", "localhost", null, null, null)]
     [InlineData("lpc:localhost", "localhost", null, null, null)]
     [InlineData("np:\\\\localhost\\pipe\\sql\\query", "localhost", null, null, null)]
