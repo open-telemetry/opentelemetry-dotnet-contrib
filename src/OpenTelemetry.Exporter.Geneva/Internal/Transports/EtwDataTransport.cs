a65a6c480ae6332b374c2f030b8580ab94e19253
Dmitrii Zvorygin <zvorygin@gmail.com>
2026-07-10 10:20:16 -0600

    Fix ETW metadata to allow .Net EventSource subscriptions

--- src/OpenTelemetry.Exporter.Geneva/Internal/Transports/EtwDataTransport.cs
+++ src/OpenTelemetry.Exporter.Geneva/Internal/Transports/EtwDataTransport.cs
@@ -20,7 +20,7 @@ internal sealed class EtwDataTransport : IDataTransport, IDisposable
 
     public void Send(byte[] data, int size)
     {
-        this.eventSource.SendEvent((int)EtwEventSource.EtwEventId.TraceEvent, data, size);
+        this.eventSource.SendEvent(data, size);
     }
 
     public bool IsEnabled()
@@ -54,8 +54,19 @@ internal sealed class EtwDataTransport : IDataTransport, IDisposable
 
 #pragma warning disable CA1822 // Mark members as static
 
+        /// <summary>
+        /// Dummy _data_ field is present so that when <see cref="SendEvent"/> is called, the event has at least one
+        /// parameter with <see cref="EtwEventId.TraceEvent"/> and single data object, it matches the runtime metadata
+        /// in ETW manifest.
+        /// </summary>
+        /// <param name="data">
+        /// Dummy placeholder for <see cref="EventSource"/> reflection-based ETW manifest generation.
+        /// Even though de-facto it's <c>byte[]</c> payload, <c>byte[]</c> when unwrapped to ETW manifest generation
+        /// is augmented with <c>size</c> metadata, and becomes two fields.
+        /// Geneva treats this only single parameter as messagepack raw blob and doesn't use manifest at all.
+        /// </param>
         [Event((int)EtwEventId.TraceEvent, Version = 1, Level = EventLevel.Informational)]
-        public void InformationalEvent()
+        public void InformationalEvent(byte[] data)
         {
         }
 
@@ -65,14 +76,16 @@ internal sealed class EtwDataTransport : IDataTransport, IDisposable
 #if NET
         [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026:RequiresUnreferencedCode", Justification = "WriteEventCore is safe when eventData object is a primitive type, which it is in this case.")]
 #endif
-        public unsafe void SendEvent(int eventId, byte[] data, int size)
+        public unsafe void SendEvent(byte[] data, int size)
         {
-            var dataDesc = stackalloc EventData[1];
+            EventData* dataDesc = stackalloc EventData[2];
             fixed (byte* ptr = data)
             {
-                dataDesc[0].DataPointer = (IntPtr)ptr;
-                dataDesc[0].Size = size;
-                this.WriteEventCore(eventId, 1, dataDesc);
+                dataDesc[0].DataPointer = (IntPtr)(&size);
+                dataDesc[0].Size = 4;
+                dataDesc[1].DataPointer = (IntPtr)ptr;
+                dataDesc[1].Size = size;
+                this.WriteEventCore((int)EtwEventId.TraceEvent, 2, dataDesc);
             }
         }
     }
